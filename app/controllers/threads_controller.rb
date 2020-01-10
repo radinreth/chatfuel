1e882c632f4247cd365db755414e6e4957fec412
radinreth <radin.reth@gmail.com>
2020-01-10 15:37:41 +0700

    mock response

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -2,12 +2,21 @@ class ThreadsController < ApplicationController
   before_action :log_console
 
   def index
+    render json: resp
   end
 
   def create
   end
 
   private
+    def resp
+      {
+        "messages": [
+          {"text": "Hello world"},
+          {"text": "your info: #{params['invoice']}-#{params['phone_number']}"}
+        ]
+      }
+    end
 
     def log_console
       logger.warn action_name
