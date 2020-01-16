710c531c6e7ca5175dd597cd8c2b14973b294539
radinreth <radin.reth@gmail.com>
2020-01-16 11:08:50 +0700

    User#last_block

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :activities
+  has_many :activities, dependent: :destroy
 
   def last_block
     activities.last.to_block
