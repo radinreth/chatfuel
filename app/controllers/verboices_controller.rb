9886f47285bbfc843e7d87b65418925aa6737908
radinreth <radin.reth@gmail.com>
2020-01-20 14:09:09 +0700

    add verboice controller

---
+++ app/controllers/verboices_controller.rb
@@ -0,0 +1,7 @@
+class VerboicesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'verboices/index'
+  get 'verboices/create'
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   guisso_for :user
 
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   resources :threads, only: %i[index create show] do
     post :continue, on: :collection
   end
+
+  resources :verboices
 end
