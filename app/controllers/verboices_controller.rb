3ac186b8cbde82afdb61d4748362823ca1f18817
radinreth <radin.reth@gmail.com>
2020-01-20 14:44:46 +0700

    handle verboice basic call

--- app/controllers/verboices_controller.rb
+++ app/controllers/verboices_controller.rb
@@ -2,6 +2,24 @@ class VerboicesController < ApplicationController
   def index
   end
 
+  def new
+    @voice = Voice.where(call_sid: params[:CallSid]).first_or_initialize
+
+    @voice.call_sid = params[:CallSid]
+    @voice.status = params[:CallStatus]
+    @voice.from = params[:From]
+    @voice.call_duration = params[:CallDuration]
+    @voice.save
+
+    head :ok
+  end
+
   def create
+  end
+
+  private
+
+  def voice_params
+    params.permit(:CallSid, :CallStatus, :From, :CallDuration)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_080759) do
+ActiveRecord::Schema.define(version: 2020_01_20_071232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 2020_01_17_080759) do
     t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "voices", force: :cascade do |t|
+    t.string "call_sid"
+    t.string "status"
+    t.string "from"
+    t.integer "call_duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "activities", "leads"
