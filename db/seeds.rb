1824bd418642fc17715f93d406ff482bbb2f21db
radinreth <radin.reth@gmail.com>
2020-02-03 10:53:54 +0700

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,50 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+
+text_messages = [
+  {
+    messenger_user_id: 1,
+    first_name: 'bopha'
+  },
+  {
+    messenger_user_id: 2,
+    first_name: 'dara'
+  },
+  {
+    messenger_user_id: 3,
+    first_name: 'visal'
+  },
+  {
+    messenger_user_id: 4,
+    first_name: 'virak'
+  }
+]
+
+text_messages.each do |text|
+  Message.create content: TextMessage.create(text)
+end
+
+
+voice_messages = [
+  {
+    call_id: 1,
+    address: 123
+  },
+  {
+    call_id: 2,
+    address: 124
+  },
+  {
+    call_id: 3,
+    address: 125
+  },
+  {
+    call_id: 4,
+    address: 125
+  }
+]
+
+voice_messages.each do |voice|
+  Message.create content: VoiceMessage.create(voice)
+end
