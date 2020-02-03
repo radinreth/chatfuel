38071cfe15f5332a39608993edf626256bb60dc3
radinreth <radin.reth@gmail.com>
2020-02-03 14:53:31 +0700

    Add voice answers

--- app/models/voice_message.rb
+++ app/models/voice_message.rb
@@ -1,3 +1,4 @@
 class VoiceMessage < ApplicationRecord
   has_one :message, as: :content, dependent: :destroy
+  has_many :voice_answers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_090020) do
+ActiveRecord::Schema.define(version: 2020_02_03_073217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,15 @@ ActiveRecord::Schema.define(version: 2020_01_31_090020) do
     t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "voice_answers", force: :cascade do |t|
+    t.string "project_variable_name"
+    t.string "value"
+    t.bigint "voice_message_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["voice_message_id"], name: "index_voice_answers_on_voice_message_id"
   end
 
   create_table "voice_messages", force: :cascade do |t|
