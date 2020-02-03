228713b66d6d3a55d57def1e2c5a8419059b1466
radinreth <radin.reth@gmail.com>
2020-02-03 14:19:15 +0700

    Refactor MessagesController

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << 'dc2229ec.ngrok.io'
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
   config.hosts << 'owso.instedd.org'
 end
