05232adc137353e95ddf733c0138420ec699ed8a
radinreth <radin.reth@gmail.com>
2020-02-05 13:51:41 +0700

    update list ui

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   has_many :steps, dependent: :destroy
-  belongs_to :content, polymorphic: true
+  belongs_to :content, polymorphic: true, dependent: :destroy
 
   def last_step
     steps.last
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,7 +1,24 @@
+class BlankStep
+  def value
+    '-'
+  end
+end
+
 class Step < ApplicationRecord
   belongs_to :message
 
+  scope :f1, -> { find_by(act: 'f1') || BlankStep.new }
+  scope :f11, -> { find_by(act: 'f1.1') || BlankStep.new }
+  scope :f111, -> { find_by(act: 'f1.1.1') || BlankStep.new }
+  scope :f112, -> { find_by(act: 'f1.1.2') || BlankStep.new }
+  scope :f113, -> { find_by(act: 'f1.1.3') || BlankStep.new }
+  scope :f12, -> { find_by(act: 'f1.2') || BlankStep.new }
+  scope :f121, -> { find_by(act: 'f1.2.1') || BlankStep.new }
+  scope :f122, -> { find_by(act: 'f1.2.2') || BlankStep.new }
+  scope :f13, -> { find_by(act: 'f1.3') || BlankStep.new }
+  scope :f131, -> { find_by(act: 'f1.3.1') || BlankStep.new }
+
   def to_block
-    act.to_s.downcase.sub /set_/, 'ask '
+    act.to_s.downcase.sub(/set_/, 'ask ')
   end
 end
