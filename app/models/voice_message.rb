82a335840503d6b1491d63ef0ea15e218a55d6f8
radinreth <radin.reth@gmail.com>
2020-02-06 10:26:00 +0700

    refactoring with method delegation

--- app/models/voice_message.rb
+++ app/models/voice_message.rb
@@ -1,4 +1,8 @@
 class VoiceMessage < ApplicationRecord
   has_one :message, as: :content, dependent: :destroy
   has_many :steps, through: :message
+
+  delegate :f1, :f11, :f111, to: :steps
+  delegate :f112, :f113, :f12, to: :steps
+  delegate :f121, :f122, :f13, :f131, to: :steps
 end
