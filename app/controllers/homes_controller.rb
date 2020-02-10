51b9ed8a90aa6433994478be98331528f90ac318
radinreth <radin.reth@gmail.com>
2020-02-10 22:13:25 +0700

    Update home view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def show
     @messages = Message.includes(:steps).all
+    @variables = ["f1", "f11", "f111", "f112", "f113", "f121", "f122", "f131"]
   end
 end
