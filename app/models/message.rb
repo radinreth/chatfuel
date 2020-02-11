64184d03eaf12b0c4517185fb2fd977c8ded52f4
radinreth <radin.reth@gmail.com>
2020-02-11 11:37:26 +0700

    refactor message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,16 @@ class Message < ApplicationRecord
   has_many :steps, dependent: :destroy
   belongs_to :content, polymorphic: true, dependent: :destroy
 
+  def completed?
+    steps.where(act: 'done', value: 'true').present?
+  end
+
+  def self.create_or_update content
+    message = find_by(content: content)
+    message = create(content: content) if !message || message.completed?
+    message
+  end
+
   def last_step
     steps.last
   end
