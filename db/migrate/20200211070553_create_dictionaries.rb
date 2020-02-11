06dca2510a8c86cb642706be3661ac81ef431936
radinreth <radin.reth@gmail.com>
2020-02-11 14:31:22 +0700

    Add dictionary

--- db/migrate/20200211070553_create_dictionaries.rb
+++ db/migrate/20200211070553_create_dictionaries.rb
@@ -2,6 +2,7 @@ class CreateDictionaries < ActiveRecord::Migration[6.0]
   def change
     create_table :dictionaries do |t|
       t.string :value
+      t.string :header
 
       t.timestamps
     end
