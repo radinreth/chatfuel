6eaba262fb694f48555ce29c72ab4922d39b520c
radinreth <radin.reth@gmail.com>
2020-02-16 22:18:16 +0700

    Update style home#show

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,6 +2,6 @@ module HomeHelper
   def valueize str
     return str unless str =~ /results\/\d+/
 
-  audio_tag str, controls: true 
+  audio_tag str, controls: true, style: 'width: 200px'
   end
 end
