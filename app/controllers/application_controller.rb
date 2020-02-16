46cd61d5c733b97d79cd327ef9dd0b18ba42eafa
radinreth <radin.reth@gmail.com>
2020-02-16 21:02:44 +0700

    Add pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
   # before_action :authenticate_api_user!
   # before_action :authenticate_user_with_guisso!
   skip_before_action :verify_authenticity_token
