4ac4baf89d589bb22146adccbbf93840cd8cbbb6
radinreth <radin.reth@gmail.com>
2020-02-17 13:25:25 +0700

    fix fontawesome & toggle sidebar

---
+++ app/javascript/shared/sidebar.js
@@ -0,0 +1,23 @@
+(function() {
+  document.addEventListener('turbolinks:load', function() {
+    var handleActiveSidebar, init, onClickSidebar;
+    init = function() {
+      console.log('hi');
+      handleActiveSidebar();
+      return onClickSidebar();
+    };
+    handleActiveSidebar = function() {
+      var cssClass;
+      cssClass = localStorage.getItem('miniSidebar') === 'true' ? 'active' : '';
+      return $('#sidebar').addClass(cssClass);
+    };
+    onClickSidebar = function() {
+      return $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+        return localStorage.setItem('miniSidebar', $('#sidebar').hasClass('active'));
+      });
+    };
+    return init();
+  });
+
+}).call(this);
