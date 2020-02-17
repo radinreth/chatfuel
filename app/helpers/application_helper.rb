0900b68c764b44bdcdb17b0126e93ff0346fcdd9
radinreth <radin.reth@gmail.com>
2020-02-17 09:53:08 +0700

    update admin layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def css_class_name
+    "#{controller_path.parameterize}-#{action_name}"
+  end
+
+  def render_notice
+    content_tag(:p, notice, class: 'notice') if notice
+  end
+
+  def render_alert
+    content_tag(:p, alert, class: 'alert') if alert
+  end
 end
