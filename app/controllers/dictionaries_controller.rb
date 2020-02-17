2c1825cd4dee808e2a3dede9c55787fca43be900
radinreth <radin.reth@gmail.com>
2020-02-17 17:49:32 +0700

    update dictionary

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -2,4 +2,20 @@ class DictionariesController < ApplicationController
   def index
     @variables = Variable.order(name: 'asc')
   end
+
+  def update
+    @variable = Variable.find(params[:id])
+
+    if @variable.update(variable_params)
+      head :ok
+    else
+      render json: @variable.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def variable_params
+    params.require(:variable).permit(:text)
+  end
 end
