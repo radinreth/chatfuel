6a64fa221173b06eea63a560bcf6060290523364
radinreth <radin.reth@gmail.com>
2020-02-17 16:03:20 +0700

    Replace bootstrap with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+require("jquery")
+require("bootstrap/dist/js/bootstrap")
 
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
 
 require("shared/sidebar")
 
