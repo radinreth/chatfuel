48321a64d961bba483498bf5f822397e9e07d462
radinreth <radin.reth@gmail.com>
2020-02-18 09:05:16 +0700

    fix audio tag in chatbot

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
   def valueize str
-    return str unless str =~ /results\/\d+/
+    return str unless str =~ /(audioclip-\d+-\d+\.mp4)|(results\/\d+)/
 
   audio_tag str, controls: true, style: 'width: 200px'
   end
