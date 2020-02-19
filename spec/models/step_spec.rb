942122e87c17e8ead30544e3fddd21af52bcf7d5
radinreth <radin.reth@gmail.com>
2020-02-19 13:51:42 +0700

    add step model spec

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe Step do
   describe 'validations' do
     xit { should validate_uniqueness_of(:act).scoped_to(:message_id) }
   end
+
+  describe 'associations' do
+    it { should belong_to(:message) }
+  end
 end
