ef41e8b7279ce2c598ef4028b1724f4d8b6f7f53
radinreth <radin.reth@gmail.com>
2020-02-19 15:04:58 +0700

    update home helper spec

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -9,12 +9,20 @@ RSpec.describe HomeHelper do
     end
 
     context 'url' do
-      it 'render audio tag if behaves like audio' do
+      it 'render audio tag if behaves like FACEBOOK audio' do
         url = '...736_n.mp4/audioclip-1582097598-2369.mp4?_nc_cat=...'
+
         value = valueize(url)
 
-        p value
         expect(value).to eq %(<audio controls=\"controls\" style=\"width: 200px\" src=\"/audios/...736_n.mp4/audioclip-1582097598-2369.mp4?_nc_cat=...\"></audio>)
+      end
+
+      it 'render audio tag if behaves like VERBOICE audio' do
+        url = 'http://verboice.com/.../282342/results/1581497078055'
+
+        value = valueize(url)
+
+        expect(value).to eq %(<audio controls=\"controls\" style=\"width: 200px\" src=\"http://verboice.com/.../282342/results/1581497078055\"></audio>)
       end
     end
   end
