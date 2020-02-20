efa1ce347a4c6082e866189f0efda9f8d1d11f1b
radinreth <radin.reth@gmail.com>
2020-02-20 21:39:40 +0700

    fix bot with double request

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,3 @@
 class Site < ApplicationRecord
-  has_many :tracks
+  has_many :tracks, dependent: :destroy
 end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe Step do
   describe 'validations' do
     let(:text_message) { create(:text_message) }
     let(:message) { create(:message, content: text_message) }
+
     before { create(:step, message_id: message.id) }
 
     it { should validate_uniqueness_of(:act).scoped_to(:message_id) }
