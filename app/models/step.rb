2178515c82ceb5fdae4f17821ddf6027857c3ba4
radinreth <radin.reth@gmail.com>
2020-02-20 11:00:21 +0700

    separate tracking logic from step

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :message
+  has_one :track
 
   validates :act, uniqueness: { scope: :message_id }
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
-  it { should have_attribute(:code) }
+  describe 'attributes' do
+    it { should have_attribute(:code) }
+  end
+
+  xdescribe 'associations' do
+    it { should belong_to(:site) }
+    it { should belong_to(:step) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:code) }
+  end
+
+  describe '#site_code' do
+    let(:track) { build(:track, code: '1234-56789') }
+    
+    it 'substring code to get site_code' do
+      expect(track.site_code).to eq "1234"
+    end
+  end
 end
