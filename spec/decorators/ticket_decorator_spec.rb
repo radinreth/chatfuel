f8de815289a3bc2f13395446c5798a5e1cef445a
radinreth <radin.reth@gmail.com>
2020-02-24 11:42:12 +0700

    clean up ticket decorator

--- spec/decorators/ticket_decorator_spec.rb
+++ spec/decorators/ticket_decorator_spec.rb
@@ -1,38 +1,30 @@
 RSpec.describe TicketDecorator do
-  let(:ticket) { build(:ticket, status: :submitted) }
-
   context ':submitted' do
-    let(:ticket) { create(:ticket, status: :submitted) }
-
-    before do
-      @decorator = described_class.new(ticket)
-    end
-
     it 'is a kind of SubmittedStatus' do
+      ticket = create(:ticket, status: :submitted)
+
+      @decorator = described_class.new(ticket)
+
       expect(@decorator.send(:klazz)).to be_a_kind_of(SubmittedStatus)
     end
   end
 
   context ':completed' do
-    let(:ticket) { create(:ticket, status: :completed) }
-
-    before do
-      @decorator = described_class.new(ticket)
-    end
-
     it 'is a kind of CompletedStatus' do
+      ticket = create(:ticket, status: :completed)
+
+      @decorator = described_class.new(ticket)
+
       expect(@decorator.send(:klazz)).to be_a_kind_of(CompletedStatus)
     end
   end
 
   context ':picked_up' do
-    let(:ticket) { create(:ticket, status: :picked_up) }
-
-    before do
-      @decorator = described_class.new(ticket)
-    end
-
     it 'is a kind of PickedUpStatus' do
+      ticket = create(:ticket, status: :picked_up)
+
+      @decorator = described_class.new(ticket)
+
       expect(@decorator.send(:klazz)).to be_a_kind_of(PickedUpStatus)
     end
   end
