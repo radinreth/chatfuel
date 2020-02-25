8c3f2d3990b8a969bc8cefa1e6e715ec596febb9
radinreth <radin.reth@gmail.com>
2020-02-25 17:23:56 +0700

    Add notification job spec

--- config.ru
+++ config.ru
@@ -3,5 +3,5 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-
+require 'sidekiq-scheduler/web'
 run Rails.application
--- spec/jobs/notify_completed_ticket_job_spec.rb
+++ spec/jobs/notify_completed_ticket_job_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe NotifyCompletedTicketJob, type: :job do
+  include ActiveJob::TestHelper
+
+  subject { NotifyCompletedTicketJob }
+
   it { is_expected.to be_processed_in :default }
+
+  it 'enqueues a job' do
+    expect do
+      subject.perform_later
+    end.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'enqueues on default queue' do
+    expect do
+      subject.perform_later
+    end.to have_enqueued_job(described_class).on_queue 'default'
+  end
 end
+
