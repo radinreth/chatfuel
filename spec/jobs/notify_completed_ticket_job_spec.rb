ac8963845f7b3ac28b1345ae652cbb114b0ba7b4
radinreth <radin.reth@gmail.com>
2020-02-25 16:11:39 +0700

    update job spec

--- spec/jobs/notify_completed_ticket_job_spec.rb
+++ spec/jobs/notify_completed_ticket_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe NotifyCompletedTicketJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to be_processed_in :default }
 end
