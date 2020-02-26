24836ba136998a56dc29c00845cbdc12a8d0f2d0
radinreth <radin.reth@gmail.com>
2020-02-26 09:00:38 +0700

    update activejob todo note

--- app/jobs/notify_completed_ticket_job.rb
+++ app/jobs/notify_completed_ticket_job.rb
@@ -1,6 +1,7 @@
 class NotifyCompletedTicketJob < ApplicationJob
   queue_as :default
 
+  # TODO: use system setting instead of predefine setting in config/sidekiq.yml
   def perform
     completed = Ticket.completed_with_session
 
