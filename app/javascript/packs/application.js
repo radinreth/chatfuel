454137b8934f352bf8a275981fe82931306d1bc5
radinreth <radin.reth@gmail.com>
2020-02-26 10:52:34 +0700

    add basic barchart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("bootstrap/dist/js/bootstrap")
+require("chart.js/dist/Chart")
 
 //= require jquery3
 //= require popper
@@ -16,6 +17,7 @@ require("bootstrap/dist/js/bootstrap")
 
 require("shared/sidebar")
 require("view/dictionary")
+require("view/reports")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
