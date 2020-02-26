0d86f595b689e52ffe17cb194b0b762dd6e54986
radinreth <radin.reth@gmail.com>
2020-02-26 11:19:48 +0700

    add stubs linechart

--- app/javascript/view/reports.js
+++ app/javascript/view/reports.js
@@ -1,38 +1,38 @@
 document.addEventListener('turbolinks:load', function() {
   var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [
+        {
+          label: 'Accessed',
+          data: [12, 19, 3, 5, 2, 1, 3],
+          backgroundColor: 'red',
+          borderColor: 'red',
+          fill: false,
+        },
+        {
+          label: 'Applied',
+          data: [22, 5, 2, 6, 9, 10, 13],
+          backgroundColor: 'blue',
+          borderColor: 'blue',
+          fill: false,
+        },
+        {
+          label: 'Completed',
+          data: [5, 7, 1, 9, 19, 4, 8],
+          backgroundColor: 'orange',
+          borderColor: 'orange',
+          fill: false,
+        }
+      ]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
+      responsive: true,
+      title: {
+        display: true,
+        text: 'OWSO services'
       }
     }
   })
