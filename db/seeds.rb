a5d8f7569a7e39ffe6569f42dc79ab34b11dc048
radinreth <radin.reth@gmail.com>
2020-02-26 22:18:26 +0700

    cleanup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
-Dir[Rails.root.join('db', 'seed', '**', '*.rb')].each { |f| require f }
+paths = Rails.root.join('db', 'seed', '**', '*.rb')
+Dir[paths].each { |f| require f }
 
-Seed::Variable.generate!
-Seed::Ticket.generate!
-Seed::Site.generate!
-Seed::Message.generate!
-Seed::Step.generate!
-Seed::Track.generate!
+%w[Variable Ticket Site Message Step Track].each do |model|
+  "Seed::#{model}".constantize.send(:generate!)
+end
