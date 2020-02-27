6798aa72b47636b794f0a133ddf31575f694593d
radinreth <radin.reth@gmail.com>
2020-02-27 15:44:34 +0700

    add daterangepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,7 @@ require("chart.js")
 
 require("shared/sidebar")
 require("view/dictionary")
+require("view/daterangepicker")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
