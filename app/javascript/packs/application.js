7677e9628481fbf700f8c945b3275aa1847825fe
radinreth <radin.reth@gmail.com>
2020-02-27 10:13:35 +0700

    add chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("bootstrap/dist/js/bootstrap")
+require("chartkick")
 require("chart.js/dist/Chart")
 
 //= require jquery3
