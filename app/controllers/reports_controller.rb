c03f31d838062276157e0e315ae647c9de0ca86d
radinreth <radin.reth@gmail.com>
2020-02-27 10:59:57 +0700

    add multi-series linecharts

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,21 @@
 class ReportsController < ApplicationController
   def index
+    @accessed  = Step.where(act: 'owso_options').extending do
+      def name
+        'Accessed'
+      end
+    end
+    @submitted = Ticket.submitted.extending do
+      def name
+        'Submitted'
+      end
+    end
+    @completed = Ticket.completed.extending do
+      def name
+        'Completed'
+      end
+    end
+
+    @goals = [ @accessed, @submitted, @completed]
   end
 end
