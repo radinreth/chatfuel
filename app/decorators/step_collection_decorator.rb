1dc87f5e2c950442ad512c52224668c680cc120d
radinreth <radin.reth@gmail.com>
2020-02-28 09:19:58 +0700

    Add total&avg section for line - chart

--- app/decorators/step_collection_decorator.rb
+++ app/decorators/step_collection_decorator.rb
@@ -2,4 +2,8 @@ class StepCollectionDecorator < BaseCollectionDecorator
   def collection
     Step.send(@sym)
   end
+  
+  def to_partial_path
+    'steps/step'
+  end
 end
--- app/decorators/ticket_collection_decorator.rb
+++ app/decorators/ticket_collection_decorator.rb
@@ -2,4 +2,16 @@ class TicketCollectionDecorator < BaseCollectionDecorator
   def collection
     Ticket.send(@sym)
   end
+
+  def total
+    collection.count
+  end
+
+  def avg
+    number_to_percentage(total / period.duration_in_day)
+  end
+
+  def to_partial_path
+    'tickets/ticket'
+  end
 end
