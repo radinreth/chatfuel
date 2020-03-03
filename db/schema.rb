b45c9550eb6ba02b1e00a1e1977dba3e1a124c64
radinreth <radin.reth@gmail.com>
2020-03-03 11:23:19 +0700

    Add model identities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_093814) do
+ActiveRecord::Schema.define(version: 2020_03_03_041923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_03_02_093814) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["step_id"], name: "index_feedbacks_on_step_id"
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.string "provider"
+    t.string "token"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -132,5 +141,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_093814) do
     t.integer "project_id"
   end
 
+  add_foreign_key "identities", "users"
   add_foreign_key "steps", "messages"
 end
