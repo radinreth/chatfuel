453a7f900626ef59411fcdc5842e8642e4bdeab9
radinreth <radin.reth@gmail.com>
2020-03-03 22:29:58 +0700

    Add user with roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.skip_confirmation!
+    if @user.save
+      redirect_to users_path, status: :moved_permanently, notice: 'Created successfully'
+    else
+      render :new, alert: 'created failed'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
   root 'homes#show'
 
   # TODO: only authenticated user
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
+
+  scope '/role' do
+    resources :users, only: [:index, :new, :create]
+  end
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # guisso_for :user
@@ -19,7 +20,6 @@ Rails.application.routes.draw do
   resources :sites do
     post 'import', on: :collection
   end
-  resources :users, only: [:index, :new, :create]
   resources :tracks, only: [:create]
   resources :reports, only: [:index]
   resources :messages, only: [:create] do
