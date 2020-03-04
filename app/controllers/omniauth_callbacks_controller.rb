f34f053dfcd3a62c383b6ff68eee7ab940165324
Borey Lim <lim.borey@gmail.com>
2020-03-04 15:12:15 +0700

    integrate instedd single sign on

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
+
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :authenticate_user_with_guisso!
+  skip_before_action :verify_authenticity_token
   skip_before_action :check_guisso_cookie
 
   def instedd
     generic do |auth|
       {
-        email: auth.info['email']
-        # name: auth.info['name'],
+        email: auth.info["email"]
       }
     end
   end
 
-  def failure
-    render plain: params.inspect
-  end
-
   def generic
-    auth = env['omniauth.auth']
+    auth = request.env["omniauth.auth"]
 
-    if identity = Identity.find_by_provider_and_token(auth['provider'], auth['uid'])
+    if identity = Identity.find_by(provider: auth["provider"], token: auth["uid"])
       user = identity.user
     else
       attributes = yield auth
@@ -26,15 +25,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       unless user
         password = Devise.friendly_token
         user = User.new(attributes.merge(password: password, password_confirmation: password))
+        # user = User.new(attributes)
         user.confirmed_at = Time.now
         user.save!
       end
       user.identities.create! provider: auth['provider'], token: auth['uid']
     end
-    
+
     sign_in user
-    next_url = request.env['omniauth.origin'] || root_path
-    next_url = root_path if next_url == new_user_session_url
+    next_url = request.env["omniauth.origin"] || root_path
+    next_url = root_path if next_url == new_user_session_path
     redirect_to next_url
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require_relative 'boot'
 
 require 'rails/all'
 require "openid"
-require 'openid/extensions/sreg'
-require 'openid/extensions/pape'
-require 'openid/store/filesystem'
+require "openid/extensions/sreg"
+require "openid/extensions/pape"
+require "openid/store/filesystem"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
