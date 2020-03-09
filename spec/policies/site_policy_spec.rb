206b4ce5b46c7fd16a0286a6bc2a00634d5ab401
radinreth <radin.reth@gmail.com>
2020-03-09 09:25:32 +0700

    Add manifest policy specs

--- spec/policies/site_policy_spec.rb
+++ spec/policies/site_policy_spec.rb
@@ -4,22 +4,23 @@ RSpec.describe SitePolicy do
   subject { described_class.new(user, site) }
 
   let(:site) { build(:site) }
+  let(:site_actions) { %i[index new edit update destroy] }
 
   context "being a ombudsman" do
     let(:user) { build(:user, :ombudsman) }
 
-    it { is_expected.to forbid_actions(%i[index]) }
+    it { is_expected.to forbid_actions(site_actions) }
   end
 
   context "being a site_admin" do
     let(:user) { build(:user, :site_admin) }
 
-    it { is_expected.to forbid_actions(%i[index]) }
+    it { is_expected.to forbid_actions(site_actions) }
   end
 
   context "being a system_admin" do
     let(:user) { build(:user, :system_admin) }
 
-    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_actions(site_actions) }
   end
 end
