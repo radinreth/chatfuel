1a0584dcb04c858d28f2bdaa7e5f9eb76904558f
radinreth <radin.reth@gmail.com>
2020-03-09 10:14:52 +0700

    Add message policy specs

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :message do
+    trait :text do
+      association :content, factory: :text_message
+    end
+
+    trait :voice do
+      association :content, factory: :voice_message
+    end
   end
 end
