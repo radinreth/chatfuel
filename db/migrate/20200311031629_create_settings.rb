d223881e73e61b413ca4aa167f30751e5d0340e5
Borey Lim <lim.borey@gmail.com>
2020-03-11 12:16:10 +0700

    implement completed/uncompleted template setting

---
+++ db/migrate/20200311031629_create_settings.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateSettings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :settings do |t|
+      t.text :uncompleted_text
+      t.text :completed_text
+
+      t.timestamps
+    end
+  end
+end
