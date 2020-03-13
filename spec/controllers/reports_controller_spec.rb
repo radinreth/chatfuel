48f3f322051c59416802dcf9943e9c617a029df6
radinreth <radin.reth@gmail.com>
2020-03-13 16:51:27 +0700

    Report with default date range

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ReportsController, type: :controller do
   setup_system_admin
@@ -6,13 +6,13 @@ RSpec.describe ReportsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET #index with params" do
     it "return instance of StepCollectionDecorator or TicketCollectionDecorator" do
-      get :index, params: { dates: '28/02/2020 - 28/02/2020' }
+      get :index, params: { dates: "28/02/2020 - 28/02/2020" }
 
       expect(assigns(:goals)).to all(be_a(StepCollectionDecorator).or be_a(TicketCollectionDecorator))
     end
