ade869f40763d5de019ec305307ddfbe78d9f531
radinreth <radin.reth@gmail.com>
2020-03-13 15:52:48 +0700

    Create dictionary

--- app/policies/variable_policy.rb
+++ app/policies/variable_policy.rb
@@ -2,4 +2,24 @@ class VariablePolicy < ApplicationPolicy
   def index?
     user.system_admin?
   end
+
+  def show?
+    user.system_admin?
+  end
+
+  def new?
+    user.system_admin?
+  end
+
+  def create?
+    user.system_admin?
+  end
+
+  def update?
+    user.system_admin?
+  end
+
+  def destroy?
+    user.system_admin?
+  end
 end
