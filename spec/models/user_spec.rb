345d44b444c9103f17b3ea20085061b767d33584
radinreth <radin.reth@gmail.com>
2020-03-13 10:24:53 +0700

    Add specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,5 +33,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to define_enum_for(:role).with_values(%i[ombudsman site_admin system_admin]) }
   it { is_expected.to define_enum_for(:status).with_values(%i[enable disable]) }
   it { is_expected.to have_many(:identities).dependent(:destroy) }
-
+  it { is_expected.to belong_to(:site).optional }
 end
