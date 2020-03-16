8a58fdf42b6443f8c36b503ecd342bc154f74f50
radinreth <radin.reth@gmail.com>
2020-03-16 16:02:45 +0700

    capture dictionary & implement IVR feedback

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,33 @@
+class MessagesController < ApplicationController
+  skip_before_action :authenticate_user_without_guisso!
+  skip_before_action :verify_authenticity_token
+
+  before_action :set_dictionary
+  before_action :set_message
+  before_action :set_step
+
+  def create
+    head :ok
+  end
+
+  private
+    def set_message
+      messenger_user_id = params[:messenger_user_id]
+      content = TextMessage.find_or_create_by(messenger_user_id: messenger_user_id)
+      @message = Message.create_or_return(content)
+    end
+
+    def set_step
+      @set_step ||= @message.steps.create(act: params[:act], value: params[:value])
+    end
+
+    def set_dictionary
+      @dictionary ||= TextVariable.create_with(value: params[:value]).find_or_create_by(text_variable_params)
+    end
+
+    def text_variable_params
+      _params = params.permit(:act, :value)
+      _params[:name] = _params.delete(:act)
+      _params
+    end
+end
