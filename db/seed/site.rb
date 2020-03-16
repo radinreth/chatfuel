9881490bc29a5e433473504db244e110245ed444
radinreth <radin.reth@gmail.com>
2020-03-16 11:32:12 +0700

    Refactor site seed to service

--- db/seed/site.rb
+++ db/seed/site.rb
@@ -1,16 +1,7 @@
-require 'csv'
-require_relative 'time_parser'
-
 module Seed
   class Site
-    def self.generate!(path = 'db/seed/assets/site.csv')
-      CSV.foreach(path, headers: true) do |row|
-        hash = row.to_hash
-        TimeParser.parse(hash) do |parsed|
-          site = ::Site.new(parsed)
-          site.save
-        end
-      end
+    def self.generate!(path = "db/seed/assets/site.csv")
+      SiteService.import(path)
     end
   end
 end
--- spec/policies/manifest_policy_spec.rb
+++ spec/policies/manifest_policy_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ManifestPolicy do
   subject { described_class.new(user) }
