f4143273b58023f8dc14249050a2d175902dabcc
radinreth <radin.reth@gmail.com>
2020-03-16 11:32:12 +0700

    Refactor site seed to service

---
+++ app/services/site_service.rb
@@ -0,0 +1,14 @@
+require "csv"
+require_relative "time_parser"
+
+class SiteService
+  def self.import(path)
+    ::CSV.foreach(path, headers: true) do |row|
+      hash = row.to_hash
+      TimeParser.parse(hash) do |parsed|
+        site = ::Site.new(parsed)
+        site.save
+      end
+    end
+  end
+end
