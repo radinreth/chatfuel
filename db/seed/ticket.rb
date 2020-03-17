67680a779a413acceecc1df3c8128a9ef82764d3
radinreth <radin.reth@gmail.com>
2020-03-17 08:21:22 +0700

    Refactor seed data

--- db/seed/ticket.rb
+++ db/seed/ticket.rb
@@ -1,10 +1,10 @@
-require 'csv'
-require_relative 'time_parser'
+require "csv"
+require "time_parser"
 
 module Seed
   class Ticket
     def self.generate!
-      CSV.foreach('db/seed/assets/tickets.csv', headers: true) do |row|
+      CSV.foreach("db/seed/assets/tickets.csv", headers: true) do |row|
         hash = row.to_hash
 
         TimeParser.parse(hash) do |parsed|
