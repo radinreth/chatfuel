791b1868abbf5e86c982fbbfb1c5c366b1dfce45
radinreth <radin.reth@gmail.com>
2020-03-18 16:50:48 +0700

    Add dynamic dictionary variable

--- app/javascript/view/dictionary.js
+++ app/javascript/view/dictionary.js
@@ -10,7 +10,26 @@ document.addEventListener('turbolinks:load', function() {
     $(".result-map table").addClass("d-none");
     $(".dict-item-" + klassName).removeClass("d-none");
   })
+
+  $(".btn-add").click(function(e) {
+    e.preventDefault()
+
+    var $tr = `<tr>
+      <td>
+        <div class="form-group hidden dictionaries_id"><input class="form-control hidden" value="" name="variable[][id]" type="hidden" id="dictionaries_id"></div>
+        <div class="form-group string required dictionaries_value"><input class="form-control string required mr-sm-2" name="variable[][value]" id="variable_value" type="text"></div>
+      </td>
+
+      <td>
+        <div class="form-group string required dictionaries_text"><input class="form-control string required mr-sm-2" name="variable[][text]" id="variable_text" type="text"></div>
+      </td>
+    </tr>`
+  
+    $(this).closest("tr")[0].before($($tr)[0])
+  })
 })
+
+
 
 document.addEventListener('turbolinks:before-render', function() {
   $(".dict-item").removeClass('active');
