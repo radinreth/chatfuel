8ee017a812c99a7581da425188d4e5461372e6a2
radinreth <radin.reth@gmail.com>
2020-03-26 13:42:36 +0700

    Add variable values

---
+++ app/models/variable_value.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: variable_values
+#
+#  id            :bigint(8)        not null, primary key
+#  mapping_value :string           default("")
+#  raw_value     :string           not null
+#  status        :integer(4)       default("0")
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  variable_id   :bigint(8)        not null
+#
+# Indexes
+#
+#  index_variable_values_on_variable_id  (variable_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (variable_id => variables.id)
+#
+class VariableValue < ApplicationRecord
+  belongs_to :variable
+end
