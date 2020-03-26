0a316f29a6a3dc335f1a33185e5a1b7e999a0163
radinreth <radin.reth@gmail.com>
2020-03-26 23:35:23 +0700

    set dictionary value

--- app/controllers/bots/feedbacks/chatbot_controller.rb
+++ app/controllers/bots/feedbacks/chatbot_controller.rb
@@ -2,10 +2,16 @@ module Bots::Feedbacks
   class ChatbotController < ::Bots::FeedbacksController
     private
       def set_dictionary
-        @dictionary ||= TextVariable.create_with(value: params[:value]).find_or_create_by(variable_params)
+        # @dictionary ||= TextVariable.create_with(value: params[:value]).find_or_create_by(variable_params)
+        @dictionary ||= TextVariable.find_or_create_by(name: text_variable_params[:name])
+        begin
+          @value ||= @dictionary.values.create!(raw_value: text_variable_params[:value])
+        rescue ActiveRecord::RecordInvalid
+          @value ||= @dictionary.values.find_by(raw_value: text_variable_params[:value])
+        end
       end
 
-      def variable_params
+      def text_variable_params
         _params = params.permit(:act, :value)
         _params[:name] = _params.delete(:act)
         _params
