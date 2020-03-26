b4d167834a1ec5f8e30526a9467e8865eeaab4f1
radinreth <radin.reth@gmail.com>
2020-03-26 10:40:54 +0700

    extract role from user model

---
+++ spec/factories/roles.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :role do
+    name { "MyString" }
+  end
+end
