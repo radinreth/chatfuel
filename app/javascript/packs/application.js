70fac25f7e026e80fe97f3f19d8a8cf85702deb6
radinreth <radin.reth@gmail.com>
2020-03-27 12:21:44 +0700

    Add toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,13 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-
 require("channels")
+
 require("jquery")
 require("bootstrap")
 require("chartkick")
 require("chart.js")
+toastr = require("toastr")
 
 require("shared/sidebar")
 require("view/dictionary")
