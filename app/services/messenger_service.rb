614112faaea1968088b7bef6ccdffb303d2bf275
radinreth <radin.reth@gmail.com>
2020-04-16 16:03:25 +0700

    Update fb token

--- app/services/messenger_service.rb
+++ app/services/messenger_service.rb
@@ -1,30 +1,32 @@
 class MessengerService
-  def self.notify(completed)
-    completed.find_each do |ticket|
-      RestClient.post url, params(ticket) if ENV["ENABLE_FB_NOTIFY"] == "enable"
-      # ticket.notified!
-    end
-  end
-
-  private
-    def url
-      host = "https://graph.facebook.com"
-      path = "v6.0/me/messages"
-      token = ENV["FB_PAGE_TOKEN"]
-
-      @url ||= "#{host}/#{path}?access_token=#{token}"
+  class << self
+    def notify(completed)
+      completed.find_each do |ticket|
+        RestClient.post(url, params(ticket), content_type: "application/json") if ENV["ENABLE_FB_NOTIFY"] == "enable"
+        # ticket.notified!
+      end
     end
 
-    # TODO: response with pre-defined template
-    def params(ticket)
-      {
-        message_type: "RESPONSE",
-        recipient: {
-          id: ticket.message.session_id
-        },
-        message: {
-          text: "#{ticket.code} is completed!"
+    private
+      def url
+        host = "https://graph.facebook.com"
+        path = "v6.0/me/messages"
+        token = ENV["FB_PAGE_TOKEN"]
+
+        @url ||= "#{host}/#{path}?access_token=#{token}"
+      end
+
+      # TODO: response with pre-defined template
+      def params(ticket)
+        {
+          messaging_type: "RESPONSE",
+          recipient: {
+            id: ticket.message.session_id
+          },
+          message: {
+            text: "#{ticket.code} is completed!"
+          }
         }
-      }
-    end
+      end
+  end
 end
