334c803781526b359310ed5be1c2889cabc52192
radinreth <radin.reth@gmail.com>
2020-04-20 10:40:34 +0700

    Add ticket spec

---
+++ spec/controllers/tickets_controller_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe TicketsController, type: :controller do
+  setup_system_admin
+
+  it "GET :index" do
+    get :index
+
+    expect(response).to render_template("index")
+  end
+
+  it "GET :new" do
+    get :new
+
+    expect(response).to render_template("new")
+  end
+
+  it "GET :edit" do
+    get :edit, params: { id: create(:ticket).id }
+
+    expect(response).to render_template("edit")
+  end
+
+  it "POST :create" do
+    expect do
+      post :create, params: { ticket: { code: "123" } }
+    end.to change { Ticket.count }.by(1)
+  end
+
+  context "updates" do
+    let(:new_template) { create(:ticket, code: "12345") }
+
+    it "PUT :update" do
+      put :update, params: { id: new_template.id, ticket: { code: "123" } }
+
+      updated = assigns(:ticket)
+
+      expect(response).to have_http_status(:moved_permanently)
+      expect(updated.code).to eq "123"
+    end
+  end
+
+  context "destroy" do
+    before do
+      @ticket = create(:ticket, code: "123")
+    end
+
+    it "DELETE :destroy" do
+      expect do
+        delete :destroy, params: { id: @ticket.id }
+      end.to change { Ticket.count }.by(-1)
+    end
+  end
+end
