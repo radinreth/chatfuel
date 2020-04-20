df82c036f1eb033bfaba56328b79d212bc49b88b
radinreth <radin.reth@gmail.com>
2020-04-20 10:09:56 +0700

    Add tagify

--- app/jobs/notify_completed_ticket_job.rb
+++ app/jobs/notify_completed_ticket_job.rb
@@ -3,7 +3,7 @@ class NotifyCompletedTicketJob < ApplicationJob
 
   # TODO: use system setting instead of predefine setting in config/sidekiq.yml
   def perform
-    completed = Ticket.completed_with_session
+    completed = Ticket.completed_with_session.limit(20)
 
     MessengerService.notify(completed)
   end
