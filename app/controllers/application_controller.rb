67b1ac46f65f195b055d81267a1ec297e0ce7cbd
radinreth <radin.reth@gmail.com>
2020-04-21 11:17:39 +0700

    Add controller localize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,20 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   before_action :authenticate_user_with_guisso!
+  around_action :switch_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   private
     def user_not_authorized
-      flash[:alert] = "You are not authorized"
+      flash[:alert] = t("not_authorized")
       redirect_to(reports_path) && return
+    end
+
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale(locale, &action)
     end
 end
