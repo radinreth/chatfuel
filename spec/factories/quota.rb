6001477fcc1db9804a4cae8cd54a66c7e9bf4a60
radinreth <radin.reth@gmail.com>
2020-05-20 17:28:06 +0700

    add quota total_sent

--- spec/factories/quota.rb
+++ spec/factories/quota.rb
@@ -7,6 +7,7 @@
 #  platform_name      :string           default("messenger")
 #  secure_zone        :float            default("0.75")
 #  threshold          :integer(4)       default("0")
+#  total_sent         :integer(4)       default("0")
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #
