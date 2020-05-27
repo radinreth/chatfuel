5fe91f79dda3a739034ac34ae3a39c46f47d955b
radinreth <radin.reth@gmail.com>
2020-05-27 09:50:38 +0700

    add whitelist to access manifest

---
+++ config/Whitelist.rb
@@ -0,0 +1,14 @@
+class Whitelist
+  def matches?(request)
+    klass.allowed_hosts.include?(request.remote_ip)
+  end
+
+  def self.allowed_hosts
+    ENV["ALLOWED_HOSTS"].split(",").map(&:strip)
+  end
+
+  private
+    def klass
+      @klass ||= self.class
+    end
+end
