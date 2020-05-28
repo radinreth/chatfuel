c708c4dfa8604d5449ad674a5e6a3a84c17886b2
radinreth <radin.reth@gmail.com>
2020-05-28 13:43:56 +0700

    refactor voice message controller

---
+++ app/controllers/bots/messages_controller.rb
@@ -0,0 +1,23 @@
+module Bots
+  class MessagesController < BotsController
+    before_action :set_variable
+    before_action :set_message
+    before_action :set_site
+    before_action :set_step
+
+    def done
+      @message.completed! if params[:name] == "done" && params[:value] == "true"
+      head :ok
+    end
+
+    private
+      def set_step
+        @step = @message.steps.create(value: @variable_value)
+        @step.update(site: @site) if @site
+      end
+
+      def set_site
+        @site = Site.find_by(name: params["value"]) if params["name"] == "feedback_site"
+      end
+  end
+end
