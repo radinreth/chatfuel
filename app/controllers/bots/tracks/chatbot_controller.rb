d0435199749974f53a339dc4aef3c2e84df3dcf3
radinreth <radin.reth@gmail.com>
2020-05-28 11:55:55 +0700

    refactor duplicated code tracks controller

--- app/controllers/bots/tracks/chatbot_controller.rb
+++ app/controllers/bots/tracks/chatbot_controller.rb
@@ -5,13 +5,5 @@ module Bots::Tracks
         @content = TextMessage.find_or_create_by(messenger_user_id: params[:messenger_user_id])
         @message = Message.create_or_return(params[:platform_name], @content)
       end
-
-      def set_site
-        @site = Site.find_by(code: params[:code][0...4])
-      end
-
-      def set_ticket
-        @ticket ||= Ticket.find_by(code: normalized_code)
-      end
   end
 end
