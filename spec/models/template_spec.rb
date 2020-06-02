1a4a91168549d9fcc410240c2cac21a0e1a084aa
radinreth <radin.reth@gmail.com>
2020-06-02 14:37:25 +0700

    add spec validate template status

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -16,10 +16,17 @@ RSpec.describe Template, type: :model do
   it { is_expected.to validate_presence_of(:content) }
   it { is_expected.to validate_presence_of(:platform_name) }
   it { is_expected.to define_enum_for(:platform_name).with_values(messenger: "0", telegram: "1", verboice: "2").backed_by_column_of_type(:string) }
+  it { is_expected.to define_enum_for(:status).with_values(incomplete: "0", completed: "1", incorrect: "2").backed_by_column_of_type(:string) }
 
   it "#platform_value" do
-    t = build(:template, platform_name: :telegram)
+    template = build(:template, platform_name: :telegram)
 
-    expect(t.platform_value).to eq "1"
+    expect(template.platform_value).to eq "1"
+  end
+
+  it "#status_value" do
+    template = build(:template, status: :completed)
+
+    expect(template.status_value).to eq "1"
   end
 end
