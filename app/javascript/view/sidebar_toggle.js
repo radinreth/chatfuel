4ca65d86b3728620fe5a012ded2ceff9e7caf068
radinreth <radin.reth@gmail.com>
2020-06-08 15:32:47 +0700

    add sidebar toggle

---
+++ app/javascript/view/sidebar_toggle.js
@@ -0,0 +1,6 @@
+document.addEventListener("turbolinks:load", function(){
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+  });
+})
