494336e106908792010fea258d8be35671d34c76
radinreth <radin.reth@gmail.com>
2020-06-08 15:07:57 +0700

    fix codeclimate similar block warning

--- app/javascript/view/chart.js
+++ app/javascript/view/chart.js
@@ -3,8 +3,7 @@ document.addEventListener('turbolinks:load', function() {
   Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
   Chart.defaults.global.defaultFontColor = '#858796';
 
-  var ctx = document.getElementById("total-visitors");
-  var totalVisitors = new Chart(ctx, {
+  var options = {
     type: 'doughnut',
     data: {
       labels: ["Direct", "Referral", "Social"],
@@ -32,39 +31,13 @@ document.addEventListener('turbolinks:load', function() {
       },
       cutoutPercentage: 80,
     },
-  });
+  }
+
+  var ctx = document.getElementById("total-visitors");
+  var totalVisitors = new Chart(ctx, options);
 
   var ctx = document.getElementById("total-feedbacks");
-  var totalFeedbacks = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ["Direct", "Referral", "Social"],
-      datasets: [{
-        data: [55, 30, 15],
-        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-      },
-      legend: {
-        display: false
-      },
-      cutoutPercentage: 80,
-    },
-  });
-
+  var totalFeedbacks = new Chart(ctx, options);
 
   var ctx = document.getElementById("total-tracked-tickets");
   var totalTrackedTickets = new Chart(ctx, {
