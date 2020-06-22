5add43d438f6836e7d29643a2ce310849795b3c5
radinreth <radin.reth@gmail.com>
2020-06-22 15:48:23 +0700

    add sync history job spec

--- spec/jobs/sync_history_job_spec.rb
+++ spec/jobs/sync_history_job_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SyncHistoryJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  subject { SyncHistoryJob }
+
+  it { is_expected.to be_processed_in :default }
+
+  it "enqueues a job" do
+    expect do
+      subject.perform_later
+    end.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it "enqueues on default queue" do
+    expect do
+      subject.perform_later
+    end.to have_enqueued_job(described_class).on_queue "default"
+  end
 end
