431a461a9ee96d09a7fbe88539815b8e38b3ef15
radinreth <radin.reth@gmail.com>
2020-06-22 15:35:24 +0700

    add missing site, ticket specs

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -21,8 +21,13 @@ require "rails_helper"
 RSpec.describe Site, type: :model do
   it { is_expected.to have_attribute(:name) }
   it { is_expected.to have_attribute(:code) }
+  it { is_expected.to have_attribute(:status) }
+  it { is_expected.to have_attribute(:sync_status) }
   it { is_expected.to define_enum_for(:status).with_values(%i[disable enable]) } 
   it { is_expected.to have_many(:users) }
+  it { is_expected.to have_many(:tickets) }
+  it { is_expected.to respond_to(:generate_token) }
+  it { is_expected.to respond_to(:valid_token?) }
 
   describe "uniqueness" do
     before do
