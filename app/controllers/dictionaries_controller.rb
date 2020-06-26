ef51e8ba507ba3cb36d124a24eff0ced27259d3b
radinreth <radin.reth@gmail.com>
2020-06-26 15:48:58 +0700

    enhance dictionary ui

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,18 +1,13 @@
 class DictionariesController < ApplicationController
+  before_action :set_roles, only: [:index, :edit, :create, :update]
+  before_action :set_new_variable, only: [:index, :search]
+
   def index
-    @pagy, @variables = pagy(Variable.except_done)
-    @variable = Variable.new
+    @pagy, @variables = pagy(Variable.except_done.order(created_at: :desc))
     authorize @variables
   end
 
-  def new
-    @variable = Variable.new
-    @variable.values.build
-    authorize @variable
-  end
-
   def edit
-    @roles = Role.all
     @variable = Variable.find(params[:id])
     @variable.values.build
     authorize @variable
@@ -25,11 +20,9 @@ class DictionariesController < ApplicationController
     respond_to do |format|
       if @variable.save
         format.html { redirect_to dictionaries_path, status: :moved_permanently, notice: t("created.success") }
-        format.json { render json: @variable, status: :created, location: @variable }
         format.js
       else
         format.html { render :new, status: :unprocessable_entity, alert: t("created.fail") }
-        format.json { render json: @variable.errors, status: :unprocessable_entity }
         format.js
       end
     end
@@ -37,18 +30,16 @@ class DictionariesController < ApplicationController
 
   def search
     @q = params[:search][:q]
-    @variable = Variable.new
     @pagy, @variables = pagy(Variable.except_done.where("name LIKE ?", "%#{@q}%"))
     render :index
   end
 
   def update
     @variable = Variable.find(params[:id])
-    @roles = Role.all
 
     authorize @variable
     respond_to do |format|
-      if @variable.update(variable_nested_params)
+      if @variable.update(variable_params)
         format.html { redirect_to dictionaries_path, status: :moved_permanently, notice: t("updated.success") }
         format.js
       else
@@ -58,57 +49,16 @@ class DictionariesController < ApplicationController
     end
   end
 
-  def batch_update
-    updates = []
-    options = { status: :moved_permanently }
-
-    batch_params.each do |item_params|
-      updates << update_variable(item_params.permit!)
-    end
-
-    if updates.all?(true)
-      options[:notice] = t("updated.success")
-    end
-
-    redirect_to dictionaries_path, options
-  end
-
-  def destroy
-    @variable = Variable.find(params[:id])
-    @variable.destroy
-  end
-
-  def batch_update
-    batch_params.each do |_param|
-      id, text = _fetch(_param)
-      update_variable(id, text)
-    end
-
-    redirect_to dictionaries_path, status: :moved_permanently, notice: "update successful!"
-  end
-
   private
-    def update_variable(item_params)
-      item_id = item_params.delete("id")
-      item_params["status"] = item_params.delete("status").to_i
-
-      if item_id.present?
-        variable = Variable.find(item_id)
-        variable.update(item_params) if variable
-      else
-        Variable.create(item_params)
-      end
+    def set_roles
+      @roles = Role.all
     end
 
-    def batch_params
-      params.require(:variable)
+    def set_new_variable
+      @variable = Variable.new
     end
 
     def variable_params
-      params.require(:variable).permit(:type, :name, :report_enabled, role_ids: [])
-    end
-
-    def variable_nested_params
-      params.require(:variable).permit(:report_enabled, role_ids: [], values_attributes: [:id, :raw_value, :mapping_value, :status, :_destroy])
+      params.require(:variable).permit(:type, :name, :report_enabled, role_ids: [], values_attributes: [:id, :raw_value, :mapping_value, :status, :_destroy])
     end
 end
