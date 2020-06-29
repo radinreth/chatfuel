af97283090961adc6531ecd50aa067c824ebd9c6
radinreth <radin.reth@gmail.com>
2020-06-29 16:53:31 +0700

    add message spec

---
+++ spec/controllers/bots/messages/chatbot_controller_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Bots::Messages::ChatbotController, type: :controller do
+  it "creates messenger variable" do
+    expect {
+      post :create, params: { messenger_user_id: 123, platform_name: "Messenger", name: "main_menu", value: "owso_info" }
+    }.to change { Variable.count }.by 1
+
+    variable = Variable.first
+
+    expect(variable.name).to eq "main_menu"
+    expect(variable.values.count).to eq 1
+    expect(variable.values.first.raw_value).to eq "owso_info"
+  end
+
+  it "create telegram variable" do
+    expect {
+      post :create, params: { messenger_user_id: 123, platform_name: "Telegram", name: "main_menu", value: "owso_info" }
+    }.to change { Variable.count }.by 1
+  end
+
+  it "create verboice variable" do
+    expect {
+      post :create, params: { messenger_user_id: 123, platform_name: "Verboice", name: "main_menu", value: "owso_info" }
+    }.to change { Variable.count }.by 1
+  end
+
+  it "cannot create variable with unknow platform" do
+    expect {
+      post :create, params: { messenger_user_id: 123, platform_name: "test", name: "main_menu", value: "owso_info" }
+    }.to raise_error ActiveRecord::RecordNotSaved
+  end
+end
