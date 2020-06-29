dd391f7e5282bc1b920756eb02af9fd0ce410bbf
radinreth <radin.reth@gmail.com>
2020-06-29 16:14:26 +0700

    add spec

---
+++ spec/controllers/bots/locations_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Bots::LocationsController, type: :controller do
+  it "create location" do
+    expect {
+      post :create, params: { messenger_user_id: 123, platform_name: 'Messenger', location_name: 'PNP' }
+    }.to change { Message.count }.by(1)
+
+    expect(TextMessage.first.location_name).to eq "PNP"
+  end
+end
