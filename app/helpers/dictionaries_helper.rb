d67f804befa956f078de62dadfc76e7d68bd538f
radinreth <radin.reth@gmail.com>
2020-07-02 16:37:18 +0700

    refactor

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -1,8 +1,8 @@
 module DictionariesHelper
   def total_users_visit_each_functions
-    Message\
-      .merge(send("join_#{@message_type}_message".to_sym))\
-      .joins(steps: { step_value: :variable_value })\
+    Message
+      .merge(send("join_#{@message_type}_message".to_sym))
+      .joins(steps: { step_value: :variable_value })
       .where("variable_values.raw_value IN (?)", ["owso_info", "tracking", "feedback"])
       .where("DATE(step_values.created_at) BETWEEN ? AND ?", @start_date, @end_date)
       .order(:raw_value)
@@ -11,20 +11,20 @@ module DictionariesHelper
   end
 
   def join_text_message
-    Message\
-      .joins("INNER JOIN text_messages ON text_messages.id=messages.content_id")\
+    Message
+      .joins("INNER JOIN text_messages ON text_messages.id=messages.content_id")
       .where("text_messages.location_name=?", @location)
   end
 
   def join_voice_message
-    Message\
+    Message
       .joins("INNER JOIN voice_messages ON voice_messages.id=messages.content_id")
   end
 
   def join_both_message
-    inner_query = Message\
-          .merge(join_voice_message)\
-          .joins("UNION SELECT messages.* FROM messages")\
+    inner_query = Message
+          .merge(join_voice_message)
+          .joins("UNION SELECT messages.* FROM messages")
           .merge(join_text_message)
 
     Message.unscoped.select("*").from(inner_query, :messages)
@@ -47,7 +47,7 @@ module DictionariesHelper
       .merge(send("join_#{@message_type}_message".to_sym))
       .joins(steps: { track: :ticket })
       .where("tickets.status IN (?)", statuses)
-      .where("DATE(steps.created_at) BETWEEN ? AND ?", @start_date, @end_date)
+      .where("DATE(tracks.created_at) BETWEEN ? AND ?", @start_date, @end_date)
       .group(:status)
       .count
   end
