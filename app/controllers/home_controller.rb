24c895ba93f7bdb7b6d75055dcd15f9927088cd2
radinreth <radin.reth@gmail.com>
2020-07-06 09:34:45 +0700

    update home data base on role

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @pagy, @messages = pagy(Message.all)
+    @pagy, @messages = pagy(Message.joins(steps: { value: :variable }).where("variables.name IN (?)", current_user.role.variables.pluck(:name)))
     authorize :menu_item
 
     render :no_message if @messages.count.zero?
