26c05ecddb13929abc6fde14a928505e900e8ab5
radinreth <radin.reth@gmail.com>
2020-07-07 10:23:58 +0700

    refactor dashboard query

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -1,24 +1,32 @@
+# TODO: filter by type : text, voice or both
+#       add line chart
 module DictionariesHelper
   def total_users_visit_each_functions
-    Message
-      .merge(send("join_#{@message_type}_message".to_sym))
-      .joins(steps: { step_value: :variable_value })
-      .where("variable_values.raw_value IN (?)", ["owso_info", "tracking", "feedback"])
-      .where("DATE(step_values.created_at) BETWEEN ? AND ?", @start_date, @end_date)
-      .order(:raw_value)
-      .group(:raw_value)
+    result = StepValue\
+      .joins(step: :message, variable_value: :variable)\
+      .where(variables: { name: "main_menu" })\
+      .where(messages: { platform_name: @platform_name })
+      .where("DATE(step_values.created_at) BETWEEN ? AND ?", @start_date, @end_date)\
+      .order(:raw_value)\
+      .group(:raw_value)\
       .count
+
+    # prevent inconsistent chartjs color
+    default ||= { "feedback" => 0, "owso_info" => 0, "tracking" => 0 }
+    default.merge(result)
   end
 
   def join_text_message
     Message\
       .joins("INNER JOIN text_messages ON text_messages.id=messages.content_id")\
       .where("text_messages.location_name=?", @location)
+      .where("DATE(text_messages.created_at) BETWEEN ? and ?", @start_date, @end_date)
   end
 
   def join_voice_message
     Message\
       .joins("INNER JOIN voice_messages ON voice_messages.id=messages.content_id")
+      .where("DATE(voice_messages.created_at) BETWEEN ? and ?", @start_date, @end_date)
   end
 
   def join_both_message
@@ -31,24 +39,24 @@ module DictionariesHelper
   end
 
   def total_users_feedback
-    Message
-      .merge(send("join_#{@message_type}_message".to_sym))
-      .joins(steps: { step_value: { variable_value: :variable } })
-      .where("variables.report_enabled=?", true)
+    StepValue
+      .joins(variable_value: :variable)
+      .where(variables: { report_enabled: true })
       .where("DATE(step_values.created_at) BETWEEN ? AND ?", @start_date, @end_date)
       .group(:raw_value)
       .count
   end
 
   def number_of_tracking_tickets
-    statuses = Ticket.statuses.fetch_values(:incomplete, :completed)
+    statuses = Ticket.statuses
+    status_values = statuses.fetch_values(:incomplete, :completed)
 
-    Message
-      .merge(send("join_#{@message_type}_message".to_sym))
-      .joins(steps: { track: :ticket })
-      .where("tickets.status IN (?)", statuses)
+    Track
+      .joins(:ticket)
+      .where(tickets: { status: status_values })
       .where("DATE(tracks.created_at) BETWEEN ? AND ?", @start_date, @end_date)
       .group(:status)
       .count
+      .transform_keys { |k| statuses.key(k).capitalize }
   end
 end
