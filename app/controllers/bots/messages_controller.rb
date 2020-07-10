182077f0ebacc32dd119872fa5fbb58829c75ac6
radinreth <radin.reth@gmail.com>
2020-07-10 15:58:01 +0700

    add map preview route

--- app/controllers/bots/messages_controller.rb
+++ app/controllers/bots/messages_controller.rb
@@ -1,13 +1,21 @@
 module Bots
   class MessagesController < BotsController
-    before_action :set_variable
-    before_action :set_message
-    before_action :set_site
-    before_action :set_step
+    before_action :set_variable, except: [:preview_map]
+    before_action :set_message, except: [:preview_map]
+    before_action :set_site, except: [:preview_map]
+    before_action :set_step, except: [:preview_map]
 
     def done
       @message.completed! if params[:name] == "done" && params[:value] == "true"
       head :ok
+    end
+
+    def preview_map
+      if params[:location]
+        render json: { messages: [ gallery ] }, status: :ok
+      else
+        head :ok
+      end
     end
 
     private
@@ -18,6 +26,36 @@ module Bots
 
       def set_site
         @site = Site.find_by(name: params["value"]) if params["name"] == "feedback_site"
+      end
+
+      def gallery
+        @gallery ||= {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "generic",
+              image_aspect_ratio: "square",
+              elements: [
+                {
+                  title: "ទីតាំងការិយាល័យច្រក",
+                  subtitle: "សម្រាប់ពត៍មានបន្ថែមសូមចុចតំនរភ្ជាប់ខាងក្រោម 👇",
+                  image_url: sample_map_image_url,
+                  buttons: [
+                    {
+                      type: "web_url",
+                      url: "https://www.google.com/maps/place/#{params[:location]}",
+                      title: "មើលលើផែនទី!"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      end
+
+      def sample_map_image_url
+        @sample_map_image_url ||= "https://dl.dropbox.com/s/0efkg615shrabhu/Google%20Maps%202020-07-10%2014-49-51.png?dl=0"
       end
   end
 end
