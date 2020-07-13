aad9d1aefa9229d1744a5229b3b57e5aa441a14b
Sokly <hengsokly23@gmail.com>
2020-07-13 09:50:11 +0700

    allow sync status nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_064423) do
+ActiveRecord::Schema.define(version: 2020_07_13_023412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_064423) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
-    t.integer "sync_status", default: 1, null: false
+    t.integer "sync_status"
     t.string "token", default: ""
     t.text "whitelist"
     t.string "province_id"
