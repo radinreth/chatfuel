66c93cd602d4dd86b5b80339a32d7c6a1478386e
Sokly <hengsokly23@gmail.com>
2020-07-15 11:11:23 +0700

    refactor

--- app/models/sync_log.rb
+++ app/models/sync_log.rb
@@ -12,8 +12,4 @@ class SyncLog < ApplicationRecord
   belongs_to :site
 
   default_scope { order(created_at: :desc) }
-
-  def html_tag
-    "<li class='list-group-item'><span>#{I18n.l(created_at, format: :long)}<span> :<span class='ml-1 #{status == 'success' ? 'text-success' : 'text-danger'}'>#{status}</span></li>"
-  end
 end
