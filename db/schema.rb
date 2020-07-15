44eb2d43c7c76345a0dd2d597c61f89f8b4f64a8
Sokly <hengsokly23@gmail.com>
2020-07-15 11:43:01 +0700

    add all option to dashboard province

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_023412) do
+ActiveRecord::Schema.define(version: 2020_07_14_064423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_07_13_023412) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
     t.string "platform_name", default: ""
-    t.datetime "last_interaction_at", default: "2020-07-15 03:02:47"
+    t.datetime "last_interaction_at", default: "2020-07-15 06:31:07"
     t.index ["content_type", "content_id"], name: "index_messages_on_content_type_and_content_id"
   end
 
