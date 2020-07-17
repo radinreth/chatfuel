3c56e8737df886145737013f0c3728901b934d43
Sokly <hengsokly23@gmail.com>
2020-07-17 15:55:19 +0700

    handle datepicker in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,21 @@
 class DashboardController < ApplicationController
+  before_action :set_daterange
+
   def show
     @options = {
       location: (params[:location] || "Banteay Meanchey"),
       platform_name: ["Messenger", "Telegram", "Verboice"],
-      start_date: (params[:start_date] || 7.days.ago),
-      end_date: (params[:end_date] || Time.current),
+      start_date: @start_date,
+      end_date: @end_date,
       user_count: [:join_text_message, :join_voice_message]
     }
     @query = DashboardQuery.new(@options)
   end
+
+  private
+    def set_daterange
+      default_date = "#{7.days.ago.strftime('%Y/%m/%d')} - #{Date.current.strftime('%Y/%m/%d')}"
+      @date_range = params['daterange'] || default_date
+      @start_date, @end_date = @date_range.split('-')
+    end
 end
--- app/javascript/view/daterangepicker.js
+++ app/javascript/view/daterangepicker.js
@@ -1,27 +1,27 @@
 document.addEventListener('turbolinks:load', function() {
-  let daterangepicker = new DateRangePicker('.datepicker_date', {
-    locale: { format: 'DD/MM/YYYY'},
-    startDate: moment().subtract(7,'d'),
-    cancelLabel: 'Clear',
-    alwaysShowCalendars: true,
-    showCustomRangeLabel: true,
-    ranges: {
-      'Today': [moment(), moment()],
-      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month': [moment().startOf('month'), moment().endOf('month')],
-      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-   },
-   opens: 'left'
-  }, function(startDate, endDate, label) {
+  // let daterangepicker = new DateRangePicker('.datepicker_date', {
+  //   locale: { format: 'DD/MM/YYYY'},
+  //   startDate: moment().subtract(7,'d'),
+  //   cancelLabel: 'Clear',
+  //   alwaysShowCalendars: true,
+  //   showCustomRangeLabel: true,
+  //   ranges: {
+  //     'Today': [moment(), moment()],
+  //     'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+  //     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+  //     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+  //     'This Month': [moment().startOf('month'), moment().endOf('month')],
+  //     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+  //  },
+  //  opens: 'left'
+  // }, function(startDate, endDate, label) {
 
-    var params = new URLSearchParams(location.search)
-    params.set('start_date', startDate.format("YYYY-MM-DD"));
-    params.set('end_date', endDate.format("YYYY-MM-DD"));
+  //   var params = new URLSearchParams(location.search)
+  //   params.set('start_date', startDate.format("YYYY-MM-DD"));
+  //   params.set('end_date', endDate.format("YYYY-MM-DD"));
 
-    var url = `${location.pathname}?${params}`
-    window.history.replaceState({}, '', url);
-    document.location = url;
-  })
+  //   var url = `${location.pathname}?${params}`
+  //   window.history.replaceState({}, '', url);
+  //   document.location = url;
+  // })
 })
