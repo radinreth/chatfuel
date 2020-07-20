91fadcde6f33117022498b32e167be22ec54d359
radinreth <radin.reth@gmail.com>
2020-07-20 09:59:04 +0700

    chunk code by features

---
+++ db/variables.rb
@@ -0,0 +1,534 @@
+variables = [
+  {
+    name: "main_menu",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: true,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "owso_info",
+        mapping_value: ""
+      },
+      {
+        raw_value: "tracking",
+        mapping_value: ""
+      },
+      {
+        raw_value: "feedback-intro",
+        mapping_value: ""
+      }
+    ]
+  },
+  # owso_info
+  {
+    name: "owso_info",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: true,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "certify_docs",
+        mapping_value: ""
+      },
+      {
+        raw_value: "main_dbs",
+        mapping_value: ""
+      },
+      {
+        raw_value: "main_construction",
+        mapping_value: ""
+      },
+      {
+        raw_value: "main_land_title",
+        mapping_value: ""
+      },
+      {
+        raw_value: "main_public_transport",
+        mapping_value: ""
+      },
+      {
+        raw_value: "main_land_refill",
+        mapping_value: ""
+      }
+    ]
+  },
+  # certify docs
+  {
+    name: "certify_docs",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "certify_doc_1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "certify_doc_2",
+        mapping_value: ""
+      }
+    ]
+  },
+  # main_dbs
+  {
+    name: "main_dbs",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "dbs_btn1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn2",
+        mapping_value: ""
+      }
+    ]
+  },
+  # dbs_btn1
+  {
+    name: "dbs_btn1",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "dbs_btn1_1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn1_2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn1_3",
+        mapping_value: ""
+      }
+    ]
+  },
+  # dbs_btn2
+  {
+    name: "dbs_btn2",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "dbs_btn2_1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn2_2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn2_3",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn2_4",
+        mapping_value: ""
+      }
+    ]
+  },
+  # dbs_btn2_2
+  {
+    name: "dbs_btn2_2",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "dbs_btn2_2_1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "dbs_btn2_2_2",
+        mapping_value: ""
+      }
+    ]
+  },
+  # main construction
+  {
+    name: "main_construction",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "construction_1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "construction_1",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "construction_1.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_1.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_1.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "construction_2",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "construction_2.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_2.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_2.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "construction_3",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "construction_3.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_3.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "construction_3.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  # main_land_title
+  {
+    name: "main_land_title",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land title 1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "land title 1",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land title 1.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 1.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 1.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "land title 2",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land title 2.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 2.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 2.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "land title 3",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land title 3.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 3.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land title 3.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  # refill
+  {
+    name: "main_refill_title",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land_refill 1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land_refill 2",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "land_refill 1",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land_refill 1.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land_refill 1.2",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "land_refill 2",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "land_refill 2.1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land_refill 2.2",
+        mapping_value: ""
+      },
+      {
+        raw_value: "land_refill 2.3",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "feedback-intro",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: []
+  },
+  {
+    name: "feedback_level",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "krong battambong",
+        mapping_value: ""
+      },
+      {
+        raw_value: "district",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "feedback_challenge",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: true,
+    values: [
+      {
+        raw_value: "high price",
+        mapping_value: ""
+      },
+      {
+        raw_value: "complicated",
+        mapping_value: ""
+      },
+      {
+        raw_value: "take long time",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "feedback_q2_like",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "working time",
+        mapping_value: ""
+      },
+      {
+        raw_value: "speech",
+        mapping_value: ""
+      },
+      {
+        raw_value: "provide info",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "feedback_q3_dislike",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "working time",
+        mapping_value: ""
+      },
+      {
+        raw_value: "speech",
+        mapping_value: ""
+      },
+      {
+        raw_value: "provide info",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "public_transport",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: [
+      {
+        raw_value: "transport 1",
+        mapping_value: ""
+      },
+      {
+        raw_value: "transport 2",
+        mapping_value: ""
+      }
+    ]
+  },
+  {
+    name: "tracking",
+    type: "TextVariable",
+    feedback_message: false,
+    is_most_request: false,
+    is_user_visit: false,
+    report_enabled: false,
+    values: []
+  }
+]
+
+puts "creating Variables"
+variables.each do |variable_params|
+  values = variable_params.delete(:values)
+  variable = Variable.new(variable_params)
+
+  variable.values.build(values)
+  unless variable.save
+    puts variable_params
+  end
+end
