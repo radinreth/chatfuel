8dfdfad3e7abca0d40069ed69015b1dc3a4cbdce
Sokly <hengsokly23@gmail.com>
2020-07-21 16:55:32 +0700

    Variable: remove column feedback_message

--- app/models/both_variable.rb
+++ app/models/both_variable.rb
@@ -2,16 +2,15 @@
 #
 # Table name: variables
 #
-#  id               :bigint(8)        not null, primary key
-#  feedback_message :boolean          default("false")
-#  is_location      :boolean
-#  is_most_request  :boolean          default("false")
-#  is_user_visit    :boolean          default("false")
-#  name             :string
-#  report_enabled   :boolean          default("false")
-#  type             :string           not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  is_location     :boolean
+#  is_most_request :boolean          default("false")
+#  is_user_visit   :boolean          default("false")
+#  name            :string
+#  report_enabled  :boolean          default("false")
+#  type            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class BothVariable < Variable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_043700) do
+ActiveRecord::Schema.define(version: 2020_07_21_090842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -273,7 +273,6 @@ ActiveRecord::Schema.define(version: 2020_07_21_043700) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "report_enabled", default: false
-    t.boolean "feedback_message", default: false
     t.boolean "is_most_request", default: false
     t.boolean "is_user_visit", default: false
     t.boolean "is_location"
