6b7fc5702526f40f526f4e7a66bef2f10b98a9cc
Sokly <hengsokly23@gmail.com>
2020-07-21 15:46:34 +0700

    dashboard: remove ivr and chatbot controller

--- app/controllers/dashboard/chatbot_dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-module Dashboard
-  class ChatbotDashboardController < DashboardController
-    def show
-      super
-      @options[:platform_name] = ["Messenger", "Telegram"]
-      @options[:user_count] = 'TextMessage'
-    end
-  end
-end
--- app/controllers/dashboard/ivr_dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-module Dashboard
-  class IvrDashboardController < DashboardController
-    def show
-      super
-      @options[:platform_name] = ["Verboice"]
-      @options[:user_count] = 'VoiceMessage'
-    end
-  end
-end
