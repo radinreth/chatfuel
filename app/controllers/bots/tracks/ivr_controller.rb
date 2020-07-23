7f2bb0139dabd29cd60e91d88a5f00c3968f6bb7
radinreth <radin.reth@gmail.com>
2020-07-23 16:17:18 +0700

    response ivr audio

--- app/controllers/bots/tracks/ivr_controller.rb
+++ app/controllers/bots/tracks/ivr_controller.rb
@@ -1,5 +1,9 @@
 module Bots::Tracks
   class IvrController < ::Bots::TracksController
+    def create
+      render xml: { Play: "https://api.twilio.com/cowbell.mp3" }.to_xml(root: "Response")
+    end
+
     private
       def set_message
         @content = VoiceMessage.find_or_create_by(callsid: params[:CallSid])
