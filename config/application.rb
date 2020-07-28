2892f1a890803690776f9cc7ef3fe96bb9a71687
kakada <kakada.chheang@gmail.com>
2020-07-28 15:31:12 +0700

    Remove accidentially commit config.hosts in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,6 @@ module Chatfuel
     end
 
     config.time_zone = 'Bangkok'
-    config.hosts << "owso-dev.ilabsea.org"
     config.action_dispatch.rescue_responses["OmniauthCallbacksController::Forbidden"] = :forbidden
   end
 end
