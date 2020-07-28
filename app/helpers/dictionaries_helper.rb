45a8e1bbc94fe2aa7e0c952e8f76cfbc970c713f
Sokly <hengsokly23@gmail.com>
2020-07-28 10:46:13 +0700

    Dictionary: fix edit variable value

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -2,4 +2,14 @@ module DictionariesHelper
   def chartjs_colors
     @chartjs_colors ||= %w(#1cc88a #4e73df #f63e3e #7400b8 #bdb2ff #bc6c25 #5e6472 #ff5d8f #fee440 #1a936f)
   end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + '_fields', f: builder)
+    end
+
+    link_to(name.html_safe, '#', class: "add_#{association} btn btn-link text-decoration-none", data: { id: id, fields: fields.gsub("\n", '') })
+  end
 end
