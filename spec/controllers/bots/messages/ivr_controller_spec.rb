1f39cd9bb09df42fe8137fb62af2a2d40db03f0b
Sokly <hengsokly23@gmail.com>
2020-08-03 15:46:35 +0700

    add spec

---
+++ spec/controllers/bots/messages/ivr_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Bots::Messages::IvrController, type: :controller do
+  it "create verboice variable" do
+    expect {
+      post :create, params: { callsid: 123, platform_name: "Verboice", name: "main_menu", value: "owso_info" }
+    }.to change { Variable.count }.by 1
+  end
+
+  it "cannot create variable with unknow platform" do
+    expect {
+      post :create, params: { callsid: 123, platform_name: "test", name: "main_menu", value: "owso_info" }
+    }.to raise_error ActiveRecord::RecordNotSaved
+  end
+end
