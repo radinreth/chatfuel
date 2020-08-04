04b29ee3e778f74b09b2fb13732f4eb10996b967
radinreth <radin.reth@gmail.com>
2020-08-04 13:31:02 +0700

    refactor MessengerChannel

--- app/services/channels/messenger_channel.rb
+++ app/services/channels/messenger_channel.rb
@@ -4,8 +4,8 @@ module Channels
       return if !ticket.message || (ENV["ENABLE_FB_NOTIFY"] != "enable")
 
       if ticket.message.reachable_period?
-        RestClient.post(ENV['FB_MESSAGING_ENDPOINT'], params(ticket), content_type: "application/json")
-        Rails.logger.info "Sent: #{ENV['FB_MESSAGING_ENDPOINT']} #{params(ticket)}"
+        RestClient.post("#{ENV['FB_MESSAGING_URL']}?access_token=#{ENV['FB_MESSAGING_TOKEN']}", params(ticket), content_type: "application/json")
+        Rails.logger.info "Sent: #{ENV['FB_MESSAGING_URL']} #{params(ticket)}"
       end
     end
 
