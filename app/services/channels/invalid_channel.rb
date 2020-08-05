2ee79c8caabcb7e5e55153446456f6c3b61720a5
radinreth <radin.reth@gmail.com>
2020-08-05 08:53:46 +0700

    update docker env variable FB_REACHABLE_IN_DAY

--- app/services/channels/invalid_channel.rb
+++ app/services/channels/invalid_channel.rb
@@ -1,7 +1,7 @@
 module Channels
   class InvalidChannel
     def send_message
-      Rails.logger.debug "EmptyChannel: #{self}"
+      Rails.logger.debug "InvalidChannel: #{self}"
     end
   end
 end
