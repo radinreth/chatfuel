b171529098660017f342311d205b08c7fef6593e
radinreth <radin.reth@gmail.com>
2020-08-05 08:50:35 +0700

    cleanup comment

--- spec/services/channels/base_channel_spec.rb
+++ spec/services/channels/base_channel_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Channels::BaseChannel do
-
   let(:ticket) { build(:ticket) }
 
   describe ".get" do
@@ -23,34 +22,4 @@ RSpec.describe Channels::BaseChannel do
   it "raise" do
     expect { subject.send_message(ticket) }.to raise_error(RuntimeError, "you have to implement in subclass")
   end
-
-  # let!(:template) { create(:template) }
-
-  # let(:ticket) { build(:ticket, :incomplete) }
-  # let(:message) { build(:message, :text) }
-  # let(:tracking_variable) { build(:variable, :tracking) }
-  # let(:variable_value) { build(:variable_value, raw_value: ticket.code, variable: tracking_variable) }
-  # let!(:message_step) { create(:step, value: variable_value, message: message) }
-
-  # it ".get" do
-  #   channel = described_class.get(ticket.platform_name)
-  #   expect(channel).to be_kind_of(Channels::MessengerChannel)
-  # end
-
-  # it "#send_message" do
-  #   channel = described_class.new
-  #   expect { channel.send_message(ticket) }.to raise_error(RuntimeError, "you have to implement in subclass")
-  # end
-
-  # it "#text_message" do
-  #   content = subject.send(:text_message, template)
-
-  #   expect(content).to eq template.content
-  # end
-
-  # it "#response_template" do
-  #   template = subject.send(:response_template, ticket)
-
-  #   expect(template).to be_kind_of(Template)
-  # end
 end
