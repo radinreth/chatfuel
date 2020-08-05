afc7b161093f7cf6fa83f377e25af567ad2b7403
Sokly <hengsokly23@gmail.com>
2020-08-05 09:58:46 +0700

    add spec

--- spec/models/variable_value_spec.rb
+++ spec/models/variable_value_spec.rb
@@ -22,5 +22,12 @@
 require 'rails_helper'
 
 RSpec.describe VariableValue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'before_destroy' do
+    let(:step_value)      { create(:step_value) }
+    let(:variable_value)  { step_value.variable_value }
+    let(:variable_value2) { create(:variable_value) }
+
+    it { expect(variable_value.destroy).to be_falsey }
+    it { expect(variable_value2.destroy).to be_truthy }
+  end
 end
