4294e7b98f02adca836007e327b8f793857782e5
radinreth <radin.reth@gmail.com>
2020-08-24 15:44:54 +0700

    migrate data from message to session

--- app/models/voice_message.rb
+++ app/models/voice_message.rb
@@ -15,7 +15,7 @@ class VoiceMessage < ApplicationRecord
   # associations
   has_one :message, as: :content, dependent: :destroy
 
-  alias_attribute :session_id, :callsid
+  alias_attribute :session_id, :address
 
   def type
     "IVR"
