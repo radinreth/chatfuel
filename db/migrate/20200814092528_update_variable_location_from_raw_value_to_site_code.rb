f3c566c21ae9b7b7c6b28ed0367a6fb42c9011cf
radinreth <radin.reth@gmail.com>
2020-08-24 10:09:08 +0700

    add migration transform wrong province id

--- db/migrate/20200814092528_update_variable_location_from_raw_value_to_site_code.rb
+++ db/migrate/20200814092528_update_variable_location_from_raw_value_to_site_code.rb
@@ -1,4 +1,21 @@
 class UpdateVariableLocationFromRawValueToSiteCode < ActiveRecord::Migration[6.0]
-  def change
+  LOC_NAMES = { Th: '02', ot: '00', Mo: '02', Kr: '02', Ka: '02', Ba: '02' }
+
+  def up
+    messages.find_each do |m|
+      m.update(province_id: LOC_NAMES[m.province_id.to_sym])
+    end
+  end
+
+  def down
+    messages.find_each do |m|
+      m.update(province_id: LOC_NAMES.key(m.province_id).to_s)
+    end
+  end
+
+  private
+
+  def messages
+    @messages ||= Message.where.not(province_id: nil)
   end
 end
