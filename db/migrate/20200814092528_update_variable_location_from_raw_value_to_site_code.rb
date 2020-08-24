38c83451704c8f6bac335b4877170764672198e9
radinreth <radin.reth@gmail.com>
2020-08-24 11:24:45 +0700

    add migration update location variable value
    update from site name to site code

--- db/migrate/20200814092528_update_variable_location_from_raw_value_to_site_code.rb
+++ db/migrate/20200814092528_update_variable_location_from_raw_value_to_site_code.rb
@@ -1,21 +1,29 @@
 class UpdateVariableLocationFromRawValueToSiteCode < ActiveRecord::Migration[6.0]
-  LOC_NAMES = { Th: '02', ot: '00', Mo: '02', Kr: '02', Ka: '02', Ba: '02' }
+  SITES = { "Krong Battambang": '0203', "Moung Ruessei": '0206', "Thma Koul": '0202', "Bavel": '0204', "Kamrieng": '0212', "others": '0000' }
 
   def up
-    messages.find_each do |m|
-      m.update(province_id: LOC_NAMES[m.province_id.to_sym])
+    return unless variable
+
+    variable.update(name: 'location')
+    # update location from site_name to site_code
+    variable.values.each do |value|
+      value.update(raw_value: SITES[value.raw_value.to_sym])
     end
   end
 
   def down
-    messages.find_each do |m|
-      m.update(province_id: LOC_NAMES.key(m.province_id).to_s)
+    return unless variable
+
+    variable.update(name: 'location_name')
+    # update location from site_code to site_name
+    variable.values.each do |value|
+      value.update(raw_value: SITES.key(value.raw_value).to_s)
     end
   end
 
   private
 
-  def messages
-    @messages ||= Message.where.not(province_id: nil)
+  def variable
+    @variable ||= Variable.find_by(is_location: true)
   end
 end
