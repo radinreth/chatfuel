5327fa8e1812e4631fa632d708cb10c288a5c7ce
radinreth <radin.reth@gmail.com>
2020-08-25 11:12:49 +0700

    Fix spec

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -17,41 +17,13 @@ require 'rails_helper'
 RSpec.describe Session, type: :model do
   it { is_expected.to define_enum_for(:status).with_values(%i[incomplete completed]) }
 
-  describe ".create_or_return" do
-    let!(:session) { Session.create_or_return("Messenger", "123") }
-
-    context "create" do
-      it "creates when it's new" do
-        expect {
-          Session.create_or_return("Messenger", "9999")
-        }.to change { Session.count }.by 1
-      end
-
-      it "creates when it's completed" do
-        session.completed!
-
-        expect {
-          Session.create_or_return("Messenger", "123")
-        }.to change { Session.count }.by 1
-      end
-    end
-
-    context "return" do
-      it "returns when it's already created and incomplete" do
-        expect {
-          Session.create_or_return("Messenger", "123")
-        }.not_to change { Session.count }
-      end
-    end
-  end
-
-  describe 'set_province_id' do
-    let(:session) { create(:session, district_id: '03122345') }
+  describe "set_province_id" do
+    let(:session) { create(:session, district_id: "03122345") }
 
     it "extracts province_id from district_id before update" do
-      session.update(district_id: '01234567')
+      session.update(district_id: "01234567")
 
-      expect(session.reload.province_id).to eq('01')
+      expect(session.reload.province_id).to eq("01")
     end
   end
 
