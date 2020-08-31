e041a2c4b1a6ba853a70da38049e8b85073a5658
radinreth <radin.reth@gmail.com>
2020-08-31 09:39:05 +0700

    fix spec

--- app/controllers/bots/sessions/chatbot_controller.rb
+++ app/controllers/bots/sessions/chatbot_controller.rb
@@ -8,7 +8,6 @@ module Bots::Sessions
 
     private
       def set_session
-        # @session = Session.create_with(platform_name: "Messenger").find_or_create_by(session_id: params[:messenger_user_id])
         @session = Session.create_or_return('Messenger', params[:messenger_user_id])
       end
   end
