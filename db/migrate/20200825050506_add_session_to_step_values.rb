db778922880e49d1a89698d4a72b56425fb51b85
radinreth <radin.reth@gmail.com>
2020-09-01 08:52:14 +0700

    migrate step value

--- db/migrate/20200825050506_add_session_to_step_values.rb
+++ db/migrate/20200825050506_add_session_to_step_values.rb
@@ -3,10 +3,25 @@ class AddSessionToStepValues < ActiveRecord::Migration[6.0]
     change_column_null :step_values, :message_id, true
     # TODO change null: false
     add_reference :step_values, :session, null: true, foreign_key: true
+
+    ActiveRecord::Base.transaction do
+      StepValue.where(session_id: nil).find_each do |step_value|
+        step_value.session_id = step_value.message_id
+        # step_value.message_id = nil
+        step_value.save
+      end
+    end
   end
 
   def down
     change_column_null :step_values, :message_id, false
     remove_reference :step_values, :session, null: true, foreign_key: true
+
+    ActiveRecord::Base.transaction do
+      StepValue.where.not(session_id: nil).find_each do |step_value|
+        step_value.message_id = step_value.session_id
+        step_value.save
+      end
+    end
   end
 end
