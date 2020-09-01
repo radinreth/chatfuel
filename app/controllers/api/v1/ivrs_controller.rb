0773837fe7f22504c878a21afe07b60c3966453e
radinreth <radin.reth@gmail.com>
2020-09-01 09:57:08 +0700

    wip add api/v1/chatbots

---
+++ app/controllers/api/v1/ivrs_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class IvrsController < SessionsController
+      private
+        def set_session
+          @session = Session.create_with(platform_name: "Verboice", callsid: params[:CallSid]).find_or_create_by(session_id: params[:address])
+        end
+    end
+  end
+end
