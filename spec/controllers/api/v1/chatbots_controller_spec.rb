59fcc1f84d47b0b6bb324876b86aa87014b52827
radinreth <radin.reth@gmail.com>
2020-09-01 10:44:11 +0700

    remove restrict access api sessions controller

--- spec/controllers/api/v1/chatbots_controller_spec.rb
+++ spec/controllers/api/v1/chatbots_controller_spec.rb
@@ -1,22 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::ChatbotsController, type: :controller do
-  
-
   describe "routes" do
     it { should route(:post, "/api/v1/chatbots").to(action: :create, format: :json) }
     it { should route(:post, "/api/v1/chatbots/mark_as_completed").to(action: :mark_as_completed, format: :json) }
-    # it { should route(:put, "/api/v1/me").to(action: :check, format: :json) }
-    # it { should route(:patch, "/api/v1/sites/0202").to(action: :update, site_code: "0202", format: :json)  }
   end
 
   describe "actions" do
-    let(:site) { create(:site) }
-
-    before do
-      request.headers["Authorization"] = "Bearer #{site.token}"
-    end
-
     describe "POST :create" do
       it "creates new record" do
         expect {
