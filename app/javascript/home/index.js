9517e619bbbb514f5aa97e5e48a918b3a051419f
radinreth <radin.reth@gmail.com>
2020-09-02 17:27:53 +0700

    show homepage kibana

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -2,6 +2,14 @@ OWSO.HomeIndex = (() => {
 
   function init() {
     OWSO.DashboardShow.renderDatetimepicker()
+    // attachEventClickToExpandedButton()
+  }
+
+  function attachEventClickToExpandedButton() {
+    $(".btn-collapse").click(function(e) {
+      e.preventDefault()
+      console.log(this)
+    })
   }
 
   return { init }
