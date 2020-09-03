0c1eb8fa92397ff02ed097d14174cfdc5db8bdcc
radinreth <radin.reth@gmail.com>
2020-09-03 08:51:23 +0700

    Rotate arrow

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -2,13 +2,13 @@ OWSO.HomeIndex = (() => {
 
   function init() {
     OWSO.DashboardShow.renderDatetimepicker()
-    // attachEventClickToExpandedButton()
+    attachEventClickToExpandedButton()
   }
 
   function attachEventClickToExpandedButton() {
     $(".btn-collapse").click(function(e) {
       e.preventDefault()
-      console.log(this)
+      $(this).toggleClass('fa-rotate-90')
     })
   }
 
