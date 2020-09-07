d99ba7901b1e8f1259ef77e5f2d3e34d11c00643
radinreth <radin.reth@gmail.com>
2020-09-07 14:00:52 +0700

    refactor

--- spec/models/step_value_spec.rb
+++ spec/models/step_value_spec.rb
@@ -6,7 +6,6 @@
 #  created_at        :datetime
 #  updated_at        :datetime
 #  message_id        :bigint(8)
-#  session_id        :bigint(8)
 #  site_id           :bigint(8)
 #  variable_id       :bigint(8)        not null
 #  variable_value_id :bigint(8)        not null
@@ -14,7 +13,6 @@
 # Indexes
 #
 #  index_step_values_on_message_id         (message_id)
-#  index_step_values_on_session_id         (session_id)
 #  index_step_values_on_site_id            (site_id)
 #  index_step_values_on_variable_id        (variable_id)
 #  index_step_values_on_variable_value_id  (variable_value_id)
@@ -22,7 +20,6 @@
 # Foreign Keys
 #
 #  fk_rails_...  (message_id => messages.id)
-#  fk_rails_...  (session_id => sessions.id)
 #  fk_rails_...  (site_id => sites.id)
 #  fk_rails_...  (variable_id => variables.id)
 #  fk_rails_...  (variable_value_id => variable_values.id)
