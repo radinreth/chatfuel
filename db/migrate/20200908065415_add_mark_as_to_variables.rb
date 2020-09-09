5e6cbf87b34ef8e2f6ecee2b7edd2034260c1e0d
radinreth <radin.reth@gmail.com>
2020-09-09 16:53:22 +0700

    add default marks_as

--- db/migrate/20200908065415_add_mark_as_to_variables.rb
+++ db/migrate/20200908065415_add_mark_as_to_variables.rb
@@ -1,6 +1,6 @@
 class AddMarkAsToVariables < ActiveRecord::Migration[6.0]
   def change
-    add_column :variables, :marks_as, :string, array: true
+    add_column :variables, :marks_as, :string, array: true, default: []
     add_index :variables, :marks_as, using: :gin
   end
 end
