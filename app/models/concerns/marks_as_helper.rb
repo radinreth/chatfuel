2c3374874118d476299b76c588d4324d5821e11b
radinreth <radin.reth@gmail.com>
2020-09-10 08:53:02 +0700

    Allows MarksAsHelper to find & check

--- app/models/concerns/marks_as_helper.rb
+++ app/models/concerns/marks_as_helper.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: rue
 
 module MarksAsHelper
+  extend ActiveSupport::Concern
+
   WHITELIST_MARKS_AS = %w(report most_request user_visit location ticket_tracking service_accessed)
 
   WHITELIST_MARKS_AS.each do |method|
-    define_method method.to_sym do
-      find_by("'#{method}' = ANY (marks_as)")
+    define_method "mark_as_#{method}?".to_sym do
+      marks_as.include? method
+    end
+  end
+
+  module ClassMethods
+    WHITELIST_MARKS_AS.each do |method|
+      define_method "mark_as_#{method}".to_sym do
+        find_by("'#{method}' = ANY (marks_as)")
+      end
     end
   end
 end
