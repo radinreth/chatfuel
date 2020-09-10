ea93999351f47b593896afc8f4c2c096160961bc
radinreth <radin.reth@gmail.com>
2020-09-10 11:51:20 +0700

    Refactor MarksAsHelper

--- app/models/concerns/marks_as_helper.rb
+++ app/models/concerns/marks_as_helper.rb
@@ -5,16 +5,21 @@ module MarksAsHelper
 
   WHITELIST_MARKS_AS = %w(report most_request user_visit location ticket_tracking service_accessed)
 
-  WHITELIST_MARKS_AS.each do |method|
-    define_method "mark_as_#{method}?".to_sym do
-      marks_as.include? method
+  WHITELIST_MARKS_AS.each do |item|
+    define_method "mark_as_#{item}?".to_sym do
+      self.marks_as.include? item
+    end
+
+    define_method "mark_as_#{item}!".to_sym do
+      marks_as << item
+      save
     end
   end
 
   module ClassMethods
-    WHITELIST_MARKS_AS.each do |method|
-      define_method "mark_as_#{method}".to_sym do
-        find_by("'#{method}' = ANY (marks_as)")
+    WHITELIST_MARKS_AS.each do |item|
+      define_method "mark_as_#{item}".to_sym do
+        find_by("'#{item}' = ANY (marks_as)")
       end
     end
   end
