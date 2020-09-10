120c43475ca59a7a2b757e651dc4f042d7575aed
radinreth <radin.reth@gmail.com>
2020-09-10 11:10:58 +0700

    Refactor variable model

--- app/models/variable_value.rb
+++ app/models/variable_value.rb
@@ -42,7 +42,7 @@ class VariableValue < ApplicationRecord
   end
 
   delegate :name, to: :variable, prefix: true
-  delegate :feedback_message?, to: :variable, prefix: false
+  delegate :mark_as_report?, to: :variable, prefix: false
   delegate :mark_as_location?, to: :variable, prefix: false
 
   private
