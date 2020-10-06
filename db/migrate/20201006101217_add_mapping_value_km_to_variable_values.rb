3fedfcc4bf2bb88bb687035ed92aa81a1a8c8de8
radinreth <radin.reth@gmail.com>
2020-10-06 17:47:13 +0700

    Add mapping_value_km form

--- db/migrate/20201006101217_add_mapping_value_km_to_variable_values.rb
+++ db/migrate/20201006101217_add_mapping_value_km_to_variable_values.rb
@@ -1,5 +1,6 @@
 class AddMappingValueKmToVariableValues < ActiveRecord::Migration[6.0]
   def change
+    # TODO: leave it blank or migrate from mapping_value_en
     add_column :variable_values, :mapping_value_km, :string, default: ""
   end
 end
