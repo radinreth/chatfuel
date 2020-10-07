c6a1dcb05d50ad5f249be13dc9ba5068a5678e5c
radinreth <radin.reth@gmail.com>
2020-10-07 11:59:43 +0700

    Update before_create callback & invoke task on migrate

--- db/migrate/20201006101217_add_mapping_value_km_to_variable_values.rb
+++ db/migrate/20201006101217_add_mapping_value_km_to_variable_values.rb
@@ -1,6 +1,10 @@
 class AddMappingValueKmToVariableValues < ActiveRecord::Migration[6.0]
-  def change
-    # TODO: leave it blank or migrate from mapping_value_en
+  def up
     add_column :variable_values, :mapping_value_km, :string, default: ""
+    Rake::Task["variable_value:migrate_raw_value_to_mapping_value_km"].execute
+  end
+
+  def down
+    remove_column :variable_values, :mapping_value_km
   end
 end
