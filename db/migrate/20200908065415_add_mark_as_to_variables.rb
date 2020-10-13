69204aa2fd5195a01841175b86ede0abdd44de1f
radinreth <radin.reth@gmail.com>
2020-10-13 17:55:08 +0700

    Change mark_as type

--- db/migrate/20200908065415_add_mark_as_to_variables.rb
+++ db/migrate/20200908065415_add_mark_as_to_variables.rb
@@ -1,6 +1,6 @@
 class AddMarkAsToVariables < ActiveRecord::Migration[6.0]
   def change
-    add_column :variables, :marks_as, :string, array: true, default: []
-    add_index :variables, :marks_as, using: :gin
+    add_column :variables, :mark_as, :string, default: ""
+    add_index :variables, :mark_as
   end
 end
