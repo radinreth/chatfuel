074751bfd17409bb78d254ca2e6aa109df93374d
radinreth <radin.reth@gmail.com>
2020-10-14 09:50:57 +0700

    add rake task to split mark_as

--- db/migrate/20200908065415_add_mark_as_to_variables.rb
+++ db/migrate/20200908065415_add_mark_as_to_variables.rb
@@ -1,6 +1,12 @@
 class AddMarkAsToVariables < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :variables, :mark_as, :string, default: ""
     add_index :variables, :mark_as
+    Rake::Task['variable:mark_as:merge'].invoke
+  end
+
+  def down
+    remove_column :variables, :mark_as
+    Rake::Task['variable:mark_as:split'].invoke
   end
 end
