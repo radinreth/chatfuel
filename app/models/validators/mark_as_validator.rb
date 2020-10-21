ea9d25ab0124656279047a4a855c81d5f854196f
radinreth <radin.reth@gmail.com>
2020-10-21 14:03:20 +0700

    Update mispelling uniq locale

--- app/models/validators/mark_as_validator.rb
+++ app/models/validators/mark_as_validator.rb
@@ -7,8 +7,9 @@ class MarkAsValidator
     return if mark_as.blank?
 
     mark_as_variable = Variable.send(mark_as.to_sym)
-
     @variable.errors.add(mark_as, I18n.t("variable.already_taken")) if mark_as_variable.present? && mark_as_variable != @variable
+  rescue
+    @variable.errors.add(:mark_as, I18n.t("variable.invalid_mark_as"))
   end
 
   private
