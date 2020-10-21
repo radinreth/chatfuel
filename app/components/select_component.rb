9641eb0eb7827ce12095dd17253395037f3194a2
radinreth <radin.reth@gmail.com>
2020-10-21 11:10:18 +0700

    Multi select dictionaries permission

--- app/components/select_component.rb
+++ app/components/select_component.rb
@@ -1,9 +1,10 @@
 class SelectComponent < ViewComponent::Base
-  def initialize(name:, value:, title: "", checked: false, disabled: false)
+  def initialize(name:, value:, input_type: "radio", title: "", checked: false, disabled: false)
     @name = name
     @value = value
     @title = title
     @checked = checked
     @disabled = disabled
+    @input_type = input_type
   end
 end
--- app/models/validators/mark_as_validator.rb
+++ app/models/validators/mark_as_validator.rb
@@ -6,7 +6,9 @@ class MarkAsValidator
   def validate
     return if mark_as.blank?
 
-    @variable.errors.add(mark_as, I18n.t("variable.already_taken")) if Variable.exists?(mark_as: mark_as)
+    mark_as_variable = Variable.send(mark_as.to_sym)
+
+    @variable.errors.add(mark_as, I18n.t("variable.already_taken")) if mark_as_variable.present? && mark_as_variable != @variable
   end
 
   private
