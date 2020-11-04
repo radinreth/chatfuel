2f02e55fb2924773833460aabbcd704a4d23dd59
radinreth <radin.reth@gmail.com>
2020-11-04 14:44:51 +0700

    Add basic public dashboard

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,10 +1,11 @@
 class WelcomesController < ApplicationController
-  layout 'welcome'
   skip_before_action :authenticate_user_with_guisso!
 
   def index
+    render layout: "welcome"
   end
 
   def show
+    render layout: "public_dashboard"
   end
 end
