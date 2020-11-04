691341ccc4299ede97f0cd3aa2181d1af573a0b0
radinreth <radin.reth@gmail.com>
2020-11-04 10:29:34 +0700

    Show all district names as tooltip

--- app/models/filters/district_filter.rb
+++ app/models/filters/district_filter.rb
@@ -12,6 +12,13 @@ class Filters::DistrictFilter < ::LocationFilter
     I18n.t("district_count", count: @codes.length)
   end
 
+  def names
+    @codes.map do |code|
+      district = Pumi::District.find_by_id(code)
+      district.send(label)
+    end.to_sentence
+  end
+
   private
     def district
       raise Filters::MultiDistrictException if @codes.length > 1
