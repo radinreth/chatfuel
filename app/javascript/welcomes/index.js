cfe4c86045d45d5b0e686f3e904aa5f62e0fa845
radinreth <radin.reth@gmail.com>
2020-11-05 15:26:19 +0700

    Add header sticky

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -1,7 +1,23 @@
 OWSO.WelcomesIndex = (() => {
+  var header
+  var logo
+  var sticky
 
   function init() {
     OWSO.DashboardShow.renderDatetimepicker()
+
+    header = document.getElementById("header")
+    logo = document.getElementById("logo")
+    sticky = header.offsetTop
+    window.onscroll = () => { stickOnScroll() }
+  }
+
+  function stickOnScroll() {
+    if(window.pageYOffset + logo.offsetHeight > sticky ) {
+      header.classList.add("fixed-top")
+    } else {
+      header.classList.remove("fixed-top")
+    }
   }
 
   return { init }
