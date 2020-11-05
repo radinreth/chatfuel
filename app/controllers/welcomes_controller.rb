26c8b2f14ad46a6ffcf7f2b476974af530237c67
radinreth <radin.reth@gmail.com>
2020-11-05 11:53:35 +0700

    Fix query date range

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,9 +1,8 @@
 class WelcomesController < ApplicationController
   skip_before_action :authenticate_user_with_guisso!
+  before_action :set_daterange
 
   def index
-    
-
     respond_to do |format|
       format.html { render layout: "welcome" }
       format.js do
@@ -12,4 +11,8 @@ class WelcomesController < ApplicationController
     end
   end
 
+  private
+    def default_start_date
+      Setting.dashboard_start_date.strftime('%Y/%m/%d')
+    end
 end
