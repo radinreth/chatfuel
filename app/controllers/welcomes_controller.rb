6a5bacca97d03e01b732147a2ec8c909ac9d2efe
radinreth <radin.reth@gmail.com>
2020-11-10 11:46:04 +0700

    Add visualize most requested services
    Mock sample data for public visualize

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -9,6 +9,8 @@ class WelcomesController < PublicAccessController
     render json: @query.access_info, status: :ok
   end
   def index
+    @query = DashboardQuery.new(filter_options)
+
     respond_to do |format|
       format.html { render layout: "welcome" }
       format.js
