712299d23cec52abc149a29bd37c443ea6aa289e
radinreth <radin.reth@gmail.com>
2020-11-11 17:48:46 +0700

    Fix specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe HomeController do
   describe "routes" do
-    it { should route(:get, "/").to(action: :index) }
+    it { should route(:get, "/home").to(action: :index) }
   end
 end
