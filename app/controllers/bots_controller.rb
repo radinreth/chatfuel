4de880660cd3606571fdd168dabe131b4ba40c40
radinreth <radin.reth@gmail.com>
2020-11-12 17:08:49 +0700

    Update children controller to private access controllers

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,4 +1,4 @@
-class BotsController < ApplicationController
+class BotsController < PrivateAccessController
   skip_before_action :authenticate_user_with_guisso!
   skip_before_action :verify_authenticity_token
 end
