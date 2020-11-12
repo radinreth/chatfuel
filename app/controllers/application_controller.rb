da5a8c78cad148979aa16483f74741ba3e2503ba
radinreth <radin.reth@gmail.com>
2020-11-12 14:03:11 +0700

    Separate controllers into public and private access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   include Pagy::Backend
   include Pundit
 
-  before_action :authenticate_user_with_guisso!
   before_action :set_raven_context
   before_action :switch_locale
 
   private
-    def user_not_authorized
-      flash[:alert] = t("not_authorized")
-      redirect_to(request.referrer || root_path)
-    end
-
     def switch_locale
       I18n.locale = params[:locale] || I18n.default_locale
     end
