f0a0a5c9aabd82468298d4d9edb8e863467c3fa5
radinreth <radin.reth@gmail.com>
2020-11-16 14:17:55 +0700

    Add static social share

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const coffee =  require('./loaders/coffee')
+const erb = require('./loaders/erb')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -10,4 +12,6 @@ environment.plugins.prepend('Provide',
     Popper: ['popper.js', 'default'],
   })
 )
+environment.loaders.prepend('erb', erb)
+environment.loaders.prepend('coffee', coffee)
 module.exports = environment
