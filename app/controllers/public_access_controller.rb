e3ca0cc3d9a778db0e878a0c7dfa0a7bfeddc31c
radinreth <radin.reth@gmail.com>
2020-11-18 09:07:53 +0700

    Skip guisso cookie check

--- app/controllers/public_access_controller.rb
+++ app/controllers/public_access_controller.rb
@@ -1,4 +1,7 @@
 class PublicAccessController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :check_guisso_cookie
+
   def current_user
     Guest.new
   end
