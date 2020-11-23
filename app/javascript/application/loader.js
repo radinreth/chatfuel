eab50dad25d280c4500bbe6bfd299a4adeb559ef
radinreth <radin.reth@gmail.com>
2020-11-23 13:51:35 +0700

    Add popup message modal

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -10,6 +10,10 @@ $( document ).on('turbolinks:load', function() {
   $('[data-toggle="popover"]').popover();
   OWSO.Util.closeAlert();
 
+  setTimeout(function(e) {
+    $("#popup").modal("show");
+  }, 5000)
+
   let currentPage = OWSO.Util.getCurrentPage();
   !!OWSO[currentPage] && OWSO[currentPage].init();
 })
