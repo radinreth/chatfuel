0a2f87cd5ba482f5664becc7294a863eec384d17
radinreth <radin.reth@gmail.com>
2020-11-23 17:30:28 +0700

    Init popup message only one time

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -1,4 +1,20 @@
 
+var initPopup = function() {
+  // Check if user saw the modal
+  var key = 'isPopMessageShown',
+  isPopMessageShown = localStorage.getItem(key);
+
+  // Show the modal only if new user
+  if (!isPopMessageShown) {
+    $('#popup').modal('show');
+  }
+
+  // If modal is displayed, store that in localStorage
+  $('#popup').on('shown.bs.modal', function () {
+    localStorage.setItem(key, true);
+  })
+}
+
 $( document ).on('turbolinks:load', function() {
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
