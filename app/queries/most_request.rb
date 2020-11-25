78a18858ec912fc1eb43e16043c7c659bd5db786
radinreth <radin.reth@gmail.com>
2020-11-25 17:30:49 +0700

    Move js options to setting

---
+++ app/queries/most_request.rb
@@ -0,0 +1,60 @@
+class MostRequest < Report
+  def result
+    @result = group_count
+    self
+  end
+
+  def transform
+    {
+      peak: peak,
+      label: I18n.t("dashboard.most_requested_services"),
+      colors: generate_colors,
+      data: tune_result
+    }
+  end
+
+  private
+
+  def generate_colors
+    return [] unless @result
+
+    super.take(@result.count)
+  end
+
+  def peak
+    @result.values.max
+  end
+
+  def tune_result
+    @result.each_with_object({}) do |(key, count), hash|
+      district, variable_value = find_objectes_by(key)
+      hash_key = district.send("name_#{I18n.locale}".to_sym)
+
+      hash[hash_key] = {
+        value: replace_new_line(variable_value.mapping_value),
+        count: count
+      } if !hash[hash_key] || hash[hash_key][:count] < count
+    end
+  end
+
+  def replace_new_line(str)
+    str.sub(/\s/, "\n")
+  end
+
+  def find_objectes_by(key)
+    district_id, variable_value_id = key
+
+    district = Pumi::District.find_by_id(district_id)
+    variable_value = VariableValue.find(variable_value_id)
+
+    [district, variable_value]
+  end
+
+  def group_count
+    @variable.step_values.joins(:message)\
+      .where.not(messages: { district_id: ["", "null"] })\
+      .group("messages.district_id")\
+      .group(:variable_value_id)\
+      .count
+  end
+end
