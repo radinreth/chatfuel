9dd58371f5eaead5836aa3af3b880f27c8d607c5
radinreth <radin.reth@gmail.com>
2020-11-25 17:30:49 +0700

    Move js options to setting

--- app/queries/report.rb
+++ app/queries/report.rb
@@ -1,56 +1,14 @@
 class Report
   def initialize(variable)
     @variable = variable
-  end
-
-  def result
-    @result = group_count
-    self
-  end
-
-  def transform
-    {
-      peak: peak,
-      data: tune_result
-    }
+    @colors = generate_colors
   end
 
   private
 
-  def peak
-    @result.values.max
-  end
-
-  def tune_result
-    @result.each_with_object({}) do |(key, count), hash|
-      district, variable_value = find_objectes_by(key)
-      hash_key = district.send("name_#{I18n.locale}".to_sym)
-
-      hash[hash_key] = {
-        value: replace_new_line(variable_value.mapping_value),
-        count: count
-      } if !hash[hash_key] || hash[hash_key][:count] < count
-    end
-  end
-
-  def replace_new_line(str)
-    str.sub(/\s/, "\n")
-  end
-
-  def find_objectes_by(key)
-    district_id, variable_value_id = key
-
-    district = Pumi::District.find_by_id(district_id)
-    variable_value = VariableValue.find(variable_value_id)
-
-    [district, variable_value]
-  end
-
-  def group_count
-    @variable.step_values.joins(:message)\
-      .where.not(messages: { district_id: ["", "null"] })\
-      .group("messages.district_id")\
-      .group(:variable_value_id)\
-      .count
+  def generate_colors
+    %w(#E25241 #60D29F #12A6A6 #C79553 #ffce56
+       #07F572 #F47E3D #DFC5F1 #4127B1 #E87CB9
+       #B953F6 #1D203F #BD2F28 #A3D1D8 #2F3559 )
   end
 end
