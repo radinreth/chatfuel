dca022328ad81d58136bc4a2f6fefbc9c89121ad
radinreth <radin.reth@gmail.com>
2020-11-26 10:52:45 +0700

    Add chart factory builder

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -4,7 +4,8 @@
   };
 }(jQuery));
 
-import * as defaults from '../data/defaults'
+
+import * as chart from '../factories/root_chart'
 
 OWSO.WelcomesIndex = (() => {
   var logoContainer, formQuery, pilotHeader
@@ -550,42 +551,9 @@ OWSO.WelcomesIndex = (() => {
   }
 
   function chartMostRequestedServices() {
-    let ctx = 'chart_most_requested_services'
-    let type = 'bar', plugins = [chartDataLabels]
+    let ctx = 'chart_most_requested_services';
 
-    let { label, colors, max, dataset } = gon.mostRequestedServices
-    let [labels, values] = [_.keys(dataset), _.values(dataset)]
-    let titles = _.map(values, el => el.value)
-    let counts = _.map(values, el => el.count)
-
-    let data = {
-      labels: labels,
-      datasets: [
-        {
-          ...defaults.initData,
-          label: label,
-          backgroundColor: colors,
-          dataTitles: titles,
-          data: counts,
-        }
-      ]
-    }
-
-    let options = {
-      ...defaults.initOptions,
-      scales: {
-        ...defaults.initOptions.scales,
-        yAxes: [{
-          display: true,
-          ticks: {
-            // extra margin needed to avoid clip label on bar chart
-            suggestedMax: (max + 200),
-          }
-        }]
-      }
-    }
-
-    new Chart(ctx, { type, plugins, data, options });
+    chart.mostRequest(ctx)
   }
 
   function onModalSave() {
