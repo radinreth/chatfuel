b139cf27e399375de4c2daa6521abfc055472aaf
radinreth <radin.reth@gmail.com>
2020-11-26 14:17:04 +0700

    Refactor welcomes controller

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,15 +2,9 @@ class WelcomesController < PublicAccessController
   include Filterable
 
   before_action :set_daterange
+  before_action :set_query, :set_gon
 
   def index
-    @query = DashboardQuery.new(filter_options)
-    
-    gon.all = I18n.t("all")
-    gon.locale = I18n.locale
-    gon.mostRequestedServices = @query.most_requested_services
-    gon.gender_info = @query.gender_info
-
     respond_to do |format|
       format.html { render layout: "welcome" }
       format.js
@@ -22,6 +16,21 @@ class WelcomesController < PublicAccessController
   end
 
   private
+    def set_query
+      @query = DashboardQuery.new(filter_options)
+    end
+
+    def set_gon
+      shared = {
+        all: I18n.t("all"),
+        locale: I18n.locale,
+        mostRequest: @query.most_requested_services,
+        genderInfo: @query.gender_info
+      }
+
+      gon.push(shared)
+    end
+
     def default_start_date
       Setting.dashboard_start_date.strftime('%Y/%m/%d')
     end
