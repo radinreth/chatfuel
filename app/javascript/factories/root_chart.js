3171ec888d990d3007aa1a8fdde25856e6346b56
radinreth <radin.reth@gmail.com>
2020-11-26 16:58:12 +0700

    Load dynamic chart access info by period(month)

--- app/javascript/factories/root_chart.js
+++ app/javascript/factories/root_chart.js
@@ -1,4 +1,5 @@
-import { MostRequestChart as mostRequest } from './most_request_chart'
-import { GenderInfoChart as genderInfo } from './information_access_by_gender_chart'
+import { MostRequestChart as mostRequest } from './most_request_chart';
+import { GenderInfoChart as genderInfo } from './information_access_by_gender_chart';
+import { AccessInfoChart as accessInfo } from './access_info_chart';
 
-export { mostRequest, genderInfo };
+export { mostRequest, genderInfo, accessInfo };
