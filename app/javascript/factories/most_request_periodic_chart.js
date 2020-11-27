0d694a1bf3defa89b3c19c0dc18d8f011aa26eb0
radinreth <radin.reth@gmail.com>
2020-11-27 10:54:47 +0700

    Load dynamic chart most requested by period (month)

---
+++ app/javascript/factories/most_request_periodic_chart.js
@@ -0,0 +1,50 @@
+import * as defaults from '../data/defaults'
+
+export const mostRequestPeriodic = (ctx) => {
+  let type = 'bar', 
+      plugins = [chartDataLabels];
+
+  let { label, colors, max, dataset } = gon.mostRequestPeriodic;
+  let [dataLabels, values] = [_.keys(dataset), _.values(dataset)];
+  let titles = _.map(values, el => el.value);
+  let counts = _.map(values, el => el.count);
+
+  let data = {
+    labels: dataLabels,
+    datasets: [
+      {
+        ...defaults.initData.datasets[0],
+        label: label,
+        backgroundColor: colors,
+        dataTitles: titles,
+        data: counts,
+      }
+    ]
+  };
+
+  let { scales } = defaults.initOptions
+
+  let options = {
+    ...defaults.initOptions,
+    scales: {
+      ...scales,
+      xAxes: [{
+        ...scales.xAxes,
+        ticks: {
+          ...scales.xAxes.ticks,
+          maxRotation: 0,
+          minRotation: 0,
+        }
+      }],
+      yAxes: [{
+        display: true,
+        ticks: {
+          // extra margin needed to avoid clip label on bar chart
+          suggestedMax: (max + 200),
+        }
+      }]
+    }
+  };
+
+  return new Chart(ctx, { type, plugins, data, options });
+}
