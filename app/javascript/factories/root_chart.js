b55e96c299b0992e0f89002fca1b7bd893a9b3db
radinreth <radin.reth@gmail.com>
2020-11-27 13:19:33 +0700

    Load ticket tracking by genders

--- app/javascript/factories/root_chart.js
+++ app/javascript/factories/root_chart.js
@@ -3,9 +3,11 @@ import { GenderInfoChart as genderInfo } from './information_access_by_gender_ch
 import { AccessInfoChart as accessInfo } from './access_info_chart';
 import { accessMainService } from './access_main_service_chart';
 import { mostRequestPeriodic } from './most_request_periodic_chart';
+import { ticketTrackingByGenders } from './ticket_tracking_by_genders_chart';
 
 export {  mostRequest, 
           genderInfo, 
           accessInfo,
           accessMainService,
-          mostRequestPeriodic };
+          mostRequestPeriodic,
+          ticketTrackingByGenders };
