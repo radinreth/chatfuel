252de0223bff998337fe75ed2902eb7f4739999c
radinreth <radin.reth@gmail.com>
2020-12-01 17:19:32 +0700

    Load feedback by sub categories by search locations

--- app/queries/feedback_sub_categories.rb
+++ app/queries/feedback_sub_categories.rb
@@ -5,8 +5,10 @@ class FeedbackSubCategories < Report
   end
 
   def transform
-    ["0204", "0212"].each_with_object({}) do |district_id, hash|
+    @query.options[:district_id].each_with_object({}) do |district_id, hash|
       hash[district_id] ||= {}
+      district = ::Pumi::District.find_by_id(district_id)
+      hash[district_id][:locationName] = district.send("name_#{I18n.locale}".to_sym)
       hash[district_id][:ratingLabels] = raw_dataset[district_id].keys
       hash[district_id][:dataset] = tuned_dataset(district_id)
     end
@@ -34,7 +36,7 @@ class FeedbackSubCategories < Report
 
       @result.each_with_object({}).with_index do |((key, count), hash), index|
         district_id, variable_id, value_id = key
-        district = ::Pumi::District.find_by_id(district_id)
+        
         variable = Variable.find(variable_id)
         variable_value = VariableValue.find(value_id)
 
@@ -46,7 +48,7 @@ class FeedbackSubCategories < Report
 
     def group_count
       StepValue.joins(:message)\
-        .where(messages: { district_id: ["0204", "0212"] })\
+        .where(messages: { district_id: @query.options[:district_id] })\
         .where(variable: [feedback_like, feedback_dislike])\
         .group("messages.district_id")\
         .group(:variable_id, :variable_value_id)\
