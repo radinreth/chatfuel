739b327ac8aab8bb52ef3798a6032aa9855c7fc2
kakada <kakada.chheang@gmail.com>
2020-12-01 17:07:41 +0700

    Fix broken creating session step value caused by no longer use message

--- app/models/concerns/step_value/trackable_concern.rb
+++ app/models/concerns/step_value/trackable_concern.rb
@@ -11,7 +11,6 @@ module StepValue::TrackableConcern
       t.site_code = variable_value.raw_value[0...4]
       t.status = variable_value.ticket_status
       t.tracking_datetime = created_at
-      t.message = message
       t.session = session
     end
   end
