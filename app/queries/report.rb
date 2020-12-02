851cc3b45b23ec4b8af3506dc587416aecb09675
radinreth <radin.reth@gmail.com>
2020-12-02 11:40:04 +0700

    \#4 piechart color contrast with text color

--- app/queries/report.rb
+++ app/queries/report.rb
@@ -16,8 +16,8 @@ class Report
   end
 
   def generate_colors
-    %w(#E25241 #60D29F #12A6A6 #C79553 #ffce56
+    %w(#E25241 #75038E #12A6A6 #C79553 #ffce56
        #07F572 #F47E3D #DFC5F1 #4127B1 #E87CB9
-       #B953F6 #1D203F #BD2F28 #A3D1D8 #2F3559 )
+       #B953F6 #1D203F #BD2F28 #0448FD #2F3559 )
   end
 end
