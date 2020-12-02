95a0d11174814e16996dce66fd6212641031c0f5
radinreth <radin.reth@gmail.com>
2020-12-02 13:14:27 +0700

    Fix tooltip not load after search

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -2,9 +2,10 @@
 var dev = () => location.hostname == "localhost"
 
 $( document ).on('turbolinks:load', function() {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
+  var $body = $('body');
   OWSO.Util.closeAlert();
+  $body.tooltip({ selector: '[data-toggle="tooltip"]' });
+  $body.popover({ selector: '[data-toggle="popover"]' });
   if(!dev()) $('#popup').modal('show');
 
   let currentPage = OWSO.Util.getCurrentPage();
