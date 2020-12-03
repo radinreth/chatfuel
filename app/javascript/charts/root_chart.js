97e63cd76c9ab4189968a1270f6e9b10aee126bd
radinreth <radin.reth@gmail.com>
2020-12-03 17:15:46 +0700

    Enhances js charts structures

--- app/javascript/charts/root_chart.js
+++ app/javascript/charts/root_chart.js
@@ -1,6 +1,6 @@
-import { MostRequestChart as mostRequest } from './most_request_chart';
-import { GenderInfoChart as genderInfo } from './information_access_by_gender_chart';
-import { AccessInfoChart as accessInfo } from './access_info_chart';
+import { mostRequest } from './most_request_chart';
+import { genderInfo } from './information_access_by_gender_chart';
+import { accessInfo } from './access_info_chart';
 import { accessMainService } from './access_main_service_chart';
 import { mostTrackedPeriodic } from './most_tracked_periodic_chart';
 import { ticketTrackingByGenders } from './ticket_tracking_by_genders_chart';
