c8e1a3e614df3d91b6b393aa6a58da6b0dce8803
radinreth <radin.reth@gmail.com>
2020-12-04 15:55:08 +0700

    Download charts

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -13,11 +13,43 @@ OWSO.WelcomesIndex = (() => {
   function init() {
     OWSO.DashboardShow.renderDatetimepicker();
     OWSO.DashboardShow.multiSelectDistricts();
+    attachEventClickToChartDownloadButton()
 
     onWindowScroll();
     onChangeDistrict();
     onModalSave();
     onClickTabNavigation();
+  }
+
+  function attachEventClickToChartDownloadButton() {
+    $(document).on("click", ".chart-dl", async function(e) {
+      e.preventDefault();
+
+      let target = $(e.currentTarget).data("target");
+      let fileName = target != undefined ? target.substring(1) : "my-chart";
+      let el = document.getElementById(fileName);
+      let area = el.getBoundingClientRect()
+
+      // related issue on github: https://github.com/niklasvh/html2canvas/issues/882
+      let canvas = await html2canvas(el, {
+        scrollX: 0,
+        scale: 2,
+        scrollY: -window.scrollY,
+        width: area.width,
+        height: area.height
+      });
+
+      download(canvas);
+    })
+  }
+
+  function download(canvas) {
+    let link = document.getElementById("link");
+
+    link.setAttribute('download', `chart.png`);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', canvas.toDataURL("image/png"));
+    link.click();
   }
 
   function onClickTabNavigation() {
