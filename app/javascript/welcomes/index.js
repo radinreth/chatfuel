04c74e1b0cfbbfdc39a49512fae9abd16597e8da
radinreth <radin.reth@gmail.com>
2020-12-04 08:31:09 +0700

    Refactor js

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -1,170 +1,108 @@
 require("../patches/jquery")
-import * as chart from '../charts/root_chart'
+import { renderChart } from '../charts/root_chart'
 
 OWSO.WelcomesIndex = (() => {
-  var logoContainer, formQuery, pilotHeader
+  let logoContainer, formQuery, pilotHeader;
 
   let items = [
     { element: ".q_province", fromClass: "col-lg-2", toClass: "col-lg-4" },
     { element: ".q_district", fromClass: "col-lg-3", toClass: "col-lg-4" },
     { element: ".q_time_period", fromClass: "col-lg-3", toClass: "col-lg-3" }
-  ]
+  ];
 
   function init() {
-    OWSO.DashboardShow.renderDatetimepicker()
-    OWSO.DashboardShow.multiSelectDistricts()
+    OWSO.DashboardShow.renderDatetimepicker();
+    OWSO.DashboardShow.multiSelectDistricts();
 
-    onWindowScroll()
-    onChangeDistrict()
-    onModalSave()
-    registerTab()
+    onWindowScroll();
+    onChangeDistrict();
+    onModalSave();
+    onClickTabNavigation();
   }
 
-  function registerTab() {
+  function onClickTabNavigation() {
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-      var target = $(e.target);
-      var active = $(target).attr('href');
+      let target = $(e.target);
+      let active = $(target).attr('href');
       $("#q_active_tab").val(active);
     })
-  }
-
-  function customChart() {
-    mostRequest()
-    genderInfo()
-    accessInfo()
-    accessMainService()
-    mostTrackedPeriodically()
-    ticketTrackingByGenders()
-
-    // citizen feedback
-    overallRating()
-    feedbackTrend()
-    feedbackSubCategories()
-  }
-
-  function feedbackSubCategories() {
-    $(".chart_feedback_by_sub_category").each(function(i, dom) {
-      chart.feedbackSubCategories(dom);
-    });
-  }
-
-  function feedbackTrend() {
-    var ctx = 'chart_owso_feedback_trend';
-    chart.feedbackTrend(ctx);
-  }
-
-  function overallRating() {
-    var ctx = 'chart_overall_rating_by_owso';
-    chart.overallRating(ctx);
-  }
-
-  function ticketTrackingByGenders() {
-    var ctx = 'chart_ticket_tracking_by_gender';
-    chart.ticketTrackingByGenders(ctx);
-  }
-
-  function mostTrackedPeriodically() {
-    var ctx = 'chart_most_service_tracked_periodically'
-    chart.mostTrackedPeriodic(ctx);
-  }
-
-  function accessMainService() {
-    var ctx = 'chart_number_access_by_main_services';
-    chart.accessMainService(ctx);
-  }
-
-  // access owso info by period { month, quater, semester }
-  function accessInfo() {
-    var ctx = 'chart_information_access_by_period';
-    chart.accessInfo(ctx);
-  }
-
-  function genderInfo() {
-    let ctx = 'chart_information_access_by_gender';
-    chart.genderInfo(ctx);
-  }
-
-  function mostRequest() {
-    let ctx = 'chart_most_requested_services';
-    chart.mostRequest(ctx)
   }
 
   function onModalSave() {
     $(".btn-save").click(function(e) {
       e.preventDefault();
-      var provinceCode = $('#province').val()
-      var districtCode = $('.district_code').val().filter( e => e)
+      let provinceCode = $('#province').val();
+      let districtCode = $('.district_code').val().filter( e => e);
+      let params = { 
+        province_code: provinceCode, 
+        locale: gon.locale,
+        district_code: districtCode 
+      };
 
       if( districtCode.length > 0 ) {
-        $.get("/welcomes/filter", 
-          { province_code: provinceCode, 
-            locale: gon.locale,
-            district_code: districtCode }, 
-          function(result) {
-          console.log(result)
-          $("#show-districts").text(result.display_name)
-          $("#q_districts").val(districtCode)
-          $(".tooltip-district")
-            .attr("data-original-title", result.district_list_name)
+        $.get("/welcomes/filter", params, function(result) {
+          $("#show-districts").text(result.display_name);
+          $("#q_districts").val(districtCode);
+          $(".tooltip-district").attr("data-original-title", result.district_list_name);
         })
       } else {
-        $("#show-districts").text(gon.all)
-        $("#q_districts").val("")
+        $("#show-districts").text(gon.all);
+        $("#q_districts").val("");
       }
 
-      $("#districtsModal").modal("hide")
+      $("#districtsModal").modal("hide");
     })
   }
 
   
 
   function onWindowScroll() {
-    formQuery = $("#form-query")
-    logoContainer = $("#logo-container")
-    pilotHeader = $("#piloting-header")
+    formQuery = $("#form-query");
+    logoContainer = $("#logo-container");
+    pilotHeader = $("#piloting-header");
     window.onscroll = () => { stickOnScroll() }
   }
 
   function onChangeDistrict() {
     $(document).on("change", "#district", function(e) {
       if( e.target.disabled ) {
-        $("#time_period, #btn-search").prop("disabled", true)
+        $("#time_period, #btn-search").prop("disabled", true);
       } else {
-        $("#time_period, #btn-search").prop("disabled", false)
+        $("#time_period, #btn-search").prop("disabled", false);
       }
     })
   }
 
   function stickOnScroll() {
     if(window.pageYOffset >= (logoContainer.outerHeight() + pilotHeader.offset().top) ) {
-      $(".logo-inline").show()
-      formQuery.addClass('highlight')
+      $(".logo-inline").show();
+      formQuery.addClass('highlight');
       $(".switch-lang").addClass('inc-top');
-      decreaseFormControlWidth()
+      decreaseFormControlWidth();
     } 
     
     if(window.pageYOffset < pilotHeader.offset().top) {
-      $(".logo-inline").hide()
-      formQuery.removeClass('highlight')
+      $(".logo-inline").hide();
+      formQuery.removeClass('highlight');
       $(".switch-lang").removeClass('inc-top');
-      increaseFormControlWidth()
+      increaseFormControlWidth();
     }
   }
 
   function decreaseFormControlWidth() {
     $.each(items, function(_, item) {
-      var { element, fromClass, toClass } = item
-      $(element).replaceClass(toClass, fromClass)
+      let { element, fromClass, toClass } = item;
+      $(element).replaceClass(toClass, fromClass);
     })
   }
 
   function increaseFormControlWidth() {
     $.each(items, function(_, item) {
-      var { element, fromClass, toClass } = item
-      $(element).replaceClass(fromClass, toClass)
+      let { element, fromClass, toClass } = item;
+      $(element).replaceClass(fromClass, toClass);
     })
   }
 
-  return { init, customChart }
+  return { init, renderChart }
 
 })()
