6aee2b8764c0f1474e3c88aba029a8e189f1c858
radinreth <radin.reth@gmail.com>
2020-12-08 14:01:51 +0700

    Prevent loading pidor popup on change language
    Pidor popup message should be shown only one time
    on the first page load.

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -1,12 +1,12 @@
 
-var dev = () => location.hostname == "localhost"
-
 $( document ).on('turbolinks:load', function() {
   var $body = $('body');
   OWSO.Util.closeAlert();
+  let { dev, freshSession } = OWSO.Util;
+  
   $body.tooltip({ selector: '[data-toggle="tooltip"]' });
   $body.popover({ selector: '[data-toggle="popover"]' });
-  if(!dev()) $('#popup').modal('show');
+  if( !dev() && freshSession() ) $('#popup').modal('show');
 
   let currentPage = OWSO.Util.getCurrentPage();
   !!OWSO[currentPage] && OWSO[currentPage].init();
