0732439810009dc80f42ec5094cf0effafd844e6
radinreth <radin.reth@gmail.com>
2020-12-21 14:07:21 +0700

    Refactor

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -55,7 +55,7 @@ OWSO.WelcomesIndex = (() => {
     let $spin = $(header.next().find(".loading")[0]);
 
     loading($spin);
-    chart = findChartInstance(option.canvasId)
+    let chart = findChartInstance(option.canvasId)
 
     $.get(option.url, serializedParams, function(result) {
       chart.data = option.extractor(result);
@@ -73,10 +73,8 @@ OWSO.WelcomesIndex = (() => {
     let chart
 
     Chart.helpers.each(Chart.instances, function (instance) {
-      if (instance.chart.canvas.id === id) {
-        chart = instance;
-        return
-      }
+      let { canvas } = instance.chart
+      if (canvas.id === id) chart = instance; return
     });
 
     return chart
