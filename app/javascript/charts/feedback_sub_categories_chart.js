c05db17777ca4a24e257c342ca16ae560bbf524f
radinreth <radin.reth@gmail.com>
2020-12-23 12:01:59 +0700

    Load sub categories visualize in modal

--- app/javascript/charts/feedback_sub_categories_chart.js
+++ app/javascript/charts/feedback_sub_categories_chart.js
@@ -1,44 +1,14 @@
-import * as defaults from '../data/defaults'
+import { setOptions } from "./feedback_sub_categories/utils"
 
-export const feedbackSubCategories = () => {
-  let type = 'bar', 
-      plugins = [chartDataLabels];
-
-  let { ratingLabels, dataset } = gon.feedbackSubCategories;
+export const feedbackSubCategories = (ctx, rs) => {
+  let type = 'bar', plugins = [chartDataLabels];
+  let { ratingLabels, dataset } = rs;
+  let options = setOptions(dataset);
 
   let data = {
     labels: ratingLabels,
     datasets: _.map(dataset, (el) => el)
   };
 
-  let { scales } = defaults.initOptions
-  let flatten = _.flatten(_.map(dataset, e => e.data))
-  let max = _.max(flatten)
-  let suggestedMax = Math.round( max * 1.25 )
-
-  let options = {
-    ...defaults.initOptions,
-    legend: {
-      display: true
-    },
-    scales: {
-      ...scales,
-      yAxes: [{
-        ...scales.yAxes[0],
-        ticks: {
-          beginAtZero: true,
-          suggestedMax: suggestedMax
-        }
-      }],
-      xAxes: [{
-        ...scales.xAxes[0],
-        ticks: {
-          maxRotation: 0,
-          minRotation: 0,
-        }
-      }]
-    }
-  };
-
-  return new Chart('chart_feedback_by_sub_category', { type, plugins, data, options });
+  return new Chart(ctx, { type, plugins, data, options });
 }
