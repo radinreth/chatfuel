19e82a255fa5ce862ff19bf1bdcd6629020f4c72
radinreth <radin.reth@gmail.com>
2021-01-04 15:55:37 +0700

    Update user visit each function chart to chartjs

---
+++ app/models/gonify.rb
@@ -0,0 +1,38 @@
+class Gonify
+  def initialize(query)
+    @query = query
+  end
+
+  def chart_data
+    access_info_chart.merge citizen_feedback_chart
+  end
+
+  def dashboard_data
+    {
+      totalUserByGender: @query.users_visited_by_each_genders,
+      totalUserVisitByCategory: @query.total_users_visit_by_category,
+      totalUserFeedback: @query.total_users_feedback,
+    }
+  end
+
+  private
+
+  def access_info_chart
+    {
+      mostRequest: @query.most_requested_services,
+      genderInfo: @query.gender_info,
+      accessInfo: @query.access_info,
+      accessMainService: @query.access_main_service,
+      mostTrackedPeriodic: @query.most_tracked_periodic,
+      ticketTrackingByGenders: @query.ticket_tracking_by_genders,
+    }
+  end
+
+  def citizen_feedback_chart
+    {
+      overallRating: @query.overall_rating,
+      feedbackTrend: @query.feedback_trend,
+      feedbackSubCategories: @query.feedback_sub_categories
+    }
+  end
+end
