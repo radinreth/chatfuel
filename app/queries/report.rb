9ca9a4d5d337ce618790e01cfd457b36624b8a8c
radinreth <radin.reth@gmail.com>
2021-01-04 11:19:18 +0700

    Enhance sub categories
    1. List selected district names
    2. Show province name

--- app/queries/report.rb
+++ app/queries/report.rb
@@ -39,4 +39,16 @@ class Report
   def districts
     @query.options['district_id'].map { |code| Pumi::District.find_by_id(code) }
   end
+
+  def location_filter
+    @location_filter = Filters::LocationFilter.new(province, districts)
+  end
+
+  def province
+    Pumi::Province.find_by_id(@query.options['province_id']) 
+  end
+
+  def districts
+    @query.options['district_id'].map { |code| Pumi::District.find_by_id(code) }
+  end
 end
