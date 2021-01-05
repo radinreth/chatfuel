d6f568afbd9fca83016725f8aa1df3a018f68f1a
radinreth <radin.reth@gmail.com>
2021-01-05 11:25:20 +0700

    Dashboard: update mark as variable not effect visualize
    Instead of raising error message when trying to set
    variable mark as that already been marked before, so that
    reset previously mark item, before mark a new one
    Example:
    
    Before:
      if A is mark as `user_visit`, if B try to mark as
        `user_visit`(the same as A) then it will raise.
    After:
      if B mark as `user_visit`, then it resets A.

--- app/models/concerns/mark_as_concern.rb
+++ app/models/concerns/mark_as_concern.rb
@@ -3,22 +3,26 @@
 module MarkAsConcern
   extend ActiveSupport::Concern
 
-  WHITELIST_MARK_AS_FEEDBACK = Array.wrap(Variable::FEEDBACK)
-  WHITELIST_MARK_AS_WITHOUT_FEEDBACK = %w(gender most_request user_visit location ticket_tracking service_accessed)
-  WHITELIST_MARK_AS = WHITELIST_MARK_AS_FEEDBACK + WHITELIST_MARK_AS_WITHOUT_FEEDBACK
+  MARK_AS_FEEDBACK = Array.wrap(Variable::FEEDBACK)
+  MARK_AS_MOST_REQUEST = Array.wrap(Variable::MOST_REQUEST)
+  MARK_AS_LIST = %w(gender user_visit location ticket_tracking service_accessed)
+  WHITELIST_MARK_AS = MARK_AS_FEEDBACK + MARK_AS_MOST_REQUEST + MARK_AS_LIST
 
   define_method :feedback? do self.mark_as == Variable::FEEDBACK end
   define_method :mark_as_feedback! do update(mark_as: Variable::FEEDBACK) end
+  define_method :most_request? do self.is_most_request == true end
+  define_method :mark_as_most_request! do update(is_most_request: true) end
 
-  WHITELIST_MARK_AS_WITHOUT_FEEDBACK.each do |item|
+  MARK_AS_LIST.each do |item|
     define_method "#{item}?".to_sym do self.mark_as == item end
     define_method "mark_as_#{item}!".to_sym do update(mark_as: item) end
   end
 
   module ClassMethods
     define_method :feedback do find_by(mark_as: Variable::FEEDBACK) end
+    define_method :most_request do find_by(is_most_request: true) end
 
-    WHITELIST_MARK_AS_WITHOUT_FEEDBACK.each do |item|
+    MARK_AS_LIST.each do |item|
       define_method "#{item}".to_sym do find_by(mark_as: item) end
     end
   end
