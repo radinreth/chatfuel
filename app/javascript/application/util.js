b7a7ed9a714bf88cbc27a310f8f028713b2c5f38
radinreth <radin.reth@gmail.com>
2021-01-06 16:40:29 +0700

    Fix feedback chart not show empty message

--- app/javascript/application/util.js
+++ app/javascript/application/util.js
@@ -1,4 +1,40 @@
+import { sum } from '../utils/array'
+
 OWSO.Util = {
+  chartReg() {
+    Chart.plugins.register({
+      afterDraw: function(chart) {
+        let { datasets } = chart.data
+
+        if (datasets && datasets[0].data && sum(datasets[0].data) === 0) {
+          var ctx = chart.chart.ctx;
+          var width = chart.chart.width;
+          var height = chart.chart.height
+          chart.clear();
+    
+          ctx.save();
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.font = "16px normal 'Helvetica Nueue'";
+          ctx.fillText(gon.no_data, width / 2, height / 2);
+          ctx.restore();
+        }
+      }
+    });
+  },
+  dev() { return location.hostname == "localhost" },
+
+  freshSession() {
+    if ( sessionStorage.getItem('fresh') ) return false;
+
+    return OWSO.Util.outdateFreshSession();
+  },
+
+  outdateFreshSession() {
+    sessionStorage.setItem('fresh', 1);
+    return true;
+  },
+
   capitalize(value) {
     return value.replace(/(^|\s)([a-z])/g, (m, p1, p2) => p1 + p2.toUpperCase());
   },
