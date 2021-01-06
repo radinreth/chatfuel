4c2977040db152a7fc7f7e61f84b7e8e1d401ebc
radinreth <radin.reth@gmail.com>
2021-01-06 15:01:36 +0700

    add total user feedback icon

--- app/queries/user_feedback.rb
+++ app/queries/user_feedback.rb
@@ -14,6 +14,15 @@ class UserFeedback < Report
   private
 
   def raw
-    StepValue.total_users_feedback(@query.options)
+    hash = StepValue.total_users_feedback(@query.options)
+    hash.transform_keys { |k| "#{icon_map[k]} #{k}" }
+  end
+
+  def icon_map
+    status_to_icon.each_with_object({}) { |(k,v), h| h[I18n.t(k)] = v }
+  end
+
+  def status_to_icon
+    { like: "😍", acceptable: "😊", dislike: "😞" }
   end
 end
