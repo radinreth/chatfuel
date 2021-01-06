9072130d55bb5e7e2772bca283a3fe2b71c63fe4
radinreth <radin.reth@gmail.com>
2021-01-06 17:05:09 +0700

    clean up gender partial

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,4 +7,12 @@ module DashboardHelper
       " &raquo; #{@criteria.mapping_value}".html_safe
     end
   end
+
+  def open_config_modal(target)
+    if policy(:dashboard).setting?
+      link_to "#", class: "ml-2", data: { toggle: "modal", target: target } do
+        content_tag :i, nil, class: "fas fa-cog cog"
+      end
+    end
+  end
 end
