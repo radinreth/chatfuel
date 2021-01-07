3e07d72a53eaae6caca9b14398591ac7a892b76d
radinreth <radin.reth@gmail.com>
2021-01-07 15:36:44 +0700

    Extract chart data formater

---
+++ app/javascript/data/formater.js
@@ -0,0 +1,9 @@
+import { accessInfo } from './formaters/accessInfoFormater'
+import { mostServiceTracked } from './formaters/mostServiceTrackedFormater'
+import { feedbackTrend } from './formaters/feedbackTrendFormater'
+
+export default {
+  accessInfo,
+  mostServiceTracked,
+  feedbackTrend
+}
