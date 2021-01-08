ce0da0049cf6cc00d64325ea9bd57b8f7a25d854
radinreth <radin.reth@gmail.com>
2021-01-08 14:08:14 +0700

    Rebase static website

--- app/queries/report.rb
+++ app/queries/report.rb
@@ -34,10 +34,6 @@ class Report
     @location_filter = Filters::LocationFilter.new(province, districts)
   end
 
-  def colors
-    Color.generate(dataset&.count.to_i)
-  end
-
   def province
     Pumi::Province.find_by_id(@query.options['province_id']) 
   end
@@ -45,4 +41,10 @@ class Report
   def districts
     @query.options['district_id'].map { |code| Pumi::District.find_by_id(code) }
   end
+
+  def colors
+    Color.generate(dataset&.count.to_i)
+  end
+
+  def dataset; raise 'must be implemented in subclass' end
 end
