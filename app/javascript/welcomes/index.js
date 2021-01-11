3b46ab403f983e2c07199a5a3de392504a8e7fd7
radinreth <radin.reth@gmail.com>
2021-01-11 14:33:07 +0700

    Refactor welcomes index js

--- app/javascript/welcomes/index.js
+++ app/javascript/welcomes/index.js
@@ -91,7 +91,7 @@ OWSO.WelcomesIndex = (() => {
     let $spin = $(header.next().find(".loading")[0]);
 
     loading($spin);
-    let chart = findChartInstance(canvasId)
+    let chart = OWSO.Util.findChartInstance(option.canvasId)
 
     $.get(url, serializedParams, function(result) {
       chart.data = extractor(result);
@@ -103,17 +103,6 @@ OWSO.WelcomesIndex = (() => {
 
       loaded($spin);
     }, "json");
-  }
-
-  function findChartInstance(id) {
-    let chart
-
-    Chart.helpers.each(Chart.instances, function (instance) {
-      let { canvas } = instance.chart
-      if (canvas.id === id) chart = instance; return
-    });
-
-    return chart
   }
 
   function loading(spin) {
@@ -209,6 +198,6 @@ OWSO.WelcomesIndex = (() => {
     })
   }
 
-  return { init, renderChart, loadSubCategories, findChartInstance }
+  return { init, renderChart, loadSubCategories }
 
 })()
