c7591e019b7686030e2e0c4514047e57938343b0
radinreth <radin.reth@gmail.com>
2021-01-11 17:57:41 +0700

    Fix social meta tag not render absolute path

--- app/models/social_meta_tag.rb
+++ app/models/social_meta_tag.rb
@@ -1,13 +1,11 @@
 class SocialMetaTag
-  extend ActionView::Helpers::AssetUrlHelper
-
   def self.to_meta_tags
     {
       og: {
         title:    'ទិន្នន័យការិយាល័យច្រកចេញចូលតែមួយ',
         type:     'website',
-        url:      'https://ow4c-dashboard-stg.ilabsea.org/',
-        image:    "#{ image_url("fb-sample-share-photo.png") }",
+        url:      ENV['STATIC_HOST'],
+        image:    "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['STATIC_HOST']) }",
         description: 'ដោយមានការអញ្ជើញចូលរួម ដោយលោក បួន ហេង ប្រធាននាយកដ្ឋានមុខងារ និងធនធាន នៃអគ្គនាយកដ្ឋានរដ្ឋបាលក្រសួងមហាផ្ទៃ និងលោក សុខ ថុល នាយករដ្ឋបាលសាលាខេត្តសៀមរាប។វគ្គបណ្តុះបណ្តាលនេះ មានរយៈពេលពេញមួយថ្ងៃ ដោយមានការចួលរួមពីអស់លោក លោកស្រី ប្រធាន អនុប្រធានអង្គ',
         site_name: 'InSTEDD iLab Southeast Asia'
       },
@@ -17,9 +15,14 @@ class SocialMetaTag
         title: "ទិន្នន័យការិយាល័យច្រកចេញចូលតែមួយ",
         description: 'ដោយមានការអញ្ជើញចូលរួម ដោយលោក បួន ហេង ប្រធាននាយកដ្ឋានមុខងារ និងធនធាន នៃអគ្គនាយកដ្ឋានរដ្ឋបាលក្រសួងមហាផ្ទៃ និងលោក សុខ ថុល នាយករដ្ឋបាលសាលាខេត្តសៀមរាប។វគ្គបណ្តុះបណ្តាលនេះ មានរយៈពេលពេញមួយថ្ងៃ ដោយមានការចួលរួមពីអស់លោក លោកស្រី ប្រធាន អនុប្រធានអង្គ',
         creator: "InSTEDD iLab Southeast Asia",
-        image: "#{ image_url("fb-sample-share-photo.png") }",
+        image: "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['STATIC_HOST']) }",
       },
       site: 'Welcome to OW4C InSTEDD iLab Southeast Asia'
     }
   end
+
+  private
+    def self.helper
+      ActionController::Base.helpers
+    end
 end
