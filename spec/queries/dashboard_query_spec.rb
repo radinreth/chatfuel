f5eb881cb7ab8255538de671b315eea7f268643e
radinreth <radin.reth@gmail.com>
2021-01-12 13:19:25 +0700

    Refactor overall rating report query

--- spec/queries/dashboard_query_spec.rb
+++ spec/queries/dashboard_query_spec.rb
@@ -116,69 +116,6 @@ RSpec.describe DashboardQuery.new do
     end
   end
 
-  describe "#gender_info" do
-    it "returns hash that include :colors, :dataset as keys" do
-      expect(subject.gender_info).to include(:colors, :dataset)
-    end
-  end
-
-  describe "#access_info" do
-    let(:variable) { build(:variable) }
-    let(:message) { build(:message, :text) }
-    let(:step_value) { create(:step_value, message: message, variable: variable) }
-
-    it "returns hash that include :colors, :dataset as keys" do
-      allow(Variable).to receive(:service_accessed).and_return(variable)
-      expect(subject.access_info).to include(:colors, :dataset)
-    end
-  end
-
-  describe "#access_main_service" do
-    context "WITHOUT service_accessed variable" do
-      it "returns empty hash" do
-        expect(subject.access_main_service).to eq({})
-      end
-    end
-
-    context "WITH service_accessed variable" do
-      let(:variable) { build(:variable) }
-
-      it "returns hash that include :colors, :dataset as keys" do
-        allow(Variable).to receive(:service_accessed).and_return(variable)
-        expect(subject.access_main_service).to include(:colors, :dataset)
-      end
-    end
-  end
-
-  describe "#most_tracked_periodic" do
-    it "returns hash that include :colors, :dataset, :label as keys" do
-      expect(subject.most_tracked_periodic).to include(:colors, :dataset, :label)
-    end
-  end
-
-  describe "#ticket_tracking_by_genders" do
-    it "returns hash that include :colors, :dataset as keys" do
-      expect(subject.ticket_tracking_by_genders).to include(:colors, :dataset)
-    end
-  end
-
-  describe "#feedback_trend" do
-    context "WITHOUT feedback variable" do
-      it "returns empty hash" do
-        expect(subject.feedback_trend).to eq({})
-      end
-    end
-
-    context "WITH feedback variable" do
-      let(:variable) { build(:variable) }
-
-      it "returns hash that include :labels, :dataset as keys" do
-        allow(Variable).to receive(:feedback).and_return(variable)
-        expect(subject.feedback_trend).to include(:labels, :dataset)
-      end
-    end
-  end
-
   describe "#overall_rating" do
     context "WITHOUT feedback variable" do
       it "returns empty hash" do
@@ -189,26 +126,9 @@ RSpec.describe DashboardQuery.new do
     context "WITH feedback variable" do
       let(:variable) { build(:variable) }
 
-      it "returns hash that include :labels, :dataset as keys" do
+      it "returns hash that include :ratingLabels, :dataset as keys" do
         allow(Variable).to receive(:feedback).and_return(variable)
-        expect(subject.overall_rating).to include(:labels, :dataset)
-      end
-    end
-  end
-
-  describe "#feedback_sub_categories" do
-    context "WITHOUT feedback variable" do
-      it "returns empty hash" do
-        expect(subject.feedback_sub_categories).to eq({})
-      end
-    end
-
-    xcontext "WITH feedback variable" do
-      let(:variable) { build(:variable) }
-
-      it "returns hash that include :labels, :dataset as keys" do
-        allow(Variable).to receive(:feedback).and_return(variable)
-        expect(subject.feedback_sub_categories).to include(:labels, :dataset)
+        expect(subject.overall_rating).to include(:ratingLabels, :dataset)
       end
     end
   end
