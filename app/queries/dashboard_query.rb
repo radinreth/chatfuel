078a30bbca2e21d9f4256f6d36da8a11c6c0fd0c
radinreth <radin.reth@gmail.com>
2021-01-12 16:50:25 +0700

    Move report related methods out of dashboard query

--- app/queries/dashboard_query.rb
+++ app/queries/dashboard_query.rb
@@ -9,57 +9,6 @@ class DashboardQuery
     @options.merge!(platform) if platform.present?
   end
 
-  def most_requested_services
-    most_request = Variable.most_request
-    most_requested_report = ::MostRequest.new(most_request, self)
-    most_requested_report.chart_options
-  rescue
-    {}
-  end
-
-  def gender_info
-    ::GenderInfo.new(nil, self).chart_options
-  end
-
-  def access_info
-    ::AccessInfo.new(nil, self).chart_options
-  end
-
-  def access_main_service
-    main_service = Variable.service_accessed
-    ::AccessMainService.new(main_service, self).chart_options
-  rescue
-    {}
-  end
-
-  def most_tracked_periodic
-    ::MostTrackedPeriodic.new(nil, self).chart_options
-  end
-
-  def ticket_tracking_by_genders
-    ::TicketTrackingByGenders.new(nil, self).chart_options
-  end
-
-  def overall_rating
-    feedback = Variable.feedback
-    ::OverallRating.new(feedback, self).chart_options
-  rescue
-    {}
-  end
-
-  def feedback_trend
-    feedback = Variable.feedback
-    ::FeedbackTrend.new(feedback, self).chart_options
-  rescue
-    {}
-  end
-
-  def feedback_sub_categories
-    categories_all.merge(categories_separate)
-  rescue
-    {}
-  end
-
   def user_count
     sessions.count
   end
@@ -97,10 +46,6 @@ class DashboardQuery
     Session.filter(@options)
   end
 
-  def total_users_visit_by_category
-    ::UserVisitEachFunction.new(nil, self).chart_options
-  end
-
   def total_users_visit_each_functions
     variable = Variable.user_visit
 
@@ -111,10 +56,6 @@ class DashboardQuery
     return {} if result.blank?
 
     default_chartjs_color_mapping.merge(result).transform_keys(&:humanize)
-  end
-
-  def users_by_genders
-    ::UserByGender.new(nil, self).chart_options
   end
 
   def users_visited_by_each_genders
@@ -146,11 +87,6 @@ class DashboardQuery
     key = "mapping_value_#{ I18n.locale }".to_sym
     user_visit.values.each { |val| default[val.send(key)] = 0 }
     default
-  end
-
-  def ticket_tracking
-    ticket_tracking_report = ::TicketTracking.new(nil, self)
-    ticket_tracking_report.chart_options
   end
 
   def number_of_tracking_tickets
