fcd3a7f6e189ada42f761f377e0bcdd804d7b3c6
radinreth <radin.reth@gmail.com>
2021-01-12 11:47:55 +0700

    Refactor most tracked by period report query

--- app/queries/most_tracked_periodic.rb
+++ app/queries/most_tracked_periodic.rb
@@ -1,41 +1,35 @@
 class MostTrackedPeriodic < Report
-  def result
-    @result = group_count
-    self
-  end
-
-  def transform
-    {
-      label: I18n.t("welcomes.most_requested_services"),
-      colors: colors,
-      dataset: dataset
-    }
+  def chart_options
+    super.merge(label, dataset: dataset)
   end
 
   private
+    def dataset
+      result_set.each_with_object({}) do |(key, count), hash|
+        date, sector = key
+        month = format_label(date)
 
-  def dataset
-    @result.each_with_object({}) do |(key, count), hash|
-      date, sector = key
-      month = format_label(date)
-
-      hash[month] = {
-        value: replace_new_line(sector),
-        count: count
-      } if !hash[month] || hash[month][:count] < count
+        hash[month] = {
+          value: replace_new_line(sector),
+          count: count
+        } if !hash[month] || hash[month][:count] < count
+      end
     end
-  end
 
-  def replace_new_line(str)
-    str.sub(/\s/, "\n")
-  end
+    def label
+      { label: I18n.t("welcomes.most_requested_services") }
+    end
 
-  def group_count
-    scope = Ticket.filter(@query.options)
-    scope = scope.joins(""" INNER JOIN trackings ON
-                            trackings.ticket_code = tickets.code""")
-    scope = scope.group_by_period(period, "trackings.created_at", format: "%b/%Y")
-    scope = scope.group(:sector)
-    scope.count
-  end
+    def replace_new_line(str)
+      str.sub(/\s/, "\n")
+    end
+
+    def result_set
+      scope = Ticket.filter(@query.options)
+      scope = scope.joins(""" INNER JOIN trackings ON
+                              trackings.ticket_code = tickets.code""")
+      scope = scope.group_by_period(period, "trackings.created_at", format: "%b/%Y")
+      scope = scope.group(:sector)
+      scope.count
+    end
 end
