9cc08ab0de004075543382602e261ba8cf4e28fb
radinreth <radin.reth@gmail.com>
2021-01-13 16:46:15 +0700

    Move #dataset, #colors, #labels to public

--- app/queries/user_feedback.rb
+++ app/queries/user_feedback.rb
@@ -9,12 +9,11 @@ class UserFeedback < BasicReport
   end
 
   private
+    def icon_map
+      status_to_icon.each_with_object({}) { |(k,v), h| h[I18n.t(k)] = v }
+    end
 
-  def icon_map
-    status_to_icon.each_with_object({}) { |(k,v), h| h[I18n.t(k)] = v }
-  end
-
-  def status_to_icon
-    { like: "😍", acceptable: "😊", dislike: "😞" }
-  end
+    def status_to_icon
+      { like: "😍", acceptable: "😊", dislike: "😞" }
+    end
 end
