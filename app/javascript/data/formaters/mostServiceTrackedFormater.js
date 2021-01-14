c1851365c0a3eb481b224bfa0d170d2e8fc9d630
radinreth <radin.reth@gmail.com>
2021-01-14 11:23:29 +0700

    Fix not reload gon data, rename chart labels

--- app/javascript/data/formaters/mostServiceTrackedFormater.js
+++ app/javascript/data/formaters/mostServiceTrackedFormater.js
@@ -1,16 +1,16 @@
 import * as defaults from '../defaults'
 
-export const mostServiceTracked = ({ label, colors, dataset }) => {
-  let [dataLabels, values] = [_.keys(dataset), _.values(dataset)];
+export const mostServiceTracked = ({ colors, dataset }) => {
+  let [labels, values] = [_.keys(dataset), _.values(dataset)];
   let titles = _.map(values, el => el.value);
   let counts = _.map(values, el => el.count);
 
   return {
-    labels: dataLabels,
+    labels: labels,
     datasets: [
       {
         ...defaults.initData.datasets[0],
-        label: label,
+        label: gon.most_tracked_label,
         backgroundColor: colors,
         dataTitles: titles,
         data: counts,
