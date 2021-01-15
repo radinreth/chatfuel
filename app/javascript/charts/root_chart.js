fcb48b4b49c0cf9b6d2fe9ca550bc112d8c84788
radinreth <radin.reth@gmail.com>
2021-01-15 13:58:33 +0700

    Add dynamic watermark total user visit chart

--- app/javascript/charts/root_chart.js
+++ app/javascript/charts/root_chart.js
@@ -7,7 +7,7 @@ import { ticketTrackingByGenders } from './ticket_tracking_by_genders_chart';
 import { overallRating } from './overall_rating_chart';
 import { feedbackTrend } from './feedback_trend_chart';
 import { feedbackSubCategories } from './feedback_sub_categories_chart';
-import { totalUserVisitByCategory } from './total_user_visit_by_category_chart';
+import { userVisit } from './total_user_visit_by_category_chart';
 import { totalUserFeedback } from './total_user_feedback_chart';
 import { totalUserByGender } from './total_user_by_gender_chart';
 
@@ -18,6 +18,8 @@ export const renderChart = function () {
 
   let ctx = 'chart_feedback_by_sub_category';
   feedbackSubCategories(ctx, gon.feedbackSubCategories);
+
+  userVisit.render();
 }
 
 const charts = [  mostRequest, 
@@ -28,6 +30,5 @@ const charts = [  mostRequest,
                   ticketTrackingByGenders,
                   overallRating,
                   feedbackTrend,
-                  totalUserVisitByCategory,
                   totalUserFeedback,
                   totalUserByGender ];
--- app/javascript/charts/total_user_visit_by_category_chart.js
+++ app/javascript/charts/total_user_visit_by_category_chart.js
@@ -1,49 +1,24 @@
 import { sum } from '../utils/array'
-import { labels } from '../data/piechart/defaults'
+import DonutChart from './donut_chart'
 
-export const totalUserVisitByCategory = () => {
-  let type = 'doughnut', 
-      plugins = [chartDataLabels];
+class TotalUserChart extends DonutChart {
+  chartId = "chart_total_user_visit";
 
-  let { colors, dataset } = gon.totalUserVisitByCategory;
-  let [categoryLabels, values] = [_.keys(dataset), _.values(dataset)];
+  dataset = () => {
+    let { colors, dataset } = gon.totalUserVisitByCategory;
+    let [labels, values] = [_.keys(dataset), _.values(dataset)];
 
-  let data = {
-    labels: categoryLabels,
-    total: sum(values),
-    datasets: [
-      {
-        backgroundColor: colors,
-        data: values,
-      }
-    ]
-  };
-
-  let options = {
-    legend: {
-      position: "left",
-      labels: labels
-    },
-    cutoutPercentage: 80,
-    plugins: {
-      datalabels: {
-        backgroundColor: function(context) {
-          return context.dataset.backgroundColor;
-        },
-        borderColor: 'white',
-        borderRadius: 100,
-        padding: 10,
-        borderWidth: 2,
-        color: 'white',
-        display: false,
-        font: {
-          family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-          weight: 'bold'
-        },
-        formatter: Math.round
-      }
-    }
-  };
-
-  new Chart('chart_total_user_visit', { type, plugins, data, options });
+    return {
+      labels: labels,
+      total: sum(values),
+      datasets: [
+        {
+          backgroundColor: colors,
+          data: values,
+        }
+      ]
+    };
+  }
 }
+
+export const userVisit = new TotalUserChart();
