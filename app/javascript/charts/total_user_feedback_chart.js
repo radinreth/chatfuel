44a627b1c34c0200d28cfbaa90d790926692e4db
radinreth <radin.reth@gmail.com>
2021-01-15 15:27:43 +0700

    Refactor BaseChart, Add dynamic watermark user feedback chart

--- app/javascript/charts/total_user_feedback_chart.js
+++ app/javascript/charts/total_user_feedback_chart.js
@@ -1,52 +1,29 @@
 import { sum } from '../utils/array'
-import { labels } from '../data/piechart/defaults'
+import DonutChart from './donut_chart'
 
-export const totalUserFeedback = () => {
-  let type = 'doughnut', 
-      plugins = [chartDataLabels];
+class UserFeedback extends DonutChart {
+  render = (opts) => {
+    let thiz = this;
+    $.each( $('.total_user_feedback'), function(_, ele) {
+      new Chart($(ele).attr("id"), thiz.config(opts));
+    });
+  }
 
-  let { colors, dataset } = gon.totalUserFeedback;
-  let [feedbackLabels, values] = [_.keys(dataset), _.values(dataset)];
+  dataset = () => {
+    let { colors, dataset } = gon.totalUserFeedback;
+    let [labels, values] = [_.keys(dataset), _.values(dataset)];
 
-  let data = {
-    labels: feedbackLabels,
-    total: sum(values),
-    datasets: [
-      {
-        backgroundColor: colors,
-        data: values,
-      }
-    ]
-  };
-
-  let options = {
-    legend: {
-      position: "left",
-      labels: labels
-    },
-    cutoutPercentage: 80,
-    plugins: {
-      datalabels: {
-        backgroundColor: function(context) {
-          return context.dataset.backgroundColor;
-        },
-        borderColor: 'white',
-        borderRadius: 100,
-        padding: 10,
-        borderWidth: 2,
-        color: 'white',
-        display: false,
-        font: {
-          family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-          weight: 'bold'
-        },
-        formatter: Math.round
-      }
-    }
-  };
-
-  $.each($('.total_user_feedback'), function(i, ele) {
-    let canvasId = $(ele).attr("id");
-    new Chart(canvasId, { type, plugins, data, options });
-  });
+    return {
+      labels: labels,
+      total: sum(values),
+      datasets: [
+        {
+          backgroundColor: colors,
+          data: values,
+        }
+      ]
+    };
+  }
 }
+
+export const userFeedback = new UserFeedback();
