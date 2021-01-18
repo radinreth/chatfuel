6f8a7f61a96b221a0967b17ac3f4cb3e0b408216
radinreth <radin.reth@gmail.com>
2021-01-18 16:52:51 +0700

    Remove CSRF check

--- spec/controllers/api/v1/social_shares_controller_spec.rb
+++ spec/controllers/api/v1/social_shares_controller_spec.rb
@@ -1,28 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::SocialSharesController do
-  context "WITH invalid TOKEN" do
-    before do
-      request.headers["Authorization"] = "Token token=123"
-    end
+  it "cannot create invalid site" do
+    post :create, params: { "social_share": { site_name: "invalid" } }
 
-    it "does not allow user to pass" do
-      post :create, params: { "social_share": { site_name: "facebook" } }
-
-      expect(response).to be_unauthorized
-    end
+    expect(response).not_to be_successful
   end
+  
+  it "creates a valid site" do
+    post :create, params: { "social_share": { site_name: "facebook" } }
 
-  context "WITH valid TOKEN" do
-    before do
-      request.session["_csrf_token"] = "abc123"
-      request.headers["Authorization"] = "Token token=abc123"
-    end
-
-    it "allows user to pass" do
-      post :create, params: { "social_share": { site_name: "facebook" } }
-
-      expect(response).to be_successful
-    end
+    expect(response).to be_successful
   end
 end
