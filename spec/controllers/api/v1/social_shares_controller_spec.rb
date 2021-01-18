3c19c98e12bc621b6761d48a971c0ee9442cf427
radinreth <radin.reth@gmail.com>
2021-01-18 15:36:20 +0700

    Add social share controller spec

---
+++ spec/controllers/api/v1/social_shares_controller_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::SocialSharesController do
+  context "WITH invalid TOKEN" do
+    before do
+      request.headers["Authorization"] = "Token token=123"
+    end
+
+    it "does not allow user to pass" do
+      post :create, params: { "social_share": { site_name: "facebook" } }
+
+      expect(response).to be_unauthorized
+    end
+  end
+
+  context "WITH valid TOKEN" do
+    before do
+      request.headers["Authorization"] = "Token token=abc123"
+    end
+
+    it "allows user to pass" do
+      post :create, params: { "social_share": { site_name: "facebook" } }
+
+      expect(response).to be_successful
+    end
+  end
+end
