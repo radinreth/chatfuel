6c2781e99c1ef74d9c2ecdb1086019d9b1152a97
radinreth <radin.reth@gmail.com>
2021-01-22 17:21:01 +0700

    Reset session column information

--- db/migrate/20201201064033_add_source_id_to_sessions.rb
+++ db/migrate/20201201064033_add_source_id_to_sessions.rb
@@ -1,7 +1,8 @@
 class AddSourceIdToSessions < ActiveRecord::Migration[6.0]
   def up
     add_column :sessions, :source_id, :string
-
+    
+    Session.reset_column_information
     Rake::Task["session:copy_session_id_to_source_id"].invoke
 
     change_column :sessions, :source_id, :string, null: false
