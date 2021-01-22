18cf44e1d0007cdf1cddd615042b1a2572218bd6
radinreth <radin.reth@gmail.com>
2021-01-22 09:12:55 +0700

    Rebase master

--- db/migrate/20201118102348_add_gender_to_session.rb
+++ db/migrate/20201118102348_add_gender_to_session.rb
@@ -1,5 +1,7 @@
 class AddGenderToSession < ActiveRecord::Migration[6.0]
   def change
-    add_column :sessions, :gender, :string, default: ""
+    unless column_exists?(:sessions, :gender)
+      add_column :sessions, :gender, :string, default: ""
+    end
   end
 end
