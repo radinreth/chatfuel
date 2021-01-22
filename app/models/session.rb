4ac4e8eae3685b1e5863d75ae45d8bbe9b941a33
radinreth <radin.reth@gmail.com>
2021-01-22 14:28:36 +0700

    Fix home duplicate filter

--- app/models/session.rb
+++ app/models/session.rb
@@ -108,16 +108,6 @@ class Session < ApplicationRecord
     scope
   end
 
-  def self.filter(params = {})
-    scope = all
-    scope = scope.where(session_type: params[:content_type]) if params[:content_type].present?
-    scope = scope.where(province_id: params[:province_id]) if params[:province_id].present?
-    scope = scope.where(district_id: params[:district_id]) if params[:district_id].present?
-    scope = scope.where(platform_name: params[:platform]) if params[:platform].present?
-    scope = scope.where("DATE(created_at) BETWEEN ? AND ?", params[:start_date], params[:end_date]) if params[:start_date].present? && params[:end_date].present?
-    scope
-  end
-
   private
     def update_last_interaction_time
       touch :last_interaction_at
