631452543c64fcfad6e1ce8d923de924725c5471
radinreth <radin.reth@gmail.com>
2021-01-25 15:25:21 +0700

    Update chatbot, ivr from show to create

--- spec/controllers/api/v1/ivr_tracks_controller_spec.rb
+++ spec/controllers/api/v1/ivr_tracks_controller_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.describe Api::V1::IvrTracksController, type: :controller do
   let!(:ticket) { create(:ticket, code: "1234-56789") }
-  
-  it "" do
-    get :show
+
+  it "response xml" do
+    post :create
 
     expect(response).to have_http_status(:ok)
+    expect(response.body).to include("xml")
   end
 end
