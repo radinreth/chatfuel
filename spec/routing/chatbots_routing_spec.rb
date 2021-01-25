313326a946c84fd5c57f66f3e63ce53b9fd6bfff
radinreth <radin.reth@gmail.com>
2021-01-25 10:49:18 +0700

    Add api/v1 routing specs

---
+++ spec/routing/chatbots_routing_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "routes for chatbots" do
+  it "routes to create dictionary" do
+    expect(post("/api/v1/chatbots")).to route_to("api/v1/chatbots#create", format: :json)
+  end
+
+  it "routes to preview map" do
+    expect(get("/api/v1/map_preview")).to route_to("api/v1/map_previews#show", format: :json)
+  end
+
+  it "routes to mark_as_completed" do
+    expect(post("/api/v1/chatbots/mark_as_completed")).to route_to("api/v1/chatbots#mark_as_completed", format: :json)
+  end
+end
