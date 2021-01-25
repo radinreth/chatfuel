831ab50da9436a3d16c94b3b69c3fcd722a761d2
radinreth <radin.reth@gmail.com>
2021-01-25 10:03:43 +0700

    Add session gender migration

--- db/migrate/20201021080039_add_gender_to_session.rb
+++ db/migrate/20201021080039_add_gender_to_session.rb
@@ -3,5 +3,13 @@ class AddGenderToSession < ActiveRecord::Migration[6.0]
     unless column_exists?(:sessions, :gender)
       add_column :sessions, :gender, :string, default: ""
     end
+
+    Session.reset_column_information
+    ActiveRecord::Base.connection.execute("
+      UPDATE sessions
+        SET gender = messages.gender
+        FROM messages
+        WHERE sessions.gender IS NULL AND
+              sessions.id = messages.id; ");
   end
 end
