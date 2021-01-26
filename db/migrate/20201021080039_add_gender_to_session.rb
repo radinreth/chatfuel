e288624189c8e806a4cf1883716b5dbfb857051e
radinreth <radin.reth@gmail.com>
2021-01-26 13:44:05 +0700

    Fix inconsistent total users by gender

--- db/migrate/20201021080039_add_gender_to_session.rb
+++ db/migrate/20201021080039_add_gender_to_session.rb
@@ -3,13 +3,5 @@ class AddGenderToSession < ActiveRecord::Migration[6.0]
     unless column_exists?(:sessions, :gender)
       add_column :sessions, :gender, :string, default: ""
     end
-
-    Session.reset_column_information
-    ActiveRecord::Base.connection.execute("
-      UPDATE sessions
-        SET gender = messages.gender
-        FROM messages
-        WHERE sessions.gender IS NULL AND
-              sessions.id = messages.id; ");
   end
 end
