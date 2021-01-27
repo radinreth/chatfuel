349ce0160b9d454eb888ddcaabf18d415eecff33
radinreth <radin.reth@gmail.com>
2021-01-27 14:14:55 +0700

    Add static website hint

--- app/models/social_provider.rb
+++ app/models/social_provider.rb
@@ -2,10 +2,10 @@
 #
 # Table name: social_providers
 #
-#  id         :bigint(8)        not null, primary key
-#  provider_name  :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint(8)        not null, primary key
+#  provider_name :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
 class SocialProvider < ApplicationRecord
   SOCIAL_PROVIDERS = %w(facebook twitter linkedin)
