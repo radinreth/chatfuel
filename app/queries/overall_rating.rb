38399eb187a3044038ebebd1153473b929c18d8a
radinreth <radin.reth@gmail.com>
2021-02-17 14:41:57 +0700

    Fix step value .filter inverted arguments

--- app/queries/overall_rating.rb
+++ app/queries/overall_rating.rb
@@ -1,8 +1,5 @@
 class OverallRating < Feedback
-<<<<<<< HEAD
-<<<<<<< HEAD
   def chart_options
-<<<<<<< HEAD
     mapping.each_with_object({}) do |(pro, districts), hash|
       hash[pro] ||= {}
       hash[pro][:labels] = districts.keys
@@ -35,67 +32,12 @@ class OverallRating < Feedback
       [pro_code] + super(key)
     end
 
-<<<<<<< HEAD
     def result_set
       scope = StepValue.filter(@query.options, @variable.step_values)
       scope = scope.joins(:session)
       scope = scope.where(sessions: { district_id: @query.district_codes_without_other })
       scope = scope.group("sessions.province_id")
       scope = scope.group("sessions.district_id")
-=======
-    def group_count
-=======
-    {
-      ratingLabels: result_set_mapping.keys,
-      dataset: dataset
-    }
-  end
-
-=======
->>>>>>> Review pull request
-  private
-    def labels
-      result_set_mapping.keys
-    end
-=======
-  def labels
-    result_set_mapping.keys
-  end
->>>>>>> Move #dataset, #colors, #labels to public
-
-  def dataset
-    @values = result_set_mapping.values
-
-    display_values.map do |values|
-      raw_value, mapping_value = values
-      {
-        label: mapping_value,
-        backgroundColor: colors_mapping[raw_value],
-        data: @values.map { |raw| raw[mapping_value] || 0 }
-      }
-    end
-  end
-
-  private
-    def result_set_mapping
-      return {} unless result_set
-
-      result_set.each_with_object({}).with_index do |((key, count), hash), index|
-        district, variable_value = find_objects_by(key)
-        location = district.send("name_#{I18n.locale}".to_sym)
-
-        hash[location] ||= {}
-        hash[location][variable_value.mapping_value] = count
-      end
-    end
-
-    def result_set
->>>>>>> Refactor overall rating report query
-      scope = StepValue.filter(@variable.step_values, @query.options)
-      scope = scope.joins(:message)
-      scope = scope.where(messages: { district_id: @query.district_codes_without_other })
-      scope = scope.group("messages.district_id")
->>>>>>> Filter other location
       scope = scope.group(:variable_value_id)
       scope.count
     end
