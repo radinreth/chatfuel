c95b70326da9c91f4076e7204104c5665055c444
radinreth <radin.reth@gmail.com>
2021-02-17 10:36:07 +0700

    Enhance map preview URL follows REST
    Update: /api/v1/map_preview => /api/v1/sites/:site_code/map

---
+++ app/controllers/concerns/previewable.rb
@@ -0,0 +1,50 @@
+module Previewable
+  extend ActiveSupport::Concern
+
+  included do
+    skip_before_action :restrict_access
+    before_action :set_site, only: :map
+  end
+
+  def map
+    if @site.present?
+      render json: { messages: [ gallery ] }, status: :ok
+    else
+      head :ok
+    end
+  end
+
+  private
+    def set_site
+      @site = Site.find_by(code: params[:site_code])
+    end
+
+    # ref: https://docs.chatfuel.com/en/articles/735122-json-api
+    def gallery
+      {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            image_aspect_ratio: "horizontal",
+            elements: [ map_element ]
+          }
+        }
+      }
+    end
+
+    def map_element
+      {
+        title: I18n.t("chatfuel.map.title", district: @site.name_km),
+        subtitle: I18n.t("chatfuel.map.subtitle"),
+        image_url: helpers.asset_url("google-map-image-sample.png"),
+        buttons: [
+          {
+            type: "web_url",
+            url: @site.map_url,
+            title: I18n.t("chatfuel.map.button_title")
+          }
+        ]
+      }
+    end
+end
