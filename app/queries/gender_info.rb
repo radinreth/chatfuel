ec9457c655744248fd0b2fe7d19ae1af666c2299
radinreth <radin.reth@gmail.com>
2021-02-19 13:44:36 +0700

    Update gender info dataset

--- app/queries/gender_info.rb
+++ app/queries/gender_info.rb
@@ -4,14 +4,17 @@ class GenderInfo < BasicReport
   end
 
   def dataset
-    genders ||= @query.unique_by_genders
-
-    return {} unless genders.present?
-
-    genders.inject({}) do |memo, result|
-      gender = @query.mapping_variable_value[result.gender]
-      memo[gender.mapping_value] = result.gender_count if gender.present?
-      memo
+    sql.each_with_object({}) do |(raw_gender, count), gender|
+      mapping_gender = @query.mapping_variable_value[raw_gender].mapping_value
+      gender[mapping_gender] = count
     end
+  end
+
+  def sql
+    scope = StepValue.filter(@query.options)
+    scope = scope.where(variable_value: VariableValue.type_of_user_feedback)
+    scope = scope.joins(:session)
+    scope = scope.group(:gender)
+    scope.count
   end
 end
