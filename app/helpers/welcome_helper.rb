304ee2bf7f0f610356f58e7ba131111c11af9686
radinreth <radin.reth@gmail.com>
2021-02-23 13:37:00 +0700

    Add information access

---
+++ app/helpers/welcome_helper.rb
@@ -0,0 +1,17 @@
+module WelcomeHelper
+  # def t_name
+  #   "full_name_#{I18n.locale}".to_sym
+  # end
+
+  # def district(district_id)
+  #   Pumi::District.find_by_id(district_id)
+  # end
+
+  def show_location_detail
+    full_district_list_name ? @location_filter.display_name : I18n.t("dashboard.all_district")
+  end
+
+  def full_district_list_name
+    @location_filter.full_district_list_name.presence
+  end
+end
