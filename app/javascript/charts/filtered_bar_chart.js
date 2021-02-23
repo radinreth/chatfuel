7d8d18a08cc0b6c74ef2894171993044c696b7db
radinreth <radin.reth@gmail.com>
2021-02-23 16:30:41 +0700

    Refactor most tracked chart

---
+++ app/javascript/charts/filtered_bar_chart.js
@@ -0,0 +1,22 @@
+import BarChart from './bar_chart'
+import { extract } from '../utils'
+
+class FilteredBarChart extends BarChart {
+  ancestor = new BarChart();
+
+  dataTitles = (data) => _.map(data.values, el => el.value);
+  dataCounts = (data) => _.map(data.values, el => el.count);
+  format = (ds) => {
+    let data = extract(ds);
+    return {  labels: _.keys(ds.dataset),
+              datasets: [
+                { ...this.ancestor.dataFormat(),
+                  backgroundColor: data.colors,
+                  dataTitles: this.dataTitles(data),
+                  data: this.dataCounts(data) }
+              ]
+            };
+  }
+}
+
+export default FilteredBarChart
