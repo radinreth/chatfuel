966bab6ad28be04cd55085cd26623f5747e3b838
radinreth <radin.reth@gmail.com>
2021-02-24 11:20:09 +0700

    Refactor feedback sub categories

---
+++ app/javascript/charts/citizen-feedback/feedback_sub_categories_chart.js
@@ -0,0 +1,12 @@
+import GroupBarChart from '../group_bar_chart'
+
+class SubCategoriesFeedbackChart extends GroupBarChart {
+  chartId = 'chart_feedback_by_sub_category';
+
+  format = () => {
+    let { labels, dataset } = this.ds || gon.feedbackSubCategories;
+    return { labels, datasets: _.map(dataset, (el) => el) };
+  }
+}
+
+export const subCategoriesFeedback = new SubCategoriesFeedbackChart();
