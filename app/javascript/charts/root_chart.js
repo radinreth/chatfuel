2683079b6ba62bedcd402284bbc8643c5dd8b34e
radinreth <radin.reth@gmail.com>
2021-02-24 10:33:52 +0700

    Add group bar chart

--- app/javascript/charts/root_chart.js
+++ app/javascript/charts/root_chart.js
@@ -1,11 +1,16 @@
-import { mostRequest } from './most_request_chart';
-import { genderInfo } from './information_access_by_gender_chart';
-import { accessInfo } from './access_info_chart';
-import { accessMainService } from './access_main_service_chart';
-import { mostTrackedPeriodic } from './most_tracked_periodic_chart';
-import { ticketTrackingByGenders } from './ticket_tracking_by_genders_chart';
-import { overallRating } from './overall_rating_chart';
-import { feedbackTrend } from './feedback_trend_chart';
+// OWSO Information Accessed
+import { mostRequestServiceAccess } from './owso-information-accessed/most_request_service_access_chart';
+import { informationAccess } from './owso-information-accessed/access_info_chart';
+import { mainServiceAccess } from './owso-information-accessed/access_main_service_chart';
+import { mostPopularAccess } from './owso-information-accessed/most_tracked_periodic_chart';
+import { ticketTrackingAccess } from './owso-information-accessed/ticket_tracking_by_genders_chart';
+
+// Citizen Feedback
+import { genderFeedback } from './citizen-feedback/gender_feedback_chart';
+import { overallFeedback } from './citizen-feedback/overall_rating_chart';
+import { trendingFeedback } from './citizen-feedback/feedback_trend_chart';
+
+
 import { feedbackSubCategories } from './feedback_sub_categories_chart';
 
 // ow4c charts
@@ -16,21 +21,28 @@ import { userFeedback } from './total_user_feedback_chart';
 export const renderChart = function () {
   OWSO.Util.chartReg();
 
-  $.each( charts, function(_, f) { f() });
+  // $.each( charts, function(_, f) { f() });
 
-  let ctx = 'chart_feedback_by_sub_category';
-  feedbackSubCategories(ctx, gon.feedbackSubCategories);
+  // let ctx = 'chart_feedback_by_sub_category';
+  // feedbackSubCategories(ctx, gon.feedbackSubCategories);
 
-  userVisit.render();
-  userGender.render();
-  userFeedback.render();
+  // public dashboard
+  // -- OWSO Information Accessed
+  // mostRequestServiceAccess.render();
+  // informationAccess.render();
+  // mainServiceAccess.render();
+  // mostPopularAccess.render();
+  // ticketTrackingAccess.render();
+
+  // -- citizen feedback
+  // genderFeedback.render();
+  // overallFeedback.render();
+  // trendingFeedback.render();
+
+  // tech tool dashboard
+  // userVisit.render();
+  // userGender.render();
+  // userFeedback.render();
 }
 
-const charts = [  mostRequest, 
-                  genderInfo, 
-                  accessInfo,
-                  accessMainService,
-                  mostTrackedPeriodic,
-                  ticketTrackingByGenders,
-                  overallRating,
-                  feedbackTrend];
+// const charts = [  feedbackTrend];
