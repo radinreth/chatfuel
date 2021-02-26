3478f97c1e0201a4be0da4838fa0dc2cb1a08162
radinreth <radin.reth@gmail.com>
2021-02-26 14:25:40 +0700

    Add overall rating

---
+++ app/javascript/charts/citizen-feedback/overall_rating_chart.js
@@ -0,0 +1,13 @@
+import GroupBarChart from '../group_bar_chart'
+
+class OverallFeedbackChart extends GroupBarChart {
+  chartId = 'chart_overall_rating_by_owso';
+
+  format = () => {
+    let { labels, dataset } = gon.overallRating;
+
+    return { labels, datasets: _.map(dataset, (el) => el) };
+  }
+}
+
+export const overallFeedback = new OverallFeedbackChart();
