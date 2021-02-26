8d561fe1c7b406fcaf8ffc557866854cec310943
radinreth <radin.reth@gmail.com>
2021-02-26 15:51:59 +0700

    Fix feedback sub categories popup

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,11 +1,11 @@
 module WelcomeHelper
-  # def t_name
-  #   "full_name_#{I18n.locale}".to_sym
-  # end
+  def t_name
+    "full_name_#{I18n.locale}".to_sym
+  end
 
-  # def district(district_id)
-  #   Pumi::District.find_by_id(district_id)
-  # end
+  def district(district_id)
+    Pumi::District.find_by_id(district_id)
+  end
 
   def show_location_detail
     full_district_list_name ? @location_filter.display_name : I18n.t("dashboard.all_district")
---
+++ app/queries/feedback_sub_categories.rb
@@ -0,0 +1,28 @@
+class FeedbackSubCategories < FeedbackReport
+  def labels
+    result_set_mapping["all"].keys
+  end
+  
+  def dataset
+    values.map.with_index do |mapping_value, index|
+      dataset_item(mapping_value, index, data_values)
+    end
+  end
+
+  def colors
+    Color.generate(values.count)
+  end
+
+  private
+    def data_values
+      result_set_mapping["all"].values
+    end
+
+    def result_set_mapping
+      accumulate_rating_each_variable(result_set)
+    end
+
+    def result_set
+      sql.count
+    end
+end
