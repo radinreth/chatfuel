0f268554f933c2732e7bb0c2487e115211bf9ab4
radinreth <radin.reth@gmail.com>
2021-03-02 09:35:49 +0700

    Refactor charts rendering call

---
+++ app/javascript/charts/citizen-feedback/index.js
@@ -0,0 +1,13 @@
+import { genderFeedback } from './gender_feedback_chart'
+import { overallFeedback } from './overall_rating_chart'
+import { trendingFeedback } from './feedback_trend_chart'
+import { subCategoriesFeedback } from './feedback_sub_categories_chart'
+
+export const citizenFeedback = {
+  render: function() {
+    genderFeedback.render()
+    overallFeedback.render()
+    trendingFeedback.render()
+    subCategoriesFeedback.render()
+  }
+}
