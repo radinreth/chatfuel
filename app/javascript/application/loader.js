0ca662c9c380ff08245aa1815757bc974d54d26d
radinreth <radin.reth@gmail.com>
2021-03-02 13:59:14 +0700

    Auto close alert

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -8,6 +8,7 @@
 $( document ).on('turbolinks:load', function() {
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
+  OWSO.Util.closeAlert();
 
   let currentPage = OWSO.Util.getCurrentPage();
   !!OWSO[currentPage] && OWSO[currentPage].init();
