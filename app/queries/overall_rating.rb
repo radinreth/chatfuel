3ff9891825346ce9192f64f967a7cb1a62f2d110
radinreth <radin.reth@gmail.com>
2021-03-03 16:06:19 +0700

    Add feedback overall rating multiple provinces

--- app/queries/overall_rating.rb
+++ app/queries/overall_rating.rb
@@ -1,31 +1,28 @@
 class OverallRating < Feedback
-  def labels
-    result_set_mapping.keys
-  end
-
-  def dataset
-    @values = result_set_mapping.values
-
-    display_values.map do |values|
-      raw_value, mapping_value = values
-      {
-        label: mapping_value,
-        backgroundColor: colors_mapping[raw_value],
-        data: @values.map { |raw| raw[mapping_value] || 0 }
-      }
+  def chart_options
+    mapping.each_with_object({}) do |(pro, districts), hash|
+      hash[pro] ||= {}
+      hash[pro][:labels] = districts.keys
+      hash[pro][:dataset] = display_values.map do |values|
+            id, raw_value, mapping_value = values
+            {
+              label: mapping_value,
+              backgroundColor: colors_mapping[raw_value],
+              data: districts.values.map { |raw| raw[id] || 0 }
+            }
+          end
     end
   end
 
   private
-    def result_set_mapping
-      return {} unless result_set
-
-      result_set.each_with_object({}).with_index do |((key, count), hash), index|
-        district, variable_value = find_objects_by(key)
-        location = district.send("name_#{I18n.locale}".to_sym)
-
-        hash[location] ||= {}
-        hash[location][variable_value.mapping_value] = count
+    
+    def mapping
+      result_set.each_with_object({}) do |(key, count), hash|
+        pro, dis, var = key
+      
+        hash[pro] ||= {}
+        hash[pro][dis] ||= {}
+        hash[pro][dis][var] ||= count
       end
     end
 
@@ -33,6 +30,7 @@ class OverallRating < Feedback
       scope = StepValue.filter(@query.options, @variable.step_values)
       scope = scope.joins(:session)
       scope = scope.where(sessions: { district_id: @query.district_codes_without_other })
+      scope = scope.group("sessions.province_id")
       scope = scope.group("sessions.district_id")
       scope = scope.group(:variable_value_id)
       scope.count
