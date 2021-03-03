bb966e49e49f28bd701f501cc867d3382b84145a
radinreth <radin.reth@gmail.com>
2021-03-03 17:58:51 +0700

    Update overall rating ticks options & localize labels

--- app/javascript/charts/citizen-feedback/overall_rating_chart.js
+++ app/javascript/charts/citizen-feedback/overall_rating_chart.js
@@ -2,9 +2,18 @@ import GroupBarChart from '../group_bar_chart'
 
 class OverallFeedbackChart extends GroupBarChart {
   chartId = 'chart_overall_rating_by_owso';
+  ticksOptions = {
+    maxRotation: 45,
+    minRotation: 45,
+    callback: function(value) {
+      let maxLength = 10,
+          ellipsisValue = `${value.substr(0, 10)}...`;
+      return (value.length >= maxLength) ? ellipsisValue : value;
+    }
+  }
 
   format = () => {
-    let { labels, dataset } = gon.overallRating;
+    let { labels, dataset } = this.ds;
 
     return { labels, datasets: _.map(dataset, (el) => el) };
   }
