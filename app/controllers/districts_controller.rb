9aedaae0afe5a6bee07271782887937e149d4673
radinreth <radin.reth@gmail.com>
2021-03-09 11:40:02 +0700

    Fix public dashboard filter districts under selected province

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -10,6 +10,12 @@ class DistrictsController < Pumi::DistrictsController
     def filter_districts
       query = DashboardQuery.new(filter_options)
 
-      query.district_codes_without_other.map { |id| Pumi::District.find_by_id(id) }
+      (district_codes & query.district_codes_without_other).map do |id|
+        Pumi::District.find_by_id(id)
+      end
+    end
+
+    def district_codes
+      Pumi::District.where(province_id: params[:province_id]).map(&:id)
     end
 end
