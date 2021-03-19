ee3da98db0dababa74b4369dfa99660b9bbf8450
radinreth <radin.reth@gmail.com>
2021-03-19 10:17:17 +0700

    Move feedback trend under province

--- app/javascript/charts/citizen-feedback/index.js
+++ app/javascript/charts/citizen-feedback/index.js
@@ -1,9 +1,9 @@
 import { genderFeedback } from './gender_feedback_chart'
-import { trendingFeedback } from './feedback_trend_chart'
+// import { trendingFeedback } from './feedback_trend_chart'
 
 export const citizenFeedback = {
   render: function() {
     genderFeedback.render()
-    trendingFeedback.render()
+    // trendingFeedback.render()
   }
 }
