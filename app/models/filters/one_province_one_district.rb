27b8a3bc8ff61b26b524a77ed62622e497d93773
radinreth <radin.reth@gmail.com>
2021-03-22 18:21:51 +0700

    Rebase latest feedback trend detail

--- app/models/filters/one_province_one_district.rb
+++ app/models/filters/one_province_one_district.rb
@@ -1,15 +1,7 @@
 module Filters
   class OneProvinceOneDistrict < ProvinceDistrict
     def display_name
-<<<<<<< HEAD
       district.send(name_i18n)
-=======
-      district.name_en
-    end
-    
-    def described_name
-      display_name
->>>>>>> multi provinces
     end
   end
 end
