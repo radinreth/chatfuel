37da1f1e6d1cf929bc7c41c09d0965375e9f02ae
radin reth <radin.reth@gmail.com>
2021-04-05 18:57:37 +0700

    Add missing changes

--- app/queries/user_feedback.rb
+++ app/queries/user_feedback.rb
@@ -10,11 +10,11 @@ class UserFeedback < BasicReport
 
   private
 
-  def icon_map
-    status_to_icon.each_with_object({}) { |(k,v), h| h[I18n.t(k)] = v }
-  end
+    def icon_map
+      status_to_icon.each_with_object({}) { |(k,v), h| h[I18n.t(k)] = v }
+    end
 
-  def status_to_icon
-    { like: "😍", acceptable: "😊", dislike: "😞" }
-  end
+    def status_to_icon
+      { like: "😍", acceptable: "😊", dislike: "😞" }
+    end
 end
