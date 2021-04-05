925027b921b26da658b92af81e922fb44901fe07
radin reth <radin.reth@gmail.com>
2021-04-05 22:15:13 +0700

    Fix feedback trend query

--- app/queries/dashboard_query.rb
+++ app/queries/dashboard_query.rb
@@ -163,6 +163,7 @@ class DashboardQuery
 
   private
 
+    # All provinces include in piloting areas
     def all_province_codes
       Filters::PumiFilter.pilot_province_codes
     end
--- app/queries/feedback_trend.rb
+++ app/queries/feedback_trend.rb
@@ -59,6 +59,7 @@ class FeedbackTrend < Feedback
       scope = StepValue.filter(@query.options, @variable.step_values)
       scope = scope.joins(:session)
       scope = scope.where(sessions: { province_id: @query.province_codes })
+      scope = scope.where(sessions: { district_id: @query.district_codes })
       scope = scope.group(location)
       scope = scope.group_by_period(period, "sessions.created_at", format: "%b/%y,%Y")
       scope = scope.group(:variable_value_id)
