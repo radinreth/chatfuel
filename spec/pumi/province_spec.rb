53b9cd02a6633237955cee4c4726377e4ae95968
radin reth <radin.reth@gmail.com>
2021-04-06 16:51:33 +0700

    Update spec

---
+++ spec/pumi/province_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Pumi::Province do
+  subject { Pumi::Province }
+  
+  pilot_provinces = ENV['PILOT_PROVINCE_CODES'].to_s.split(',')
+
+  it ".pilots" do
+    expect(subject.pilots.map(&:id)).to eq(pilot_provinces)
+  end
+
+  pilot_provinces.each do |province_code|
+    context "with #{province_code}" do
+      let(:province) { subject.find_by_id(province_code) }
+
+      it '#pilot_districts' do
+        district_codes = ENV["PILOT_DISTRICT_CODES_FOR_#{province_code}"].to_s.split(',')
+        expect(province.pilot_districts.map(&:id)).to eq(district_codes)
+      end
+    end
+  end
+end
