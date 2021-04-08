f082a1c5c96a91160bb9c87c88c0f35fcddbfc9d
radin reth <radin.reth@gmail.com>
2021-04-08 22:31:06 +0700

    Fix owso feedback trend label

--- app/queries/feedback_trend.rb
+++ app/queries/feedback_trend.rb
@@ -19,8 +19,9 @@ class FeedbackTrend < Feedback
       return {} unless result_set
 
       result_set.each_with_object({}).with_index do |((key, count), hash), index|
-        month, value_id = key
+        date, value_id = key
         variable_value = VariableValue.find(value_id)
+        month = month = format_label(date)
 
         hash[month] ||= {}
         hash[month][variable_value.mapping_value] = count
