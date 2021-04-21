8d8e978d473d970bf444bfa270b9062ce33c44c3
radin reth <radin.reth@gmail.com>
2021-04-21 17:35:49 +0700

    Loading form by default

--- app/controllers/concerns/query_filter.rb
+++ app/controllers/concerns/query_filter.rb
@@ -1,8 +1,10 @@
 class QueryFilter
   def self.before(controller)
     controller.send(:set_daterange)
-    controller.send(:set_query)
-    controller.send(:set_gon)
-    controller.send(:set_active_tab_nav)
+    if controller.request.xhr?
+      controller.send(:set_query)
+      controller.send(:set_gon)
+      controller.send(:set_active_tab_nav)
+    end
   end
 end
