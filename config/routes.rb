c7b332a25b8bc17c7f3180094ec69d1895e27049
radin reth <radin.reth@gmail.com>
2021-04-22 16:33:41 +0700

    Remove showcase action

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,6 @@ Rails.application.routes.draw do
     resources :templates
     resources :quotas, only: [:index]
 
-    # always view by default
     # listview => show the whole frame
     # listview/showcase => only showcase detail
     # listview/summary => only summary tab
@@ -37,7 +36,6 @@ Rails.application.routes.draw do
     resources :listview, only: [:index], as: :listview do
       collection do
         get 'developer'
-        get 'showcase'
       end
     end
 
