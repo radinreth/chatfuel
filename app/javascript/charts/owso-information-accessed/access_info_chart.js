6e4e7464b9bbe68c1b46874ff4cd95acd586c7e8
radin reth <radin.reth@gmail.com>
2021-04-27 10:32:33 +0700

    Show multilines x-axe label instead of truncated

--- app/javascript/charts/owso-information-accessed/access_info_chart.js
+++ app/javascript/charts/owso-information-accessed/access_info_chart.js
@@ -1,9 +1,8 @@
 import BarChart from '../bar_chart'
-import { extractBarDataset, ticksOptions, suggestedMax } from '../../utils/bar_chart'
+import { extractBarDataset, suggestedMax } from '../../utils/bar_chart'
 
 class InformationAccessChart extends BarChart {
   chartId = 'chart_information_access_by_period';
-  ticksOptions = ticksOptions;
 
   suggestedMax = (data) => suggestedMax(data, 1.5);
   dataset = () => extractBarDataset(gon.accessInfo);
