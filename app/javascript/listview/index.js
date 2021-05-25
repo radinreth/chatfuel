c46f71665034e8840b1482595f688d8745274236
radin reth <radin.reth@gmail.com>
2021-05-25 11:05:04 +0700

    Delegate OWSO.WelcomesIndex to OWSO.ListviewIndex

--- app/javascript/listview/index.js
+++ app/javascript/listview/index.js
@@ -1,17 +1,1 @@
-import { onDistrictModalSave, onProvinceModalSave } from '../welcomes/locationSave'
-
-OWSO.ListviewIndex = (() => {
-  function init() {
-    OWSO.DashboardShow.attachEventClickToChartDownloadButton();
-
-    loadform()
-    onDistrictModalSave()
-    onProvinceModalSave()
-  }
-
-  function loadform() {
-    $.rails.fire(document.getElementById("q"), "submit");
-  }
-
-  return { init }
-})()
+OWSO.ListviewIndex = OWSO.WelcomesIndex
