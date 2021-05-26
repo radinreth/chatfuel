d534b24c7ad7039d2b63b2667d0a6b476b8e5f12
radin reth <radin.reth@gmail.com>
2021-05-26 15:46:06 +0700

    Hide popup modal & facebook from iframe

--- app/javascript/application/loader.js
+++ app/javascript/application/loader.js
@@ -1,11 +1,11 @@
 $( document ).on('turbolinks:load', function() {
   var $body = $('body');
   OWSO.Util.closeAlert();
-  let { dev, freshSession } = OWSO.Util;
+  let { dev, freshSession, isEmbed } = OWSO.Util;
   
   $body.tooltip({ selector: '[data-toggle="tooltip"]' });
   $body.popover({ selector: '[data-toggle="popover"]' });
-  if( !dev() && freshSession() ) $('#popup').modal('show');
+  if( !dev() && !isEmbed() && freshSession() ) $('#popup').modal('show');
 
   let currentPage = OWSO.Util.getCurrentPage();
   !!OWSO[currentPage] && OWSO[currentPage].init();
