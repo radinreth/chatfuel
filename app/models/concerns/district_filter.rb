13d5565d2ac77b1ef229b4b918fd55a9b8b2ddf7
radin reth <radin.reth@gmail.com>
2021-06-03 13:17:25 +0700

    Refactor with AreaFilter

--- app/models/concerns/district_filter.rb
+++ app/models/concerns/district_filter.rb
@@ -1,34 +1,21 @@
-class DistrictFilter
+class DistrictFilter < AreaFilter
   class << self
-    def fetch(*codes)
-      return beside_pilot_codes if parse(codes).include?(other_code)
-
-      codes.flatten & district_pilot_codes
-    end
-
     private
-
       def other_code
         '0000'
       end
 
-      def parse(codes)
-        return codes.flatten if codes.is_a? Array
-
-        codes.to_s.split(',') rescue []
-      end
-
       def beside_pilot_codes
-        all_district_codes - district_pilot_codes
+        all_codes - pilot_codes
       end
 
-      def district_pilot_codes
+      def pilot_codes
         Pumi::Province.pilots.inject([]) do |codes, province|
           codes += province.pilot_districts.map(&:id)
         end - Filters::LocationFilter.dump_codes rescue []
       end
 
-      def all_district_codes
+      def all_codes
         Session.pluck(:district_id).uniq - Filters::LocationFilter.dump_codes
       end
   end
