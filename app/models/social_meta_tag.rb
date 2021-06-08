bb7b027946654b9f1fedf75063bcf4139900dae1
radin reth <radin.reth@gmail.com>
2021-06-08 14:09:28 +0700

    Update meta tags data

--- app/models/social_meta_tag.rb
+++ app/models/social_meta_tag.rb
@@ -2,19 +2,19 @@ class SocialMetaTag
   def self.to_meta_tags
     {
       og: {
-        title:    I18n.t('meta_tags.title'),
-        type:     'website',
-        url:      "#{ENV['ENDPOINT_URL']}#{I18n.locale}",
-        image:    "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['ENDPOINT_URL']) }",
+        title:  I18n.t('meta_tags.title'),
+        type: "website",
+        url:  "#{ENV['ENDPOINT_URL']}#{I18n.locale}",
+        image:  "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['ENDPOINT_URL']) }",
         description: I18n.t('meta_tags.description'),
-        site_name: 'InSTEDD iLab Southeast Asia'
+        site_name: I18n.t('meta_tags.site')
       },
       twitter: {
         card: "summary",
         site: I18n.t('meta_tags.twitter.site'),
         title: I18n.t('meta_tags.title'),
         description: I18n.t('meta_tags.description'),
-        creator: "InSTEDD iLab Southeast Asia",
+        creator: I18n.t('meta_tags.site'),
         image: "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['ENDPOINT_URL']) }",
       },
       site: I18n.t('meta_tags.site')
