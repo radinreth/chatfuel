5cf2feb0b8978a626094a40a2ae11c2614bc3d1f
radin reth <radin.reth@gmail.com>
2021-06-15 11:58:38 +0700

    Apply html safe for social meta tag missing after rebased

--- app/models/social_meta_tag.rb
+++ app/models/social_meta_tag.rb
@@ -2,19 +2,19 @@ class SocialMetaTag
   def self.to_meta_tags
     {
       og: {
-        title:  I18n.t('meta_tags.title'),
+        title:  I18n.t('meta_tags.title').html_safe,
         type: "website",
         url:  "#{ENV['ENDPOINT_URL']}#{I18n.locale}",
         image:  "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['ENDPOINT_URL']) }",
-        description: I18n.t('meta_tags.description'),
-        site_name: I18n.t('meta_tags.site')
+        description: I18n.t('meta_tags.description').html_safe,
+        site_name: I18n.t('meta_tags.site').html_safe
       },
       twitter: {
         card: "summary",
-        site: I18n.t('meta_tags.twitter.site'),
-        title: I18n.t('meta_tags.title'),
-        description: I18n.t('meta_tags.description'),
-        creator: I18n.t('meta_tags.site'),
+        site: I18n.t('meta_tags.twitter.site').html_safe,
+        title: I18n.t('meta_tags.title').html_safe,
+        description: I18n.t('meta_tags.description').html_safe,
+        creator: I18n.t('meta_tags.site').html_safe,
         image: "#{ helper.image_url("fb-sample-share-photo.png", host: ENV['ENDPOINT_URL']) }",
       },
     }
