7a6f9816331284c312c5168f0d02e9d364af7deb
radin reth <radin.reth@gmail.com>
2021-06-15 13:32:20 +0700

    Fixed horizontal bar break ui on filter period

--- app/javascript/charts/owso-information-accessed/most_tracked_periodic_chart.js
+++ app/javascript/charts/owso-information-accessed/most_tracked_periodic_chart.js
@@ -1,8 +1,8 @@
-import FilteredBarChart from '../filtered_bar_chart'
-import { suggestedMax } from '../../utils/bar_chart'
+import FilteredBarChart from "../filtered_bar_chart";
+import { suggestedMax } from "../../utils/bar_chart";
 
 class MostPopularAccessChart extends FilteredBarChart {
-  chartId = 'chart_most_service_tracked_periodically';
+  chartId = "chart_most_service_tracked_periodically";
 
   suggestedMax = (data) => suggestedMax(data, 1.5);
   dataset = () => this.format(gon.mostTrackedPeriodic);
