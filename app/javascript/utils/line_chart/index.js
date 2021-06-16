93372124d994590312cafef1cb6118c4dd059eb1
radin reth <radin.reth@gmail.com>
2021-06-16 14:40:37 +0700

    Add poipet user access chart

--- app/javascript/utils/line_chart/index.js
+++ app/javascript/utils/line_chart/index.js
@@ -1,15 +1,16 @@
 export const extractLineDataset = (ds) => {
-  if(ds.length == 0) return {}
+  if (ds.length == 0) return {};
 
+  let labels = _.keys(ds[0].data);
   return {
-    labels: _.keys(ds[0].data),
-    datasets: ds.map( line => ({
-        label: line.name,
-        backgroundColor: line.color,
-        borderColor: line.color,
-        data: _.values(line.data),
-        fill: false,
-      }
-    ))
+    labels: labels,
+    datasets: ds.map((line) => ({
+      label: line.name,
+      backgroundColor: line.color,
+      borderColor: line.color,
+      data: labels.map((label) => line.data[label]),
+      fill: false,
+      spanGaps: true,
+    })),
   };
-}
+};
