3392a45f2a2b1605ddaf3351eaa6a8a01d1c722e
radin reth <radin.reth@gmail.com>
2021-06-17 15:38:19 +0700

    Dynamic label rotation

--- app/javascript/charts/owso-information-accessed/access_info_chart.js
+++ app/javascript/charts/owso-information-accessed/access_info_chart.js
@@ -1,8 +1,13 @@
-import BarChart from '../bar_chart'
-import { extractBarDataset, suggestedMax } from '../../utils/bar_chart'
+import BarChart from "../bar_chart";
+import { extractBarDataset, suggestedMax } from "../../utils/bar_chart";
 
 class InformationAccessChart extends BarChart {
-  chartId = 'chart_information_access_by_period';
+  chartId = "chart_information_access_by_period";
+  rotateOption = {
+    autoSkip: false,
+    maxRotation: 45,
+    minRotation: 45,
+  };
 
   suggestedMax = (data) => suggestedMax(data, 1.5);
   dataset = () => extractBarDataset(gon.accessInfo);
