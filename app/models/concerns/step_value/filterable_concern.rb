d412838eb588d1754bb1fc52801658649863db88
radin reth <radin.reth@gmail.com>
2021-06-23 22:41:59 +0700

    Fix filter service accessed for other location

--- app/models/concerns/step_value/filterable_concern.rb
+++ app/models/concerns/step_value/filterable_concern.rb
@@ -5,8 +5,8 @@ module StepValue::FilterableConcern
     def self.filter(params={}, scope = all)
       scope = scope.where(session_id: Session.where(gender: params[:gender])) if params[:gender].present?
       scope = scope.where(session_id: Session.where(session_type: params[:content_type])) if params[:content_type].present?
-      scope = scope.where(session_id: Session.where(province_id: params[:province_id])) if params[:province_id].present?
-      scope = scope.where(session_id: Session.where(district_id: params[:district_id])) if params[:district_id].present?
+      scope = scope.where(session_id: Session.where("province_id IN (?) ", ProvinceFilter.fetch(params[:province_id]))) if params[:province_id].present?
+      scope = scope.where(session_id: Session.where("district_id IN (?) ", DistrictFilter.fetch(params[:district_id]))) if params[:district_id].present?
       scope = scope.where(session_id: Session.where(platform_name: params[:platform])) if params[:platform].present?
       scope = scope.where(variable_id: params[:variable_id]) if params[:variable_id].present?
       scope = scope.where(variable_value: params[:variable_value]) if params[:variable_value].present?
