51c7a40c142caabed9220174cd4f05f49a3fb458
radin reth <radin.reth@gmail.com>
2021-06-23 13:53:40 +0700

    Add db query

--- app/models/provincial_usage.rb
+++ app/models/provincial_usage.rb
@@ -2,7 +2,7 @@
 # province filter
 class ProvincialUsage
   attr_reader :pro_code, :unique_user, :visit, :service_delivered
-  # pivot by province
+
   def initialize(pro_code:, unique_user:, visit:, service_delivered:)
     @pro_code = pro_code
     @unique_user = unique_user
@@ -40,44 +40,34 @@ end
 
 class ServiceDelivered
   def self.all
-    {"08"=>63, "12"=>377, nil=>3794, "21"=>65, "06"=>22, 
-      "10"=>5, "00"=>2673, "03"=>20, "13"=>9, "17"=>113, 
-      "15"=>40, "01"=>2783, "04"=>1124, "20"=>32, "14"=>31, 
-      "22"=>5, "02"=>23766, "nu"=>4, "07"=>26, "18"=>5, 
-      "11"=>97, "09"=>3, "05"=>33
-    }
+    scope = StepValue.joins(:session, variable_value: :variable)
+    StepValue.filter({ start_date: "2020/08/28",
+                        end_date: "2021/06/21", 
+                        variable_id: Variable.user_visit.id }, 
+                        scope)\
+              .group(:province_id)\
+              .count
   end
 end
 
 class TotalUniqueUser
   def self.all
-    {
-      "00"=>297, "01"=>181, "02"=>2821, "03"=>4, 
-      "04"=>94, "05"=>3, "06"=>3, "07"=>5, "08"=>9, 
-      "09"=>1, "10"=>1, "11"=>12, "12"=>50, "13"=>3, 
-      "14"=>6, "15"=>6, "17"=>10, "18"=>1, "20"=>4, 
-      "21"=>4, "22"=>1, nil=>1114
-    }
+    unique_sql = Session.filter(start_date: "2020/08/28", end_date: "2021/06/21")\
+            .select("DISTINCT ON (session_id) *")\
+            .reorder(:session_id, :gender)\
+            .to_sql
+
+    Session.select("*").from("(#{unique_sql}) sessions")\
+            .unscope(:order)\
+            .group(:province_id)\
+            .count
   end
 end
 
 
 class TotalVisit
-  attr_reader :pro, :count
-
-  def initialize(pro, count)
-    @pro = pro
-    @count = count
-  end
-
   def self.all
-    # query.map { |pro, count| new(pro, count) }
-    {"00"=>579, "01"=>404, "02"=>5420, "03"=>4, 
-      "04"=>167, "05"=>5, "06"=>4, "07"=>5, 
-      "08"=>12, "09"=>1, "10"=>1, "11"=>19, 
-      "12"=>66, "13"=>3, "14"=>6, "15"=>8, "17"=>18, 
-      "18"=>1, "20"=>5, "21"=>10, "22"=>1, nil=>1306
-    }
+    query
   end
 
   private
