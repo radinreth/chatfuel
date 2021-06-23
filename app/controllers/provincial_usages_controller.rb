917f648bc059f3d84b89da80c8b6364b7204607b
radin reth <radin.reth@gmail.com>
2021-06-23 13:24:50 +0700

    Add filter options, pagination

--- app/controllers/provincial_usages_controller.rb
+++ app/controllers/provincial_usages_controller.rb
@@ -1,5 +1,16 @@
 class ProvincialUsagesController < ApplicationController
+  include Filterable
+
+  before_action :default_start_date
+  before_action :set_daterange
+
   def index
-    @provincial_usages = ProvincialUsage.all
+    @pagy, @provincial_usages = pagy_array(ProvincialUsage.all)
+  end
+
+  private
+
+  def default_start_date
+    Setting.dashboard_start_date.strftime('%Y/%m/%d')
   end
 end
