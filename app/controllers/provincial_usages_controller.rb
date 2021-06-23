a41c86a66b5d46b01152c40315ab864c5b8ce488
radin reth <radin.reth@gmail.com>
2021-06-23 15:24:16 +0700

    Filter option

--- app/controllers/provincial_usages_controller.rb
+++ app/controllers/provincial_usages_controller.rb
@@ -3,12 +3,17 @@ class ProvincialUsagesController < ApplicationController
 
   before_action :default_start_date
   before_action :set_daterange
+  before_action :set_query
 
   def index
-    @pagy, @provincial_usages = pagy_array(ProvincialUsage.all)
+    @pagy, @provincial_usages = pagy_array(ProvincialUsage.within(@query))
   end
 
   private
+
+  def set_query
+    @query ||= DashboardQuery.new(filter_options)
+  end
 
   def default_start_date
     Setting.dashboard_start_date.strftime('%Y/%m/%d')
--- app/models/provincial_usage.rb
+++ app/models/provincial_usage.rb
@@ -1,81 +1,80 @@
 # query options (filter)
 # province filter
 class ProvincialUsage
-  attr_reader :pro_code, :unique_user, :visit, :service_delivered
+  attr_reader :pro_name, :unique_user, :visit, :service_delivered
 
-  def initialize(pro_code:, unique_user:, visit:, service_delivered:)
-    @pro_code = pro_code
+  def initialize(pro_name:, unique_user:, visit:, service_delivered:)
+    @pro_name = pro_name
     @unique_user = unique_user
     @visit = visit
     @service_delivered = service_delivered
   end
 
-  def self.all
-    ProvinceFilter.fetch(pro_codes).map do |pro_code|
+  def self.within(query)
+    ProvinceFilter.fetch(query.province_codes).map do |pro_code|
       new(
-        pro_code: pro_code,
-        unique_user: unique_users[pro_code],
-        visit: visit[pro_code],
-        service_delivered: service_delivered[pro_code]
+        pro_name: Pumi::Province.find_by_id(pro_code).name_latin,
+        unique_user: unique_users(query.options)[pro_code],
+        visit: visit(query.options)[pro_code],
+        service_delivered: service_delivered(query.options)[pro_code]
       )
     end
   end
 
-  def self.unique_users
-    @unique_users ||= TotalUniqueUser.all
+  def self.unique_users(options)
+    TotalUniqueUser.within(options)
   end
 
-  def self.visit
-    @visit ||= TotalVisit.all
+  def self.visit(options)
+    TotalVisit.within(options)
   end
 
-  def self.service_delivered
-    @service_delivered ||= ServiceDelivered.all
-  end
-
-  def self.pro_codes
-    Filters::PumiFilter.pilot_province_codes
+  def self.service_delivered(options)
+    ServiceDelivered.within(options)
   end
 end
 
 class ServiceDelivered
-  def self.all
-    scope = StepValue.joins(:session, variable_value: :variable)
-    StepValue.filter({ start_date: "2020/08/28",
-                        end_date: "2021/06/21", 
-                        variable_id: Variable.user_visit.id }, 
-                        scope)\
+  def self.within(options)
+    StepValue.filter(options.merge(variable_options), scope)\
               .group(:province_id)\
               .count
+  end
+  
+  private
+
+  def self.variable_options
+    { variable_id: Variable.user_visit&.id }
+  end
+
+  def self.scope
+    StepValue.joins(:session, variable_value: :variable)
   end
 end
 
 class TotalUniqueUser
-  def self.all
-    unique_sql = Session.filter(start_date: "2020/08/28", end_date: "2021/06/21")\
-            .select("DISTINCT ON (session_id) *")\
-            .reorder(:session_id, :gender)\
-            .to_sql
-
-    Session.select("*").from("(#{unique_sql}) sessions")\
+  def self.within(options)
+    sql = Session.select("*")\
+            .from("(#{distinct_within(options).to_sql}) subquery_sessions")\
             .unscope(:order)\
             .group(:province_id)\
             .count
   end
-end
-
-
-class TotalVisit
-  def self.all
-    query
-  end
 
   private
 
-  def self.query
+  def self.distinct_within(options)
+    sql = Session.filter(options)\
+            .select("DISTINCT ON (province_id, session_id) *")\
+            .reorder(:province_id, :session_id, :gender)
+  end
+end
+
+# start_date: "2020/08/28", end_date: "2021/06/21"
+class TotalVisit
+  def self.within(options)
     Session\
-      .filter(start_date: "2020/08/28", \
-              end_date: "2021/06/21")  \
+      .filter(options)\
       .unscope(:order)\
       .group(:province_id)\
       .count
