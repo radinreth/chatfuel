9ca1b2219311c6e2ce4888e9ea22c5f474e9efae
radin reth <radin.reth@gmail.com>
2021-06-24 12:17:24 +0700

    Add flatpickr and resolve conflict style with select2

--- app/javascript/dashboard/show.js
+++ app/javascript/dashboard/show.js
@@ -154,7 +154,7 @@ OWSO.DashboardShow = (() => {
   }
 
   function multiSelectDistricts() {
-    $("select")
+    $(".select")
       .select2({
         theme: "bootstrap",
       })
@@ -214,33 +214,25 @@ OWSO.DashboardShow = (() => {
   }
 
   function renderDatetimepicker() {
-    $(".datepicker_date")
-      .daterangepicker({
-        locale: { format: "YYYY/MM/DD" },
-        cancelLabel: "Clear",
-        alwaysShowCalendars: true,
-        showCustomRangeLabel: true,
-        ranges: {
-          Today: [moment(), moment()],
-          Yesterday: [
-            moment().subtract(1, "days"),
-            moment().subtract(1, "days"),
-          ],
-          "Last 7 Days": [moment().subtract(6, "days"), moment()],
-          "Last 30 Days": [moment().subtract(29, "days"), moment()],
-          "This Month": [moment().startOf("month"), moment().endOf("month")],
-          "Last Month": [
-            moment().subtract(1, "month").startOf("month"),
-            moment().subtract(1, "month").endOf("month"),
-          ],
-        },
-        opens: "left",
-      })
-      .on("apply.daterangepicker", function (ev, picker) {
-        $(".start_date").val(picker.startDate.format("YYYY/MM/DD"));
-        $(".end_date").val(picker.endDate.format("YYYY/MM/DD"));
-        $(".form").submit();
-      });
+    $(".datepicker_date").flatpickr({
+      dateFormat: "Y/m/d",
+      mode: "range",
+      maxDate: "today",
+      locale: {
+        rangeSeparator: " - ",
+      },
+      defaultDate: [gon.start_date, gon.end_date],
+      // defaultDate: ["2016-10-10", "2016-10-20"],
+      onChange: function (selectedDates, dateStr, instance) {
+        const [startDate, endDate] = selectedDates;
+
+        if (startDate != undefined && endDate != undefined) {
+          $(".start_date").val(instance.formatDate(startDate, "Y/m/d"));
+          $(".end_date").val(instance.formatDate(endDate, "Y/m/d"));
+          $(".form").submit();
+        }
+      },
+    });
   }
 
   function onChangeProvince() {
