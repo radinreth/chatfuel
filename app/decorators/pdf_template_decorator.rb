52df7a82b7d8bf6d74174eca691cfc6bf76481c1
radin reth <radin.reth@gmail.com>
2021-07-02 15:02:18 +0700

    Add display available variable

--- app/decorators/pdf_template_decorator.rb
+++ app/decorators/pdf_template_decorator.rb
@@ -8,4 +8,18 @@ class PdfTemplateDecorator < ApplicationDecorator
     template.render(LiquidServices::BaseLiquid.new(site))
   end
 
+  def variables
+    hashes.keys.each_with_object([]) do |k, arr|
+      hashes[k].keys.each { |k2| arr << "{{#{k}.#{k2}}}" }
+    end
+  rescue
+    []
+  end
+
+  private
+
+  def hashes
+    LiquidServices::BaseLiquid.new(Site.first).to_h
+  end
+
 end
