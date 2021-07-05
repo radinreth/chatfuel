1828c8f5089a0910718c5304c9ce8188c1c1cee1
radin reth <radin.reth@gmail.com>
2021-07-05 17:39:29 +0700

    Fix pdf template request spec

--- spec/requests/pdf_templates_request_spec.rb
+++ spec/requests/pdf_templates_request_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "PdfTemplates", type: :request do
+  let(:pdf_template) { create(:pdf_template) }
+  login_system_admin
 
   describe "GET /index" do
     it "returns http success" do
-      get "/pdf_templates/index"
+      get "/pdf_templates"
       expect(response).to have_http_status(:success)
     end
   end
@@ -18,36 +20,36 @@ RSpec.describe "PdfTemplates", type: :request do
 
   describe "GET /show" do
     it "returns http success" do
-      get "/pdf_templates/show"
+      get "/pdf_templates/#{pdf_template.id}"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
+  describe "POST /create" do
     it "returns http success" do
-      get "/pdf_templates/create"
-      expect(response).to have_http_status(:success)
+      post "/pdf_templates", params: { pdf_template: { name: "test template", content: "HTML content", lang_code: "km" } }
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/pdf_templates/edit"
+      get "/pdf_templates/#{pdf_template.id}/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
+  describe "PUT /update" do
     it "returns http success" do
-      get "/pdf_templates/update"
-      expect(response).to have_http_status(:success)
+      put "/pdf_templates/#{pdf_template.id}", params: { pdf_template: { name: "test template", content: "HTML content", lang_code: "km" } }
+      expect(response).to have_http_status(:found)
     end
   end
 
-  describe "GET /destroy" do
+  describe "DELETE /destroy" do
     it "returns http success" do
-      get "/pdf_templates/destroy"
-      expect(response).to have_http_status(:success)
+      delete "/pdf_templates/#{pdf_template.id}"
+      expect(response).to have_http_status(:found)
     end
   end
 
