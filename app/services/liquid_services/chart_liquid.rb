e5df26ac09317d58311762b3a8e55185dcdae00f
radin reth <radin.reth@gmail.com>
2021-07-05 13:32:41 +0700

    Refactor liquid parsing template

--- app/services/liquid_services/chart_liquid.rb
+++ app/services/liquid_services/chart_liquid.rb
@@ -11,73 +11,25 @@ module LiquidServices
     private
 
     def summary_html
-      summary_html = <<~HTML
-      .bs.container-fluid.d-table.nobreak
-        .row.d-table-row{ style: "margin: 0 auto;"}
-          .col.w-50.d-table-cell
-            .card.mb-4{ role: "figure", style: "width: 600px; border: 1px solid #dfdfdf; margin: 0 auto;" }
-              .card-header.position-relative
-                .d-flex.flex-row.align-items-center.justify-content-between
-                  .h5.m-0.font-weight-bold.d-flex.justify-content-center.align-items-center
-                    %span.chart-name.text-primary.d-inline-block.mr-1.chart-head.text-capitalize
-                      = I18n.t("dashboard.total_user_visit")
-              .card-body
-                .chart-wrapper.pt-4.d-flex.justify-content-around.align-items-center.position-relative{ style: "width: inherit;"}
-                  %canvas#chart_total_user_visit
-          .col.w-50.d-table-cell
-            .card.mb-4{ role: "figure", style: "width: 600px; border: 1px solid #dfdfdf; margin: 0 auto;" }
-              .card-header.position-relative
-                .d-flex.flex-row.align-items-center.justify-content-between
-                  .h5.m-0.font-weight-bold.d-flex.justify-content-center.align-items-center
-                    %span.chart-name.text-primary.d-inline-block.mr-1.chart-head.text-capitalize
-                      = I18n.t("dashboard.total_user_feedback")
-              .card-body
-                .chart-wrapper.pt-4.d-flex.justify-content-around.align-items-center.position-relative{ style: "width: inherit;"}
-                  %canvas#total_user_feedback
-      HTML
-
-      template = Tilt::HamlTemplate.new { summary_html }
+      html = File.read("#{template_path}/total_user_feedback.html.haml")
+      template = Tilt::HamlTemplate.new { html }
       template.render
     end
 
     def feedback_sub_categories_html
-      html = <<~HTML
-      .container-fluid
-        .row
-          .col
-            .card.mb-4{ role: "figure", style: "width: 992px; border: 1px solid #dfdfdf; margin: 0 auto;" }
-              .card-header.position-relative
-                .d-flex.flex-row.align-items-center.justify-content-between
-                  .h5.m-0.font-weight-bold.d-flex.justify-content-center.align-items-center
-                    %span.chart-name.text-primary.d-inline-block.mr-1.chart-head.text-capitalize
-                      = I18n.t("dashboard.total_user_visit")
-              .card-body
-                .chart-wrapper.pt-4.d-flex.justify-content-around.align-items-center.position-relative{ style: "width: inherit; "}
-                  %canvas#chart_feedback_by_sub_category
-      HTML
-
+      html = File.read("#{template_path}/feedback_sub_categories.html.haml")
       template = Tilt::HamlTemplate.new { html }
       template.render
     end
 
     def most_popular_service_html
-      html = <<~HTML
-      .container-fluid
-        .row
-          .col
-            .card.mb-4{ role: "figure", style: "width: 992px; border: 1px solid #dfdfdf; margin: 0 auto;" }
-              .card-header.position-relative
-                .d-flex.flex-row.align-items-center.justify-content-between
-                  .h5.m-0.font-weight-bold.d-flex.justify-content-center.align-items-center
-                    %span.chart-name.text-primary.d-inline-block.mr-1.chart-head.text-capitalize
-                      = I18n.t("welcomes.number_access_by_main_services")
-              .card-body
-                .chart-wrapper.pt-4.d-flex.justify-content-around.align-items-center.position-relative{ style: "width: inherit;"}
-                  %canvas#chart_number_access_by_main_services
-      HTML
-
+      html = File.read("#{template_path}/most_popular_service.html.haml")
       template = Tilt::HamlTemplate.new { html }
       template.render
+    end
+
+    def template_path
+      'app/views/sites/pdf_templates'
     end
   end
 end
